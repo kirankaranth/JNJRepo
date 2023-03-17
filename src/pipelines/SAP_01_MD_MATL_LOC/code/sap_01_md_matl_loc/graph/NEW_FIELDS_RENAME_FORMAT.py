792002cceeb8b1e2b458ab8d907102700bec0a4a
GGrilo1 <ggrilo1@its.jnj.com>
2023-03-17 18:51:52 +0000

    JEKT-186-Update NEW_FIELDS_RENAME_FORMAT

--- src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/NEW_FIELDS_RENAME_FORMAT.py
+++ src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/NEW_FIELDS_RENAME_FORMAT.py
@@ -6,4 +6,4 @@ from sap_01_md_matl_loc.config.ConfigStore import *
 from sap_01_md_matl_loc.udfs.UDFs import *
 
 def NEW_FIELDS_RENAME_FORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.withColumn("SRC_SYS_CD", lit(Config.SRC_SYS_CD)).withColumn("MATL_NUM", col("matnr"))
+    return in0.withColumn("SRC_SYS_CD", lit(Config.SRC_SYS_CD)).withColumn("MATL_NUM", col("matnr").withColumn("PLNT_CD", col("werks"))
