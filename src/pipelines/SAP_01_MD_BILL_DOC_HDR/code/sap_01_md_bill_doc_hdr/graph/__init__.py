3c865424e3025b7f39da981d870f72148e500d8e
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-03-20 12:25:46 +0000

    JEKT-195 join tvfkt

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/__init__.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/__init__.py
@@ -1,4 +1,6 @@
+from .MANDT_FILTER_TVFKT import *
 from .DS_SAP_01_TSPAT import *
+from .DS_SAP_01_TVFKT import *
 from .DS_SAP_01_VBRK import *
 from .MANDT_FILTER import *
 from .MANDT_FILTER_TSPAT import *
--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/pipeline.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/pipeline.py
@@ -11,7 +11,9 @@ def pipeline(spark: SparkSession) -> None:
     df_MANDT_FILTER = MANDT_FILTER(spark, df_DS_SAP_01_VBRK)
     df_DS_SAP_01_TSPAT = DS_SAP_01_TSPAT(spark)
     df_MANDT_FILTER_TSPAT = MANDT_FILTER_TSPAT(spark, df_DS_SAP_01_TSPAT)
-    df_Join_1 = Join_1(spark, df_MANDT_FILTER, df_MANDT_FILTER_TSPAT)
+    df_DS_SAP_01_TVFKT = DS_SAP_01_TVFKT(spark)
+    df_MANDT_FILTER_TVFKT = MANDT_FILTER_TVFKT(spark, df_DS_SAP_01_TVFKT)
+    df_Join_1 = Join_1(spark, df_MANDT_FILTER, df_MANDT_FILTER_TSPAT, df_MANDT_FILTER_TVFKT)
 
 def main():
     spark = SparkSession.builder\
