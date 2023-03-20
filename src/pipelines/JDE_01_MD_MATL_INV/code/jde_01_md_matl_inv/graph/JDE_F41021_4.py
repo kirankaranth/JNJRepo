f7df7b5418ba251b27d5c75d242a0e0460b70172
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-03-20 14:59:11 +0000

    JET-176 finished creating raw source gems

---
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/graph/JDE_F41021_4.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from jde_01_md_matl_inv.config.ConfigStore import *
+from jde_01_md_matl_inv.udfs.UDFs import *
+
+def JDE_F41021_4(spark: SparkSession) -> DataFrame:
+    return spark.sql(f"SELECT * FROM mtr.f41021 WHERE _deleted_ = 'F'")
--- src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/pipeline.py
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/pipeline.py
@@ -8,11 +8,14 @@ from jde_01_md_matl_inv.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_JDE_F4101 = JDE_F4101(spark)
+    df_JDE_F41021_4 = JDE_F41021_4(spark)
     df_JDE_F4101_3 = JDE_F4101_3(spark)
     df_JDE_F4101_4 = JDE_F4101_4(spark)
     df_JDE_F4101_2 = JDE_F4101_2(spark)
     df_JDE_F4101_5 = JDE_F4101_5(spark)
+    df_JDE_F41021_3 = JDE_F41021_3(spark)
     df_JDE_F41021 = JDE_F41021(spark)
+    df_JDE_F41021_2 = JDE_F41021_2(spark)
     df_JDE_F4101_6 = JDE_F4101_6(spark)
 
 def main():
