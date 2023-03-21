becae7e24175a49124474d687effd6dd689238ac
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-03-21 17:15:53 +0000

    JEKT-176 Final clean up

--- src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/pipeline.py
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/pipeline.py
@@ -8,11 +8,11 @@ from jde_01_md_matl_inv.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_JDE_F4101 = JDE_F4101(spark)
-    MD_MAT_INV_SWAP(spark)
     df_JDE_F41021 = JDE_F41021(spark)
     df_JOIN = JOIN(spark, df_JDE_F41021, df_JDE_F4101)
     df_TRANSFORM = TRANSFORM(spark, df_JOIN)
     df_SET_FIELD_ORDER = SET_FIELD_ORDER(spark, df_TRANSFORM)
+    MD_MAT_INV_SWAP(spark, df_SET_FIELD_ORDER)
 
 def main():
     spark = SparkSession.builder\
