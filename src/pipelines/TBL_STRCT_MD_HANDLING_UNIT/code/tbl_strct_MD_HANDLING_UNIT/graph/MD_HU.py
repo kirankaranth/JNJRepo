9321e2ebb4f208f308ac06a99668036e011dcd87
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-03-21 19:42:33 +0000

    JEKT-205 Update job

--- src/pipelines/TBL_STRCT_MD_HANDLING_UNIT/code/tbl_strct_MD_HANDLING_UNIT/graph/MD_HU.py
+++ src/pipelines/TBL_STRCT_MD_HANDLING_UNIT/code/tbl_strct_MD_HANDLING_UNIT/graph/MD_HU.py
@@ -8,6 +8,9 @@ from tbl_strct_MD_HANDLING_UNIT.udfs.UDFs import *
 def MD_HU(spark: SparkSession, in0: DataFrame):
     in0.write\
         .format("delta")\
+        .option("optimizeWrite", True)\
+        .option("overwriteSchema", True)\
         .option("path", f"/mnt/{Config.targetEnv}_curdelta/{Config.targetApp}/{Config.targetDomain}/MD_HU")\
         .mode("overwrite")\
+        .partitionBy("SRC_SYS_CD")\
         .saveAsTable(f"{Config.targetSchema}.MD_HU")
--- src/pipelines/TBL_STRCT_MD_HANDLING_UNIT/code/tbl_strct_MD_HANDLING_UNIT/graph/MD_HU_SER.py
+++ src/pipelines/TBL_STRCT_MD_HANDLING_UNIT/code/tbl_strct_MD_HANDLING_UNIT/graph/MD_HU_SER.py
@@ -8,6 +8,9 @@ from tbl_strct_MD_HANDLING_UNIT.udfs.UDFs import *
 def MD_HU_SER(spark: SparkSession, in0: DataFrame):
     in0.write\
         .format("delta")\
+        .option("optimizeWrite", True)\
+        .option("overwriteSchema", True)\
         .option("path", f"/mnt/{Config.targetEnv}_curdelta/{Config.targetApp}/{Config.targetDomain}/MD_HU_SER")\
         .mode("overwrite")\
+        .partitionBy("SRC_SYS_CD")\
         .saveAsTable(f"{Config.targetSchema}.MD_HU_SER")
