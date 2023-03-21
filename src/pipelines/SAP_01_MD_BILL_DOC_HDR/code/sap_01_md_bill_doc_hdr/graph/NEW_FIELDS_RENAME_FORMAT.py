9f11ab0c0b22a72261fa90539d73c355397cda5e
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-03-21 16:40:52 +0000

    JEKT-226 More Columns Added

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT.py
@@ -6,4 +6,17 @@ from sap_01_md_bill_doc_hdr.config.ConfigStore import *
 from sap_01_md_bill_doc_hdr.udfs.UDFs import *
 
 def NEW_FIELDS_RENAME_FORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.withColumn("BILL_DOC", col("VBELN")).withColumn("SLORG_CD", col("VKORG"))
+    return in0\
+        .withColumn("BILL_DOC", col("VBELN"))\
+        .withColumn("SLORG_CD", trim(col("VKORG")))\
+        .withColumn("SLORG_DESC", trim(col("TVKOT_VTEXT")))\
+        .withColumn("DSTR_CHNL_CD", trim(col("VTWEG")))\
+        .withColumn("DSTR_CHNL_DESC", trim(col("TVTWT_VTEXT")))\
+        .withColumn("SLS_DIV_CD", trim(col("SPART")))\
+        .withColumn("SLS_DIV_DESC", trim(col("TSPAT_VTEXT")))\
+        .withColumn("BILL_TYPE_CD", trim(col("FKART")))\
+        .withColumn("BILL_TYPE_DESC", trim(col("TVFKT_VTEXT")))\
+        .withColumn("BILL_CAT", trim(col("FKTYP")))\
+        .withColumn("DOC_CAT", trim(col("VBTYP")))\
+        .withColumn("PYR", trim(col("KUNRG")))\
+        .withColumn("SOLD_TO", trim(col("KUNAG")))
