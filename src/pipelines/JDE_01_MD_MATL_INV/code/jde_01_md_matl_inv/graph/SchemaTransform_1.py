20f63c68873ea6aad93149989fd59ac151bd6c11
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-03-21 10:03:33 +0000

    JEKT-176 Commit simple transformations

--- src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/graph/SchemaTransform_1.py
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/graph/SchemaTransform_1.py
@@ -6,4 +6,20 @@ from jde_01_md_matl_inv.config.ConfigStore import *
 from jde_01_md_matl_inv.udfs.UDFs import *
 
 def SchemaTransform_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0
+    return in0\
+        .withColumn(
+          "XFRM_SPCL_STCK_IND",
+          when(
+              ((length(trim(col("lipbin"))) == lit(0)) | col("lipbin").isNull()), 
+              lit(
+                "#"
+              )
+            )\
+            .otherwise(col("lipbin"))
+        )\
+        .withColumn(
+        "XFRM_PRTY_NUM",
+        lit(
+          "#"
+        )
+    )
