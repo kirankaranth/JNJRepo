ca51c3341a5df1e39ffffd2dfbde5a534a43e226
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-03-22 13:26:13 +0000

    JEKT-236 Commit initial changes

---
+++ src/pipelines/SAP_01_MD_MATL_INV/code/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'SAP_01_MD_MATL_INV',
+    version = '1.0',
+    packages = find_packages(include = ('sap_01_md_matl_inv*', )) + ["prophecy_config_instances"],
+    package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
+    package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.4.2'],
+    entry_points = {
+'console_scripts' : [
+'main = sap_01_md_matl_inv.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
