99204111aadf5e7caf66ae9edc4ea3ba3c51692d
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-03-22 16:46:23 +0000

    JEKT-195 added new config variables and updated schematransofrm gem

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/config/Config.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/config/Config.py
@@ -6,18 +6,20 @@ class Config(ConfigBase):
 
     def __init__(
             self,
-            SRC_SYS_CD: str=None, 
+            sourceSystem: str=None, 
+            targetSchema: str=None, 
             MANDT: str=None, 
             COLUMNS: int=None, 
             DBNAME: str=None, 
             DAI_ETL_ID: int=None
     ):
         self.spark = None
-        self.update(SRC_SYS_CD, MANDT, COLUMNS, DBNAME, DAI_ETL_ID)
+        self.update(sourceSystem, targetSchema, MANDT, COLUMNS, DBNAME, DAI_ETL_ID)
 
     def update(
             self,
-            SRC_SYS_CD: str="hm2", 
+            sourceSystem: str="hm2", 
+            targetSchema: str="l1_md_prophecy", 
             MANDT: str="100", 
             COLUMNS: int=80, 
             DBNAME: str="pqa_hm2", 
@@ -25,7 +27,8 @@ class Config(ConfigBase):
     ):
         global prophecy_spark_context
         prophecy_spark_context = self.spark
-        self.SRC_SYS_CD = SRC_SYS_CD
+        self.sourceSystem = sourceSystem
+        self.targetSchema = targetSchema
         self.MANDT = MANDT
         self.COLUMNS = self.get_int_value(COLUMNS)
         self.DBNAME = DBNAME
