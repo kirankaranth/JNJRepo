a285f2683cfa8d3b9922887be873c40b5a49c3a9
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-03-22 13:55:22 +0000

    JEKT-24 Switch order of operations to allow flexibility on source system missing columns

--- src/pipelines/SK_SAP_01_MD_SER_NUM_STOCK_SGMNT/code/sk_sap_01_md_ser_num_stock_sgmnt/pipeline.py
+++ src/pipelines/SK_SAP_01_MD_SER_NUM_STOCK_SGMNT/code/sk_sap_01_md_ser_num_stock_sgmnt/pipeline.py
@@ -9,9 +9,9 @@ from sk_sap_01_md_ser_num_stock_sgmnt.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_SAP_EQBS = SAP_EQBS(spark)
     df_MANDT_Filter_Parameter = MANDT_Filter_Parameter(spark, df_SAP_EQBS)
-    df_NEW_FIELDS_RENAME_FORMAT = NEW_FIELDS_RENAME_FORMAT(spark, df_MANDT_Filter_Parameter)
-    df_SET_FIELD_ORDER_REFORMAT = SET_FIELD_ORDER_REFORMAT(spark, df_NEW_FIELDS_RENAME_FORMAT)
-    MD_SER_NUM_STOCK_SGMNT(spark, df_SET_FIELD_ORDER_REFORMAT)
+    df_SET_FIELD_ORDER_REFORMAT_1 = SET_FIELD_ORDER_REFORMAT_1(spark, df_MANDT_Filter_Parameter)
+    df_NEW_FIELDS_RENAME_FORMAT = NEW_FIELDS_RENAME_FORMAT(spark, df_SET_FIELD_ORDER_REFORMAT_1)
+    MD_SER_NUM_STOCK_SGMNT(spark, df_NEW_FIELDS_RENAME_FORMAT)
 
 def main():
     spark = SparkSession.builder\
