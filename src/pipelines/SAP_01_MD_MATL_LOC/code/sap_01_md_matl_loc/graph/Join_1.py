2dcfbdfe0ff792a6a4e80c42aaa0b05f7a58b8fa
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-03-23 17:36:18 +0000

    JEKT-215: Update join conditions

--- src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/Join_1.py
+++ src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/Join_1.py
@@ -16,7 +16,11 @@ def Join_1(
     return in0\
         .alias("in0")\
         .join(in1.alias("in1"), (col("in0.MMSTA") == col("in1.MMSTA")), "left_outer")\
-        .join(in2.alias("in2"), (col("in0.DISPO") == col("in2.DISPO")), "left_outer")\
+        .join(
+          in2.alias("in2"),
+          ((col("in0.WERKS") == col("in2.WERKS")) & (col("in0.DISPO") == col("in2.DISPO"))),
+          "left_outer"
+        )\
         .join(
           in3.alias("in3"),
           ((col("in0.WERKS") == col("in3.WERKS")) & (col("in0.FEVOR") == col("in3.FEVOR"))),
