6e94b6056423e0e9a4009ee8aa138e82194bcbde
ggrilo1@its.jnj.com <ggrilo1@its.jnj.com>
2023-03-24 15:55:51 +0000

    JEKT-186 ❇️ New     - Pipeline        - JDE_MD_MATL_LOC_DEU        - JDE_MD_MATL_LOC_SJD        - JDE_MD_MATL_LOC_GMD        - JDE_MD_MATL_LOC_DJD        - JDE_MD_MATL_LOC_JEM        - JDE_MD_MATL_LOC_MTR     - Dataset        - DS_JDE_4102
    🔁 Modified
        - Pipeline
           - JDE_MD_MATL_LOC_JSW
        - Dataset
           - DS_JDE_F4102_ADT
           - DS_SAP_MARC
           - MD_MATL_LOC

---
+++ src/pipelines/JDE_MD_MATL_LOC_SJD/code/sap_01_md_matl_loc/config/Config.py
@@ -0,0 +1,33 @@
+from prophecy.config import ConfigBase
+
+
+class Config(ConfigBase):
+
+    def __init__(
+            self,
+            sourceSystem: str=None,
+            COLUMNS: int=None,
+            targetSchema: str=None,
+            TABLENAME: str=None,
+            DAI_ETL_ID: int=None,
+            **kwargs
+    ):
+        self.spark = None
+        self.update(sourceSystem, COLUMNS, targetSchema, TABLENAME, DAI_ETL_ID)
+
+    def update(
+            self,
+            sourceSystem: str="jet",
+            COLUMNS: int=134,
+            targetSchema: str="l1_md_prophecy",
+            TABLENAME: str="f4102",
+            DAI_ETL_ID: int=0,
+            **kwargs
+    ):
+        prophecy_spark = self.spark
+        self.sourceSystem = sourceSystem
+        self.COLUMNS = self.get_int_value(COLUMNS)
+        self.targetSchema = targetSchema
+        self.TABLENAME = TABLENAME
+        self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
+        pass
