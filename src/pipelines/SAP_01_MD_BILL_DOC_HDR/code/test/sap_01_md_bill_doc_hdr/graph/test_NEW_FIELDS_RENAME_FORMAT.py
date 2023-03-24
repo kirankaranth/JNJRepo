b911260ffc988305895e00da9b46c16bca072d4c
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-03-24 16:39:41 +0000

    JEKT-195 modifies unit test

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/test/sap_01_md_bill_doc_hdr/graph/test_NEW_FIELDS_RENAME_FORMAT.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/test/sap_01_md_bill_doc_hdr/graph/test_NEW_FIELDS_RENAME_FORMAT.py
@@ -5,50 +5,48 @@ from argparse import Namespace
 from prophecy.test import BaseTestCase
 from prophecy.test.utils import *
 from sap_01_md_bill_doc_hdr.graph.NEW_FIELDS_RENAME_FORMAT import *
-import sap_01_md_bill_doc_hdr.config.ConfigStore as ConfigStore
+from sap_01_md_bill_doc_hdr.config.ConfigStore import *
 
 
 class NEW_FIELDS_RENAME_FORMATTest(BaseTestCase):
 
-    def test_unit_test_0(self):
+    def test_timestamp(self):
         dfIn0 = createDfFromResourceFiles(
             self.spark,
             'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/in0/schema.json',
-            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/in0/data/test_unit_test_0.json',
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/in0/data/test_timestamp.json',
             'in0'
         )
         dfOut = createDfFromResourceFiles(
             self.spark,
             'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/out/schema.json',
-            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/out/data/test_unit_test_0.json',
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/out/data/test_timestamp.json',
             'out'
         )
         dfOutComputed = NEW_FIELDS_RENAME_FORMAT(self.spark, dfIn0)
         assertDFEquals(
-            dfOut.select(
-              "BILL_DOC",
-              "SLORG_DESC",
-              "DSTR_CHNL_DESC",
-              "SLS_DIV_DESC",
-              "BILL_TYPE_DESC",
-              "CRT_DTTM",
-              "BILL_DTTM",
-              "INDSTR_CD_1",
-              "BILL_DOC_IS_CAN",
-              "EXCH_RT_FIN_PSTNG"
-            ),
-            dfOutComputed.select(
-              "BILL_DOC",
-              "SLORG_DESC",
-              "DSTR_CHNL_DESC",
-              "SLS_DIV_DESC",
-              "BILL_TYPE_DESC",
-              "CRT_DTTM",
-              "BILL_DTTM",
-              "INDSTR_CD_1",
-              "BILL_DOC_IS_CAN",
-              "EXCH_RT_FIN_PSTNG"
-            ),
+            dfOut.select("BILL_DOC", "CRT_DTTM", "BILL_DTTM"),
+            dfOutComputed.select("BILL_DOC", "CRT_DTTM", "BILL_DTTM"),
+            self.maxUnequalRowsToShow
+        )
+
+    def test_decimals(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/in0/schema.json',
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/in0/data/test_decimals.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/out/schema.json',
+            'test/resources/data/sap_01_md_bill_doc_hdr/graph/NEW_FIELDS_RENAME_FORMAT/out/data/test_decimals.json',
+            'out'
+        )
+        dfOutComputed = NEW_FIELDS_RENAME_FORMAT(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("EXCH_RT_FIN_PSTNG", "NET_VAL_AMT", "BILL_DOC"),
+            dfOutComputed.select("EXCH_RT_FIN_PSTNG", "NET_VAL_AMT", "BILL_DOC"),
             self.maxUnequalRowsToShow
         )
 
@@ -56,7 +54,12 @@ class NEW_FIELDS_RENAME_FORMATTest(BaseTestCase):
         BaseTestCase.setUp(self)
         import os
         fabricName = os.environ['FABRIC_NAME']
-        ConfigStore.Utils.initializeFromArgs(
+        Utils.initializeFromArgs(
             self.spark,
-            Namespace(file = f"configs/resources/config/{fabricName}.json", config = None, overrideJson = None)
+            Namespace(
+              file = f"configs/resources/config/{fabricName}.json",
+              config = None,
+              overrideJson = None,
+              defaultConfFile = None
+            )
         )
