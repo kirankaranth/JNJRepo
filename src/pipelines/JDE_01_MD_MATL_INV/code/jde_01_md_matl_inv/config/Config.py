1eb22e62a4266b30c48bb958f0094192f664eb3f
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-03-24 14:10:11 +0000

    JEKT-176 🔁 Modified     - Pipeline        - JDE_01_MD_MATL_INV

--- src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/config/Config.py
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/config/Config.py
@@ -3,40 +3,10 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(
-            self,
-            sourceSystem: str=None,
-            targetSchema: str=None,
-            DBNAME: str=None,
-            DBTABLE: str=None,
-            DBTABLE1: str=None,
-            COLUMNS: int=None,
-            DAI_ETL_ID: str=None,
-            DIVISOR: int=None,
-            **kwargs
-    ):
+    def __init__(self, **kwargs):
         self.spark = None
-        self.update(sourceSystem, targetSchema, DBNAME, DBTABLE, DBTABLE1, COLUMNS, DAI_ETL_ID, DIVISOR)
+        self.update()
 
-    def update(
-            self,
-            sourceSystem: str="gmd",
-            targetSchema: str="l1_md_prophecy",
-            DBNAME: str="gmd",
-            DBTABLE: str="f4101  ",
-            DBTABLE1: str="f41021  ",
-            COLUMNS: int=24,
-            DAI_ETL_ID: str="1",
-            DIVISOR: int=1,
-            **kwargs
-    ):
+    def update(self, **kwargs):
         prophecy_spark = self.spark
-        self.sourceSystem = sourceSystem
-        self.targetSchema = targetSchema
-        self.DBNAME = DBNAME
-        self.DBTABLE = DBTABLE
-        self.DBTABLE1 = DBTABLE1
-        self.COLUMNS = self.get_int_value(COLUMNS)
-        self.DAI_ETL_ID = DAI_ETL_ID
-        self.DIVISOR = self.get_int_value(DIVISOR)
         pass
