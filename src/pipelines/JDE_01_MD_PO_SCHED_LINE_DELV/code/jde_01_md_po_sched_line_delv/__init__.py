22e0156e69df649df239f52286e97ae60673b3b9
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-03-27 15:34:46 +0000

    JEKT-166
    🔁 Modified
        - Pipeline
           - JDE_01_MD_PO_SCHED_LINE_DELV

--- src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/__init__.py
+++ src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/__init__.py
@@ -1,4 +1,4 @@
 from .graph import *
 from .udfs import *
-from .config import *
 from .pipeline import *
+from .config import *
--- src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/config/Config.py
+++ src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/config/Config.py
@@ -1,30 +1,30 @@
 from prophecy.config import ConfigBase
-prophecy_spark_context = None
 
 
 class Config(ConfigBase):
 
     def __init__(
             self,
-            SRC_SYS_CD: str=None, 
-            DBNAME: str=None, 
-            TABLENAME: str=None, 
-            DAI_ETL_ID: int=None, 
-            COLUMNS: int=None
+            SRC_SYS_CD: str=None,
+            DBNAME: str=None,
+            TABLENAME: str=None,
+            DAI_ETL_ID: int=None,
+            COLUMNS: int=None,
+            **kwargs
     ):
         self.spark = None
         self.update(SRC_SYS_CD, DBNAME, TABLENAME, DAI_ETL_ID, COLUMNS)
 
     def update(
             self,
-            SRC_SYS_CD: str="jes", 
-            DBNAME: str="jes", 
-            TABLENAME: str="f4311", 
-            DAI_ETL_ID: int=0, 
-            COLUMNS: int=21
+            SRC_SYS_CD: str="jes",
+            DBNAME: str="jes",
+            TABLENAME: str="f4311",
+            DAI_ETL_ID: int=0,
+            COLUMNS: int=21,
+            **kwargs
     ):
-        global prophecy_spark_context
-        prophecy_spark_context = self.spark
+        prophecy_spark = self.spark
         self.SRC_SYS_CD = SRC_SYS_CD
         self.DBNAME = DBNAME
         self.TABLENAME = TABLENAME
