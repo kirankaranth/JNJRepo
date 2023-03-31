dd25076242d1268dc70c25b3d9f8561220d01cc7
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-03-31 17:48:52 +0000

    JEKT-236 (Updated Set Operation) 🔁 Modified     - Pipeline        - SAP_MD_MATL_INV_HM2

--- src/pipelines/SAP_01_MD_MATL_INV/code/sap_01_md_matl_inv/graph/SetOperation_1_Union.py
+++ src/pipelines/SAP_01_MD_MATL_INV/code/sap_01_md_matl_inv/graph/SetOperation_1_Union.py
@@ -6,4 +6,6 @@ from sap_01_md_matl_inv.config.ConfigStore import *
 from sap_01_md_matl_inv.udfs.UDFs import *
 
 def SetOperation_1_Union(spark: SparkSession, in0: DataFrame, in1: DataFrame, in2: DataFrame) -> DataFrame:
-    return in0.exceptAll(in1).exceptAll(in2)
+    df1 = in0.unionByName(in1, allowMissingColumns = True)
+
+    return df1.unionByName(in2, allowMissingColumns = True)
