073f629753558c4bab4faf7c087d24bbbd4a1fd1
smille29@its.jnj.com <smille29@its.jnj.com>
2023-04-03 21:33:43 +0000

    JEKT-14 - Updated Config Parameter syntax in Pipeline     - Pipeline        - SAP_MD_BOM_HDR_BBA_BBN_HCS_MRS     - Dataset        - SAP_01_STKO

--- src/pipelines/SAP_MD_BOM_HDR_BBA_BBN_HCS_MRS/code/sap_md_bom_hdr_bba_bbn_hcs_mrs/graph/SAP_01_STKO.py
+++ src/pipelines/SAP_MD_BOM_HDR_BBA_BBN_HCS_MRS/code/sap_md_bom_hdr_bba_bbn_hcs_mrs/graph/SAP_01_STKO.py
@@ -6,4 +6,4 @@ from sap_md_bom_hdr_bba_bbn_hcs_mrs.config.ConfigStore import *
 from sap_md_bom_hdr_bba_bbn_hcs_mrs.udfs.UDFs import *
 
 def SAP_01_STKO(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM bba.stko WHERE _deleted_ = 'F'")
+    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.stko WHERE _deleted_ = 'F'")
