5687345465b8e64ab99ad4da2a40229d3c702ede
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-04 19:00:39 +0000

    JEKT-166: config value changed 🔁 Modified     - Pipeline        - JDE_01_MD_PO_SCHED_LINE_DELV_JES_JEM_JET_JSW_MTR_SJD_DEU

--- src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/config/Config.py
+++ src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/jde_01_md_po_sched_line_delv/config/Config.py
@@ -10,11 +10,11 @@ class Config(ConfigBase):
             sourceTable: str=None,
             DAI_ETL_ID: int=None,
             targetSchema: str=None,
-            ConfigDatabase: str=None,
+            configDatabase: str=None,
             **kwargs
     ):
         self.spark = None
-        self.update(sourceSystem, sourceDatabase, sourceTable, DAI_ETL_ID, targetSchema, ConfigDatabase)
+        self.update(sourceSystem, sourceDatabase, sourceTable, DAI_ETL_ID, targetSchema, configDatabase)
 
     def update(
             self,
@@ -23,7 +23,7 @@ class Config(ConfigBase):
             sourceTable: str="f4311",
             DAI_ETL_ID: int=0,
             targetSchema: str="dev_l1_md",
-            ConfigDatabase: str=None,
+            configDatabase: str=None,
             **kwargs
     ):
         prophecy_spark = self.spark
@@ -32,5 +32,5 @@ class Config(ConfigBase):
         self.sourceTable = sourceTable
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.targetSchema = targetSchema
-        self.ConfigDatabase = ConfigDatabase
+        self.configDatabase = configDatabase
         pass
