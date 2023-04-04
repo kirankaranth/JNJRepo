9a34b7958150fcdbea783c460cbe824842483f26
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-04 16:59:00 +0000

    JEKT-314 🔁 deleted all gems to restart     - Pipeline        - SAP_MD_DELV_LINE_BBA_BBL_BBN

--- src/pipelines/SAP_MD_DELV_LINE_BBA_BBL_BBN/code/sap_md_delv_line_bba_bbl_bbn/graph/DS_SAP_01_VBAP.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_md_delv_line_bba_bbl_bbn.config.ConfigStore import *
-from sap_md_delv_line_bba_bbl_bbn.udfs.UDFs import *
-
-def DS_SAP_01_VBAP(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM bba.vbap WHERE _deleted_ = 'F'")
