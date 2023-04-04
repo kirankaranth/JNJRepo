76a2ce941eb8160704ef53b7cc4845464b747acd
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-04-04 04:08:27 +0000

    ❌ Deleted     - Pipeline        - SK_SAP_01_MD_SER_NUM_STOCK_SGMNT     - Job        - JOB_TBL_STRCT_MD        - L1_MD_BBN

--- src/pipelines/SK_SAP_01_MD_SER_NUM_STOCK_SGMNT/code/main.py
+++
@@ -1,3 +0,0 @@
-from sk_sap_01_md_ser_num_stock_sgmnt.pipeline import main
-
-main()
--- src/pipelines/SK_SAP_01_MD_SER_NUM_STOCK_SGMNT/code/sk_sap_01_md_ser_num_stock_sgmnt/graph/MANDT_Filter_Parameter.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sk_sap_01_md_ser_num_stock_sgmnt.config.ConfigStore import *
-from sk_sap_01_md_ser_num_stock_sgmnt.udfs.UDFs import *
-
-def MANDT_Filter_Parameter(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter((col("MANDT") == lit(Config.MANDT)))
