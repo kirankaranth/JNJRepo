828be2715a5d4023ec5cdf665b7eef1894a11388
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-04-04 16:49:52 +0000

    JEKT-246     - Pipeline        - JDE_01_DEU_F0401     - Dataset        - DS_JDE_01_F0401        - DS_JDE_01_F0101

--- src/pipelines/JDE_01_DEU_F0401/code/jde_01_deu_f0401/graph/DS_JDE_01_F0101.py
+++ src/pipelines/JDE_01_DEU_F0401/code/jde_01_deu_f0401/graph/DS_JDE_01_F0101.py
@@ -6,4 +6,4 @@ from jde_01_deu_f0401.config.ConfigStore import *
 from jde_01_deu_f0401.udfs.UDFs import *
 
 def DS_JDE_01_F0101(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM deu.f0101 WHERE _deleted_ = 'F'")
+    return spark.sql(f"SELECT * FROM {f" {Config.sourceSystem}"}.F0101 WHERE _deleted_ = 'F'")
--- src/pipelines/JDE_01_DEU_F0401/code/jde_01_deu_f0401/graph/DS_JDE_01_F0401.py
+++ src/pipelines/JDE_01_DEU_F0401/code/jde_01_deu_f0401/graph/DS_JDE_01_F0401.py
@@ -6,4 +6,4 @@ from jde_01_deu_f0401.config.ConfigStore import *
 from jde_01_deu_f0401.udfs.UDFs import *
 
 def DS_JDE_01_F0401(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM deu.f0401 WHERE _deleted_ = 'F' ")
+    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.F0401 WHERE _deleted_ = 'F' ")
