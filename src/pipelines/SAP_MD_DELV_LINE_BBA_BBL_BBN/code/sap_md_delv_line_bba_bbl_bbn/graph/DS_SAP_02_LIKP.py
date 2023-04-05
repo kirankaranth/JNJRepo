8e162cf96cc8eb2108eeeccc26ff08314c641eb6
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-05 13:25:36 +0000

    JEKT-314 🔁 Modified Source GEMs databases     - Pipeline        - SAP_MD_DELV_LINE_BBA_BBL_BBN     - Dataset        - DS_SAP_01_VBUP        - DS_SAP_02_LIKP        - DS_SAP_02_LIPS        - DS_SAP_02_VBAK

--- src/pipelines/SAP_MD_DELV_LINE_BBA_BBL_BBN/code/sap_md_delv_line_bba_bbl_bbn/graph/DS_SAP_02_LIKP.py
+++ src/pipelines/SAP_MD_DELV_LINE_BBA_BBL_BBN/code/sap_md_delv_line_bba_bbl_bbn/graph/DS_SAP_02_LIKP.py
@@ -6,4 +6,4 @@ from sap_md_delv_line_bba_bbl_bbn.config.ConfigStore import *
 from sap_md_delv_line_bba_bbl_bbn.udfs.UDFs import *
 
 def DS_SAP_02_LIKP(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM bba.likp WHERE _deleted_ = 'F'")
+    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.likp WHERE _deleted_ = 'F'")
--- src/pipelines/SAP_MD_DELV_LINE_BBA_BBL_BBN/code/sap_md_delv_line_bba_bbl_bbn/graph/DS_SAP_02_LIPS.py
+++ src/pipelines/SAP_MD_DELV_LINE_BBA_BBL_BBN/code/sap_md_delv_line_bba_bbl_bbn/graph/DS_SAP_02_LIPS.py
@@ -6,4 +6,4 @@ from sap_md_delv_line_bba_bbl_bbn.config.ConfigStore import *
 from sap_md_delv_line_bba_bbl_bbn.udfs.UDFs import *
 
 def DS_SAP_02_LIPS(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM bba.lips WHERE _deleted_ = 'F'")
+    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.lips WHERE _deleted_ = 'F'")
