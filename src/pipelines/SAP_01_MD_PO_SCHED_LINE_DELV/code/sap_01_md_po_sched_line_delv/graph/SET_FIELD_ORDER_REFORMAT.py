7fbf053843459fab48706b79e82ee7f51efc7391
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-05 18:10:33 +0000

    JEKT-166: schema adjustment 🔁 Modified     - Pipeline        - SAP_MD_PO_SCHED_LINE_DELV_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_HMD_MBP_MRS_P01_SVS_TAI_HMD     - Dataset        - MD_PO_SCHED_LINE_DELV

--- src/pipelines/SAP_01_MD_PO_SCHED_LINE_DELV/code/sap_01_md_po_sched_line_delv/graph/SET_FIELD_ORDER_REFORMAT.py
+++ src/pipelines/SAP_01_MD_PO_SCHED_LINE_DELV/code/sap_01_md_po_sched_line_delv/graph/SET_FIELD_ORDER_REFORMAT.py
@@ -31,6 +31,6 @@ def SET_FIELD_ORDER_REFORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
         col("_l0_upt_"), 
         col("_pk_"), 
         col("_pk_md5_"), 
-        col("_l1_upt_").alias("_l1_upt_\u00A0"), 
-        col("_deleted_").alias("_deleted_\u00A0")
+        col("_l1_upt_"), 
+        col("_deleted_")
     )
