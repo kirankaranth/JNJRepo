8913ee1307d0cf9b51a559c08ee16b457ae96c91
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-05 10:12:04 +0000

    JEKT-402 Created config for all SRC_SYS_CDs 🔁 Modified     - Pipeline        - MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ

--- src/pipelines/MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ/code/md_matl_alt_uom_bw2_deu_djd_gmd_jem_jes_jet_jsw_mtr_sdj/config/Config.py
+++ src/pipelines/MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ/code/md_matl_alt_uom_bw2_deu_djd_gmd_jem_jes_jet_jsw_mtr_sdj/config/Config.py
@@ -3,10 +3,37 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            sourceSystem: str=None,
+            targetSchema: str=None,
+            DBNAME: str=None,
+            DBTABLE: str=None,
+            DBTABLE1: str=None,
+            COLUMNS: int=None,
+            DAI_ETL_ID: int=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update()
+        self.update(sourceSystem, targetSchema, DBNAME, DBTABLE, DBTABLE1, COLUMNS, DAI_ETL_ID)
 
-    def update(self, **kwargs):
+    def update(
+            self,
+            sourceSystem: str="gmd",
+            targetSchema: str="dev_md_l1",
+            DBNAME: str="gmd",
+            DBTABLE: str="f41002",
+            DBTABLE1: str="f4101",
+            COLUMNS: int=28,
+            DAI_ETL_ID: int=0,
+            **kwargs
+    ):
         prophecy_spark = self.spark
+        self.sourceSystem = sourceSystem
+        self.targetSchema = targetSchema
+        self.DBNAME = DBNAME
+        self.DBTABLE = DBTABLE
+        self.DBTABLE1 = DBTABLE1
+        self.COLUMNS = self.get_int_value(COLUMNS)
+        self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         pass
