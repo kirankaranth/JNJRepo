530ad32f97d102fe794b7e07e055e60bc3a2c4d3
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-05 14:46:43 +0000

    JEKT-402 Final changes 🔁 Modified     - Pipeline        - MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI

---
+++ src/pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI/code/test/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/graph/test_SchemaTransform_1.py
@@ -0,0 +1,45 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from argparse import Namespace
+from prophecy.test import BaseTestCase
+from prophecy.test.utils import *
+from md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai.graph.SchemaTransform_1 import *
+from md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai.config.ConfigStore import *
+
+
+class SchemaTransform_1Test(BaseTestCase):
+
+    def test_unit_test_(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/graph/SchemaTransform_1/in0/schema.json',
+            'test/resources/data/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/graph/SchemaTransform_1/in0/data/test_unit_test_.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/graph/SchemaTransform_1/out/schema.json',
+            'test/resources/data/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/graph/SchemaTransform_1/out/data/test_unit_test_.json',
+            'out'
+        )
+        dfOutComputed = SchemaTransform_1(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("GTIN_NUM_HRMZD"),
+            dfOutComputed.select("GTIN_NUM_HRMZD"),
+            self.maxUnequalRowsToShow
+        )
+
+    def setUp(self):
+        BaseTestCase.setUp(self)
+        import os
+        fabricName = os.environ['FABRIC_NAME']
+        Utils.initializeFromArgs(
+            self.spark,
+            Namespace(
+              file = f"configs/resources/config/{fabricName}.json",
+              config = None,
+              overrideJson = None,
+              defaultConfFile = None
+            )
+        )
