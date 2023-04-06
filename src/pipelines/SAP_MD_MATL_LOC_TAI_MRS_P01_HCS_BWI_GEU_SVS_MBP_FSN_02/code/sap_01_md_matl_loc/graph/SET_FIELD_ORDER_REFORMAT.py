0da159e936312afab29fc5837b396d9fb9216895
ggrilo1@its.jnj.com <ggrilo1@its.jnj.com>
2023-04-06 07:45:21 +0000

    JEKT-186 🔁 Modified     - Pipeline        - SAP_MD_MATL_LOC_TAI_MRS_P01_HCS_BWI_GEU_SVS_MBP_FSN     - Dataset        - MD_MATL_LOC

--- src/pipelines/SAP_MD_MATL_LOC_TAI_MRS_P01_HCS_BWI_GEU_SVS_MBP_FSN_02/code/sap_01_md_matl_loc/graph/SET_FIELD_ORDER_REFORMAT.py
+++ src/pipelines/SAP_MD_MATL_LOC_TAI_MRS_P01_HCS_BWI_GEU_SVS_MBP_FSN_02/code/sap_01_md_matl_loc/graph/SET_FIELD_ORDER_REFORMAT.py
@@ -53,6 +53,7 @@ def SET_FIELD_ORDER_REFORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
         col("vint2").cast(DecimalType(18, 4)).alias("FRWD_CNSMPTN_DAYS_CNT"), 
         trim(col("vrmod")).alias("CNSMPTN_MODE_CD"), 
         trim(col("ekgrp")).alias("PRCHSNG_GRP_CD"), 
+        trim(col("stlal")).alias("ALT_BOM"), 
         lit(Config.DAI_ETL_ID).alias("DAI_ETL_ID"), 
         current_timestamp().alias("DAI_CRT_DTTM"), 
         current_timestamp().alias("DAI_UPDT_DTTM"), 
