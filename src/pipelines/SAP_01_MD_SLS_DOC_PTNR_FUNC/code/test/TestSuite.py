dbf9bce2243e5845091668ba29842405159f6b02
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-04-06 14:59:07 +0000

    JEKT-323 Unit Testing Creation

--- src/pipelines/SAP_01_MD_SLS_DOC_PTNR_FUNC/code/test/TestSuite.py
+++ src/pipelines/SAP_01_MD_SLS_DOC_PTNR_FUNC/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.sap_01_md_sls_doc_ptnr_func.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/SAP_01_MD_SLS_DOC_PTNR_FUNC/code/test/__init__.py
+++ src/pipelines/SAP_01_MD_SLS_DOC_PTNR_FUNC/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .sap_01_md_sls_doc_ptnr_func import *
 from .conftest import *
 from .TestSuite import *
