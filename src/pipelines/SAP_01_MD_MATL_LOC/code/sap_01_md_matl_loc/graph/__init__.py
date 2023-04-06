ce015856aed7974f22723c4b8032115444acd540
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-04-06 19:07:48 +0000

    JEKT-215 Datasets recreated after copy Pipeline from local branch

--- src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/__init__.py
+++ src/pipelines/SAP_01_MD_MATL_LOC/code/sap_01_md_matl_loc/graph/__init__.py
@@ -3,7 +3,6 @@ from .SAP_T141T import SAP_T141T
 from .SAP_T024F import SAP_T024F
 from .Join_1 import Join_1
 from .SAP_T024 import SAP_T024
-from .SAP_MD_MATL_LOC import SAP_MD_MATL_LOC
 from .SET_FIELDS_ORDER_REFORMAT import SET_FIELDS_ORDER_REFORMAT
 from .MANDT_FILTER_04 import MANDT_FILTER_04
 from .SAP_T024D import SAP_T024D
