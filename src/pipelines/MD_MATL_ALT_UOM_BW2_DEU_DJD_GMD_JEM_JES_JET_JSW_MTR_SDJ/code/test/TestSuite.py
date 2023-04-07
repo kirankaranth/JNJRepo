18e97088a9ec578a633bef7c04ec73a808f8b2c3
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-07 09:21:45 +0000

    JEKT-402 Unit test added
    🔁 Modified
        - Pipeline
           - MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ

--- src/pipelines/MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ/code/test/TestSuite.py
+++ src/pipelines/MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.md_matl_alt_uom_bw2_deu_djd_gmd_jem_jes_jet_jsw_mtr_sdj.graph.test_SchemaTransform_1 import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
