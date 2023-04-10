313cba9057affb37e97327f9beeb3516a7852494
fsano1@its.jnj.com <fsano1@its.jnj.com>
2023-04-10 12:59:28 +0000

    JEKT-136 🔁 Modified     - Pipeline        - SAP_01_MD_SLS_ORDR_LINE     - Dataset        - DS_SAP_01_VBAP

--- src/pipelines/SAP_01_MD_SLS_ORDR_LINE/code/sap_01_md_sls_ordr_line/config/Config.py
+++ src/pipelines/SAP_01_MD_SLS_ORDR_LINE/code/sap_01_md_sls_ordr_line/config/Config.py
@@ -3,10 +3,31 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            sourceSystem: str=None,
+            MANDT: str=None,
+            sourceDatabase: str=None,
+            DAI_ETL_ID: int=None,
+            targetSchema: str=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update()
+        self.update(sourceSystem, MANDT, sourceDatabase, DAI_ETL_ID, targetSchema)
 
-    def update(self, **kwargs):
+    def update(
+            self,
+            sourceSystem: str="bbl",
+            MANDT: str="100",
+            sourceDatabase: str="bbl",
+            DAI_ETL_ID: int=0,
+            targetSchema: str="dev-md_l1",
+            **kwargs
+    ):
         prophecy_spark = self.spark
+        self.sourceSystem = sourceSystem
+        self.MANDT = MANDT
+        self.sourceDatabase = sourceDatabase
+        self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
+        self.targetSchema = targetSchema
         pass
