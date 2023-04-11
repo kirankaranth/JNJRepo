0f0858a964ce0ab72b1cc2bcedac1246f318416c
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-04-11 20:33:42 +0000

    JEKT-246 🔁 Modified     - Pipeline        - SAP_01_LFM1_MBP

--- src/pipelines/JDE_01_LFM1_MBP/code/jde_01_deu_f0401/graph/NEW_FIELDS_TRANSFORMATION.py
+++ src/pipelines/JDE_01_LFM1_MBP/code/jde_01_deu_f0401/graph/NEW_FIELDS_TRANSFORMATION.py
@@ -6,7 +6,14 @@ from jde_01_deu_f0401.config.ConfigStore import *
 from jde_01_deu_f0401.udfs.UDFs import *
 
 def NEW_FIELDS_TRANSFORMATION(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.withColumn(
-        "CRT_ON_DTTM",
-        when((col("ERDAT") == lit("00000000")), lit(None)).otherwise(to_timestamp(col("ERDAT"), "yyyyMMdd"))
-    )
+    return in0\
+        .withColumn("SUP_NUM", col("LIFNR"))\
+        .withColumn("PRCHSNG_ORG_NUM", col("EKORG"))\
+        .withColumn(
+          "CRT_ON_DTTM",
+          when((col("ERDAT") == lit("00000000")), lit(None)).otherwise(to_timestamp(col("ERDAT"), "yyyyMMdd"))
+        )\
+        .withColumn("PRCH_BLK_IND", trim(col("SPERM")))\
+        .withColumn("DEL_IND", trim(col("LOEVM")))\
+        .withColumn("CRNCY_CD", trim(col("WAERS")))\
+        .withColumn("PMT_TERM_CD", trim(col("ZTERM")))
