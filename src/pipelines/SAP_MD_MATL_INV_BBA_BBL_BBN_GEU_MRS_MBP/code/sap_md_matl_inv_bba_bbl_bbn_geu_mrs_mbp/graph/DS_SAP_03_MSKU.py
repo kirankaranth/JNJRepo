c2b5323040ae39d2d31057b26ef8aad9f4725337
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-04-11 17:27:09 +0000

    JEKT-176 (Updated format for Date fields) 🔁 Modified     - Pipeline        - SAP_MD_MATL_INV_BBA_BBL_BBN_GEU_MRS        - SAP_MD_MATL_INV_P01        - SAP_MD_MATL_INV_MBP     - Dataset        - DS_SAP_01_MARD        - DS_SAP_03_MSKU        - MD_MATL_INV        - DS_SAP_05_MSLB
    ❇️ New
        - Dataset
           - DS_SAP_03_MSLB

--- src/pipelines/SAP_MD_MATL_INV_BBA_BBL_BBN_GEU_MRS_MBP/code/sap_md_matl_inv_bba_bbl_bbn_geu_mrs_mbp/graph/DS_SAP_03_MSKU.py
+++ src/pipelines/SAP_MD_MATL_INV_BBA_BBL_BBN_GEU_MRS_MBP/code/sap_md_matl_inv_bba_bbl_bbn_geu_mrs_mbp/graph/DS_SAP_03_MSKU.py
@@ -6,4 +6,4 @@ from sap_md_matl_inv_bba_bbl_bbn_geu_mrs_mbp.config.ConfigStore import *
 from sap_md_matl_inv_bba_bbl_bbn_geu_mrs_mbp.udfs.UDFs import *
 
 def DS_SAP_03_MSKU(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.msku WHERE _deleted_ = 'F'")
+    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.mslb WHERE _deleted_ = 'F'")
