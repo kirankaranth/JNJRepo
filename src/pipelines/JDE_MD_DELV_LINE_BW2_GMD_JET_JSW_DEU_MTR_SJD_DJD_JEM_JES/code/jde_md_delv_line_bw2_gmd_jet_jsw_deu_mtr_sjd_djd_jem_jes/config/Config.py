24dd0c0e183d969cc9f6616e02185fcbb77c06d8
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-11 15:08:23 +0000

    JEKT-314 🔁 created config parameters for pipeline for all JDE systems     - Pipeline        - JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
+++ src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
@@ -3,10 +3,49 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            sourceSystem: str=None,
+            sourceDatabase: str=None,
+            sourceTable: str=None,
+            DAI_ETL_ID: int=None,
+            targetSchema: str=None,
+            ConfigDatabase: str=None,
+            DBTABLE1: str=None,
+            DBTABLE2: str=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update()
+        self.update(
+            sourceSystem, 
+            sourceDatabase, 
+            sourceTable, 
+            DAI_ETL_ID, 
+            targetSchema, 
+            ConfigDatabase, 
+            DBTABLE1, 
+            DBTABLE2
+        )
 
-    def update(self, **kwargs):
+    def update(
+            self,
+            sourceSystem: str="bw2",
+            sourceDatabase: str="bw2",
+            sourceTable: str="F43121, F4211",
+            DAI_ETL_ID: int=0,
+            targetSchema: str="dev_md_l1",
+            ConfigDatabase: str=" ",
+            DBTABLE1: str="F43121",
+            DBTABLE2: str="F4211",
+            **kwargs
+    ):
         prophecy_spark = self.spark
+        self.sourceSystem = sourceSystem
+        self.sourceDatabase = sourceDatabase
+        self.sourceTable = sourceTable
+        self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
+        self.targetSchema = targetSchema
+        self.ConfigDatabase = ConfigDatabase
+        self.DBTABLE1 = DBTABLE1
+        self.DBTABLE2 = DBTABLE2
         pass
