75b62bdc504eb3dbaf375765eb0eafb682958059
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-11 15:42:29 +0000

    JEKT-314 🔁 deleted filter gems and added schematransform gems     - Pipeline        - JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/__init__.py
+++ src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/__init__.py
@@ -1,4 +1,3 @@
-from .Filter_1 import Filter_1
+from .NEW_FIELDS_RENAME_FORMAT_F4211 import NEW_FIELDS_RENAME_FORMAT_F4211
 from .DS_JDE_01_F43121 import DS_JDE_01_F43121
 from .DS_JDE_01_F4211 import DS_JDE_01_F4211
-from .Filter_2 import Filter_2
