39f2db22aa9d29494e1b813935e8dae167492543
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-11 17:09:28 +0000

    JEKT-314 🔁 added reformat gems     - Pipeline        - JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

---
+++ src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/Reformat_1.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.config.ConfigStore import *
+from jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.udfs.UDFs import *
+
+def Reformat_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0
