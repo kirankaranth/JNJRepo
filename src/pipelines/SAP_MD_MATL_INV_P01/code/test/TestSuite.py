a7a65a1e678bf06de6a92981d518ddf172cb8993
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-04-12 18:53:20 +0000

    JEKT-176 (Added Unit Test for P01) 🔁 Modified     - Pipeline        - SAP_MD_MATL_INV_P01

--- src/pipelines/SAP_MD_MATL_INV_P01/code/test/TestSuite.py
+++ src/pipelines/SAP_MD_MATL_INV_P01/code/test/TestSuite.py
@@ -2,6 +2,7 @@ import unittest
 
 from test.sap_md_matl_inv_p01.graph.test_SchemaTransform_5_MSLB import *
 from test.sap_md_matl_inv_p01.graph.test_SchemaTransform_2_MCHB import *
+from test.sap_md_matl_inv_p01.graph.test_SchemaTransform_1_MARD import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
