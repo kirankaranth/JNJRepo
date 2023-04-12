c4c93c0e6a0dcaac6310e9080fba3541f2733e41
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-12 12:32:18 +0000

    JEKT-314 🔁 corrected timestamp rule     - Pipeline        - JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/__init__.py
+++ src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/__init__.py
@@ -3,4 +3,5 @@ from .NEW_FIELDS_RENAME_FORMAT_F4211 import NEW_FIELDS_RENAME_FORMAT_F4211
 from .DS_JDE_01_F43121 import DS_JDE_01_F43121
 from .DS_JDE_01_F4211 import DS_JDE_01_F4211
 from .Reformat_2 import Reformat_2
+from .NEW_FIELDS_RENAME_FORMAT_F43121 import NEW_FIELDS_RENAME_FORMAT_F43121
 from .SetOperation_1 import SetOperation_1
