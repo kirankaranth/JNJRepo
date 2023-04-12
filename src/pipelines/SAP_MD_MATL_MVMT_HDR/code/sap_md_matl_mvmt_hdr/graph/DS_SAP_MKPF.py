2bfb0023846d3f2095b58d4aa9ced3f2291d63d2
ggrilo1@its.jnj.com <ggrilo1@its.jnj.com>
2023-04-12 11:43:11 +0000

    JEKT-1210 ❇️ New     - Dataset        - DS_SAP_MKPF     - Pipeline        - SAP_MD_MATL_MVMT_HDR
    🔁 Modified
        - Dataset
           - MD_MATL_MVMT_HDR

---
+++ src/pipelines/SAP_MD_MATL_MVMT_HDR/code/sap_md_matl_mvmt_hdr/graph/DS_SAP_MKPF.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from sap_md_matl_mvmt_hdr.config.ConfigStore import *
+from sap_md_matl_mvmt_hdr.udfs.UDFs import *
+
+def DS_SAP_MKPF(spark: SparkSession) -> DataFrame:
+    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.mkpf WHERE _deleted_ = 'F'")
