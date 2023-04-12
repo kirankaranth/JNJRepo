0188df7078d9716bb15d5d8513538304825a472a
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-04-12 13:37:42 +0000

    JEKT-314 🔁 Renamed filter gems     - Pipeline        - JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/DELETED_FILTER_F43121.py
+++ src/pipelines/JDE_MD_DELV_LINE_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/DELETED_FILTER_F43121.py
@@ -5,5 +5,5 @@ from prophecy.libs import typed_lit
 from jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.config.ConfigStore import *
 from jde_md_delv_line_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.udfs.UDFs import *
 
-def Filter_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
+def DELETED_FILTER_F43121(spark: SparkSession, in0: DataFrame) -> DataFrame:
     return in0.filter((col("_deleted_") == lit("F")))
