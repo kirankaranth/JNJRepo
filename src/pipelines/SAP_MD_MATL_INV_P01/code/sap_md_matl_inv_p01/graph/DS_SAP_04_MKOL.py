22d76ddece2b8936804af8ec028d8d81451a1ba7
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-04-12 15:59:27 +0000

    JEKT-176 (Updated Predicate and MANDT Filters) 🔁 Modified     - Pipeline        - SAP_MD_MATL_INV_P01        - SAP_MD_MATL_INV_BBA_BBL_BBN_GEU_MRS        - SAP_MD_MATL_INV_MBP     - Job        - JOB_MD_MATL_INV     - Dataset        - DS_SAP_06_MSSL        - DS_SAP_04_MKOL        - DS_SAP_03_MSKU        - DS_SAP_07_MARA        - DS_SAP_05_MSLB        - DS_SAP_01_MARD        - DS_SAP_02_MCHB

--- src/pipelines/SAP_MD_MATL_INV_P01/code/sap_md_matl_inv_p01/graph/DS_SAP_04_MKOL.py
+++ src/pipelines/SAP_MD_MATL_INV_P01/code/sap_md_matl_inv_p01/graph/DS_SAP_04_MKOL.py
@@ -6,4 +6,4 @@ from sap_md_matl_inv_p01.config.ConfigStore import *
 from sap_md_matl_inv_p01.udfs.UDFs import *
 
 def DS_SAP_04_MKOL(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.mkol WHERE _deleted_ = 'F'")
+    return spark.read.table(f"{Config.sourceSystem}.mkol")
