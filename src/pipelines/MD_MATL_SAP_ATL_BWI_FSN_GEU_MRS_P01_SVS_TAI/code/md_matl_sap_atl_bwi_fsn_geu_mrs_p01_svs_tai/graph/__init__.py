0671cbc6961bea7609f9866d7d7763f92c451b49
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-13 07:33:19 +0000

    JEKT-1209 Target gem built 🔁 Modified     - Pipeline        - SAP_MD_MATL_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI     - Dataset        - MD_MATL

--- src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/__init__.py
+++ src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/__init__.py
@@ -1,6 +1,7 @@
 from .MAKTX_LU import MAKTX_LU
 from .XFORM import XFORM
 from .SELECT_FIELDS import SELECT_FIELDS
+from .TARGET import TARGET
 from .DEL_AND_MANDT import DEL_AND_MANDT
 from .MARA import MARA
 from .MAKT import MAKT
