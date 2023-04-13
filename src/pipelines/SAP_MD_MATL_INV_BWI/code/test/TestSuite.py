c3ad6404f64995c17d57c5ee91aa61a2dd5476d0
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-04-13 22:21:29 +0000

    JEKT-176 (Created Pipeline and Job for BWI) 🔁 Modified     - Dataset        - MD_MATL_INV     - Job        - JOB_MD_MATL_INV
    ❇️ New
        - Dataset
           - DS_SAP_005_MARA
           - DS_SAP_001_MARD
           - DS_SAP_003_MCHB
           - DS_SAP_003_MSLB
           - DS_SAP_004_MSSL
        - Pipeline
           - SAP_MD_MATL_INV_BWI

---
+++ src/pipelines/SAP_MD_MATL_INV_BWI/code/test/TestSuite.py
@@ -0,0 +1,8 @@
+import unittest
+
+from test.sap_md_matl_inv_bwi.graph.test_SchemaTransform_2_MCHB import *
+from test.sap_md_matl_inv_bwi.graph.test_SchemaTransform_3_MSLB import *
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(unittest.TestSuite())
