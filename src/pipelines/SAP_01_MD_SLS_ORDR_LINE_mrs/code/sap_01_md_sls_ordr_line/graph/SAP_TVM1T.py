6cf9f388ddec5185a7130d556a019efff7160634
fsano1@its.jnj.com <fsano1@its.jnj.com>
2023-04-13 13:14:05 +0000

    JEKT-136 ❇️ New     - Dataset        - DS_SAP_MRS_VBAP     - Pipeline        - SAP_01_MD_SLS_ORDR_LINE_mrs
    🔁 Modified
        - Pipeline
           - SAP_MD_SLS_ORDR_LINE_MBP

---
+++ src/pipelines/SAP_01_MD_SLS_ORDR_LINE_mrs/code/sap_01_md_sls_ordr_line/graph/SAP_TVM1T.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from sap_01_md_sls_ordr_line.config.ConfigStore import *
+from sap_01_md_sls_ordr_line.udfs.UDFs import *
+
+def SAP_TVM1T(spark: SparkSession) -> DataFrame:
+    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.tvm1t WHERE spras = 'E'")
