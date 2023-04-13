f57d75a384ddccd0c129f8e4c7929c430b13bd47
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-13 01:54:31 +0000

    ❇️ JEKT-305     - Dataset        - DS_SAP_02_VTTK     - Pipeline        - SAP_01_MD_SHIP_FSN
    🔁 Modified
        - Dataset
           - MD_SHIP

---
+++ src/pipelines/SAP_01_MD_SHIP_FSN/code/sap_01_md_ship_fsn/graph/SAP_VTTK.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from sap_01_md_ship_fsn.config.ConfigStore import *
+from sap_01_md_ship_fsn.udfs.UDFs import *
+
+def SAP_VTTK(spark: SparkSession) -> DataFrame:
+    return spark.sql(f"SELECT * FROM {Config.sourceSystem}.vttk WHERE _deleted_ = 'F'")
