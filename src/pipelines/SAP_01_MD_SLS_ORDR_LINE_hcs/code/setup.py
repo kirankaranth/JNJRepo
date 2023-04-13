ed347fc0878992dea0b1351c532226e7fceb0ee4
fsano1@its.jnj.com <fsano1@its.jnj.com>
2023-04-13 11:29:36 +0000

    JEKET-136 ❇️ New     - Dataset        - DS_SAP_HCS_VBAP        - DS_SAP_TVM4T_HCS     - Pipeline        - SAP_01_MD_SLS_ORDR_LINE_hcs
    🔁 Modified
        - Dataset
           - MD_SLS_ORDR_LINE
           - DS_SAP_01_TVM2T

---
+++ src/pipelines/SAP_01_MD_SLS_ORDR_LINE_hcs/code/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'SAP_01_MD_SLS_ORDR_LINE_hcs',
+    version = '1.0',
+    packages = find_packages(include = ('sap_01_md_sls_ordr_line*', )) + ["prophecy_config_instances"],
+    package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
+    package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.4.8'],
+    entry_points = {
+'console_scripts' : [
+'main = sap_01_md_sls_ordr_line.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
