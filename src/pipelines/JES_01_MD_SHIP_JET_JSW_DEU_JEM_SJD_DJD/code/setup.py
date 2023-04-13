b388dd90e6ea3208d8c21e53334310856d46fdb2
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-13 01:34:43 +0000

    🔁 JEKT-305     - Pipeline        - JDE_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD

--- src/pipelines/JES_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD/code/setup.py
+++ src/pipelines/JES_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD/code/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'JES_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD',
+    name = 'JDE_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD',
     version = '1.0',
     packages = find_packages(include = ('jes_01_md_ship_jet_jsw_deu_jem_sjd_djd*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
