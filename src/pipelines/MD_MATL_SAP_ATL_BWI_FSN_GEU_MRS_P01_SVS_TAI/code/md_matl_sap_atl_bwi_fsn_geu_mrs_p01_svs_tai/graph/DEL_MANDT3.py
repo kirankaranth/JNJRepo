7a84c10924ae2291ff88d7aefe2818ae5684515a
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-13 16:11:03 +0000

    JEKT-1209 Added in filters for new source gems 🔁 Modified     - Pipeline        - SAP_MD_MATL_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI

--- src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/DEL_MANDT3.py
+++ src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/DEL_MANDT3.py
@@ -5,7 +5,5 @@ from prophecy.libs import typed_lit
 from md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai.config.ConfigStore import *
 from md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai.udfs.UDFs import *
 
-def DEL_AND_MANDT_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(
-        (((col("_deleted_") == lit("F")) & (col("MANDT") == lit(Config.MANDT))) & (col("SPRAS") == lit("E")))
-    )
+def DEL_MANDT3(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.filter(((col("_deleted_") == lit("F")) & (col("MANDT") == lit(Config.MANDT))))
--- src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/pipeline.py
+++ src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/pipeline.py
@@ -8,16 +8,20 @@ from md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_MAKT = MAKT(spark)
-    df_DEL_AND_MANDT_1 = DEL_AND_MANDT_1(spark, df_MAKT)
-    MAKTX_LU(spark, df_DEL_AND_MANDT_1)
+    df_DEL_MANDT4 = DEL_MANDT4(spark, df_MAKT)
+    MAKTX_LU(spark, df_DEL_MANDT4)
     df_MARA = MARA(spark)
-    df_DEL_AND_MANDT = DEL_AND_MANDT(spark, df_MARA)
-    df_XFORM = XFORM(spark, df_DEL_AND_MANDT)
+    df_DEL_MANDT3 = DEL_MANDT3(spark, df_MARA)
+    df_XFORM = XFORM(spark, df_DEL_MANDT3)
     df_SELECT_FIELDS = SELECT_FIELDS(spark, df_XFORM)
     df_CABN = CABN(spark)
+    df_DEL_MANDT2 = DEL_MANDT2(spark, df_CABN)
     df_INOB = INOB(spark)
     df_AUSP = AUSP(spark)
+    df_DEL_MANDT = DEL_MANDT(spark, df_AUSP)
     TARGET(spark, df_SELECT_FIELDS)
+    df_D_M_OBTAB = D_M_OBTAB(spark, df_INOB)
+    df_SQLStatement_1 = SQLStatement_1(spark, df_DEL_MANDT, df_D_M_OBTAB, df_DEL_MANDT2)
 
 def main():
     spark = SparkSession.builder\
