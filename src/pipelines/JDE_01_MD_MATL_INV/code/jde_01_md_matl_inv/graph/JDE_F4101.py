db218cf348d377498e430b3c6f2f8a783dcdf409
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-13 15:25:57 +0000

    JEKT-176 Updated config per latest requirements and removed predicate filter 🔁 Modified     - Pipeline        - JDE_MD_MATL_INV_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ     - Dataset        - DS_JDE_01_F4101        - DS_JDE_01_F41021

--- src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/graph/JDE_F4101.py
+++ src/pipelines/JDE_01_MD_MATL_INV/code/jde_01_md_matl_inv/graph/JDE_F4101.py
@@ -6,4 +6,4 @@ from jde_01_md_matl_inv.config.ConfigStore import *
 from jde_01_md_matl_inv.udfs.UDFs import *
 
 def JDE_F4101(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.DBNAME}.{Config.DBTABLE} WHERE _deleted_ = 'F'")
+    return spark.read.table(f"{Config.sourceDatabase}.{Config.DBTABLE}")
