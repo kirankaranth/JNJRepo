a38aa588f55cbd704f91f3a87b7cbadb70e92910
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-14 11:54:31 +0000

    🔁 JEKT-305     - Unit Tests created     - Pipeline        - JDE_01_MD_SHIP_MTR_BW2_GMD_JES

--- src/pipelines/JDE_01_MD_SHIP_MTK/code/test/TestSuite.py
+++ src/pipelines/JDE_01_MD_SHIP_MTK/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_01_md_ship_mtk.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/JDE_01_MD_SHIP_MTK/code/test/__init__.py
+++ src/pipelines/JDE_01_MD_SHIP_MTK/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .jde_01_md_ship_mtk import *
 from .conftest import *
 from .TestSuite import *
