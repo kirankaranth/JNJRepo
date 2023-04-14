48568955e37f0e87cb0b406ca0464910f0804c5a
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-04-14 12:10:19 +0000

    JEKT-1272 Unit Testing & Job Creation ❇️ New     - Job        - JOB_MD_SLS_DOC_PTNR_FUNC_HM2
    🔁 Modified
        - Pipeline
           - SAP_MD_SLS_DOC_PTNR_FUNC_HM2

---
+++ src/pipelines/SAP_MD_SLS_DOC_PTNR_FUNC_HM2/code/test/sap_md_sls_doc_ptnr_func_hm2/graph/test_NEW_FIELDS_RENAME_FORMAT.py
@@ -0,0 +1,45 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from argparse import Namespace
+from prophecy.test import BaseTestCase
+from prophecy.test.utils import *
+from sap_md_sls_doc_ptnr_func_hm2.graph.NEW_FIELDS_RENAME_FORMAT import *
+from sap_md_sls_doc_ptnr_func_hm2.config.ConfigStore import *
+
+
+class NEW_FIELDS_RENAME_FORMATTest(BaseTestCase):
+
+    def test_unit_test_(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_sls_doc_ptnr_func_hm2/graph/NEW_FIELDS_RENAME_FORMAT/in0/schema.json',
+            'test/resources/data/sap_md_sls_doc_ptnr_func_hm2/graph/NEW_FIELDS_RENAME_FORMAT/in0/data/test_unit_test_.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_sls_doc_ptnr_func_hm2/graph/NEW_FIELDS_RENAME_FORMAT/out/schema.json',
+            'test/resources/data/sap_md_sls_doc_ptnr_func_hm2/graph/NEW_FIELDS_RENAME_FORMAT/out/data/test_unit_test_.json',
+            'out'
+        )
+        dfOutComputed = NEW_FIELDS_RENAME_FORMAT(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("SLS_DOC_ITEM_NBR", "SLS_DOC_ID", "PTNR_FUNC_CD"),
+            dfOutComputed.select("SLS_DOC_ITEM_NBR", "SLS_DOC_ID", "PTNR_FUNC_CD"),
+            self.maxUnequalRowsToShow
+        )
+
+    def setUp(self):
+        BaseTestCase.setUp(self)
+        import os
+        fabricName = os.environ['FABRIC_NAME']
+        Utils.initializeFromArgs(
+            self.spark,
+            Namespace(
+              file = f"configs/resources/config/{fabricName}.json",
+              config = None,
+              overrideJson = None,
+              defaultConfFile = None
+            )
+        )
