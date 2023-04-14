255cc0829c3e48bb32d582eb6a00b0d5e819aae0
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-14 07:37:47 +0000

    JEKT-1209 updated source table and name to variables 🔁 Modified     - Pipeline        - SAP_MD_MATL_BBA_BBN     - Dataset        - DS_SAP_MARA_BBA_BBN

--- src/pipelines/SAP_MD_MATL_BBA_BBN/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/DS_SAP_MARA_BBA_BBN.py
+++ src/pipelines/SAP_MD_MATL_BBA_BBN/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/DS_SAP_MARA_BBA_BBN.py
@@ -6,4 +6,4 @@ from md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai.config.ConfigStore import *
 from md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai.udfs.UDFs import *
 
 def DS_SAP_MARA_BBA_BBN(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"bba.mara")
+    return spark.read.table(f"{Config.sourceDatabase}.{Config.DBTABLE1}")
