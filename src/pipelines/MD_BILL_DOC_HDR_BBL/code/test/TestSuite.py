83c505763e200d1fee6abba0bd624084a9cc23c2
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-04-18 03:23:46 +0000

    JEKT-226 remove unit tests 🔁 Modified     - Pipeline        - SAP_01_MD_BILL_DOC_HDR_BBL

--- src/pipelines/MD_BILL_DOC_HDR_BBL/code/test/TestSuite.py
+++ src/pipelines/MD_BILL_DOC_HDR_BBL/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-from test.md_bill_doc_hdr_bbl.graph.test_NEW_FIELDS_RENAME_FORMAT import *
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/MD_BILL_DOC_HDR_BBL/code/test/__init__.py
+++ src/pipelines/MD_BILL_DOC_HDR_BBL/code/test/__init__.py
@@ -1,3 +1,2 @@
-from .md_bill_doc_hdr_bbl import *
 from .conftest import *
 from .TestSuite import *
