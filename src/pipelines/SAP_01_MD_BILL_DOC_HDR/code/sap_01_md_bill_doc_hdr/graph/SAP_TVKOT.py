c3cae1cf1c228ec01906e29ccbc20d3e0e0f7fda
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-04-18 03:03:46 +0000

    JEKT-226 remove failing unit tests before merge to Beta 🔁 Modified     - Pipeline        - SAP_01_MD_BILL_DOC_HDR_BBA_BBN     - Dataset        - MD_BILL_DOC_HDR        - DS_SAP_01_VBRK

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/SAP_TVKOT.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/SAP_TVKOT.py
@@ -6,4 +6,4 @@ from sap_01_md_bill_doc_hdr.config.ConfigStore import *
 from sap_01_md_bill_doc_hdr.udfs.UDFs import *
 
 def SAP_TVKOT(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.TVKOT WHERE _deleted_ = 'F' and SPRAS = 'E'")
+    return spark.read.table(f"{Config.sourceDatabase}.tvkot")
