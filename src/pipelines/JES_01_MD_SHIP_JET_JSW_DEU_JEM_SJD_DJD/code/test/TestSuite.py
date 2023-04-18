c1c5be5d5ca56de3b073176928d97d3486356a77
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-18 22:08:12 +0000

    🔁 JEKT-305     - Pipeline        - SAP_01_MD_SHIP_BBN_BBL_P01_HCS        - JDE_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD        - SAP_01_MD_SHIP_FSN        - SAP_01_MD_SHIP_MBP

--- src/pipelines/JES_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD/code/test/TestSuite.py
+++ src/pipelines/JES_01_MD_SHIP_JET_JSW_DEU_JEM_SJD_DJD/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jes_01_md_ship_jet_jsw_deu_jem_sjd_djd.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/SAP_01_MD_SHIP_FSN/code/test/__init__.py
+++ src/pipelines/SAP_01_MD_SHIP_FSN/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .sap_01_md_ship_fsn import *
 from .conftest import *
 from .TestSuite import *
