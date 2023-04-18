8bb953428d84e5fa34a8d567ad31e423e96ab026
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-18 11:53:04 +0000

    🔁 JEKT-226     - Job        - JOB_MD_BILL_DOC_HDR     - Dataset        - DS_SAP_01_TVFKT        - DS_SAP_01_TVKOT        - DS_SAP_01_TVTWT        - DS_SAP_01_TSPAT     - Pipeline        - SAP_01_MD_BILL_DOC_HDR_BBA_BBN

--- src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/SAP_TVFKT.py
+++ src/pipelines/SAP_01_MD_BILL_DOC_HDR/code/sap_01_md_bill_doc_hdr/graph/SAP_TVFKT.py
@@ -6,4 +6,4 @@ from sap_01_md_bill_doc_hdr.config.ConfigStore import *
 from sap_01_md_bill_doc_hdr.udfs.UDFs import *
 
 def SAP_TVFKT(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.TVFKT WHERE _deleted_ = 'F' and SPRAS = 'E'")
+    return spark.read.table(f"{Config.sourceDatabase}.tvfkt")
