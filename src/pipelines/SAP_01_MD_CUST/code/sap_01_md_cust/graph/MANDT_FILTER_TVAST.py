866eef4edcffbf2c2b8f4d9294f755370ded0738
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-04-18 00:49:49 +0000

    🔁 JEKT-1367     - Pipeline        - SAP_01_MD_CUST

--- src/pipelines/SAP_01_MD_CUST/code/sap_01_md_cust/graph/MANDT_FILTER_TVAST.py
+++ src/pipelines/SAP_01_MD_CUST/code/sap_01_md_cust/graph/MANDT_FILTER_TVAST.py
@@ -6,4 +6,6 @@ from sap_01_md_cust.config.ConfigStore import *
 from sap_01_md_cust.udfs.UDFs import *
 
 def MANDT_FILTER_TVAST(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(lit(True))
+    return in0.filter(
+        (((col("MANDT") == lit(Config.MANDT)) & (col("SPRAS") == lit("E"))) & (col("_deleted_") == lit("F")))
+    )
