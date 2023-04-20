a8aee7cfb87e0db33d5a7a0847fbbcd454a0446b
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-20 15:05:30 +0000

    JEKT-166 🔁 Modified     - Pipeline        - SAP_MD_PO_SCHED_LINE_DELV_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_HMD_MBP_MRS_P01_SVS_TAI_HMD        - JDE_01_MD_PO_SCHED_LINE_DELV_JES_JEM_JET_JSW_MTR_SJD_DEU_GMD_BW2

--- src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/setup.py
+++ src/pipelines/JDE_01_MD_PO_SCHED_LINE_DELV/code/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'JDE_01_MD_PO_SCHED_LINE_DELV_JES_JEM_JET_JSW_MTR_SJD_DEU',
+    name = 'JDE_01_MD_PO_SCHED_LINE_DELV_JES_JEM_JET_JSW_MTR_SJD_DEU_GMD_BW2',
     version = '1.0',
     packages = find_packages(include = ('jde_01_md_po_sched_line_delv*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
     package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==1.4.8'],
+'prophecy-libs==1.5.0'],
     entry_points = {
 'console_scripts' : [
 'main = jde_01_md_po_sched_line_delv.pipeline:main'], },
