fd05a804c7cf479ffb9865b134bdd2aa6277f549
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-20 21:00:53 +0000

    JEKT-296 🔁 Modified     - Pipeline        - SAP_MD_PO_HIST_BBA_BBL_BBN_MBP_MRS_TAI_P01_HMD_GEU_HCS

--- src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST/code/setup.py
+++ src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST/code/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'SAP_MD_PO_HIST',
+    name = 'SAP_MD_PO_HIST_BBA_BBL_BBN_MBP_MRS_TAI_P01_HMD_GEU_HCS',
     version = '1.0',
     packages = find_packages(include = ('dart_l1_purchase_order_md_po_hist*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
