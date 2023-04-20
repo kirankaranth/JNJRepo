ec2424c91b2565550946119924c05988f4893cd0
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-20 21:22:27 +0000

    JEKT-296 🔁 Modified     - Pipeline        - JDE_MD_PO_HIST_BW2_DEU_GMD_JEM_JES_JET_JSW_SJD

--- src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST_JDE/code/test/TestSuite.py
+++ src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST_JDE/code/test/TestSuite.py
@@ -1,6 +1,7 @@
 import unittest
 
-
+from test.dart_l1_purchase_order_md_po_hist.graph.test_SET_FIELD_ORDER_REFORMAT import *
+from test.dart_l1_purchase_order_md_po_hist.graph.test_NEW_FIELDS import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
