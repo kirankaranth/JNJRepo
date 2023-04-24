2a758262d427ec5f94b6f508ba288165a8b67b39
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-04-24 13:58:36 +0000

    JEKT-296 🔁 Modified     - Pipeline        - JDE_MD_PO_HIST_BW2_DEU_GMD_JEM_JES_JET_JSW_SJD     - Dataset        - MD_PO_HIST

--- src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST_JDE/code/test/TestSuite.py
+++ src/pipelines/DART_L1_Purchase_Order_MD_PO_HIST_JDE/code/test/TestSuite.py
@@ -1,7 +1,6 @@
 import unittest
 
 from test.dart_l1_purchase_order_md_po_hist.graph.test_SET_FIELD_ORDER_REFORMAT import *
-from test.dart_l1_purchase_order_md_po_hist.graph.test_NEW_FIELDS import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
