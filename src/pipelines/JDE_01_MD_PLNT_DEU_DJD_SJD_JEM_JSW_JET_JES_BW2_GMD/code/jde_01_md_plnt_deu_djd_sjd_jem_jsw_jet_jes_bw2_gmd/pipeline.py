52e7d53e3c6df8fafd7aeee9317fcd8fffa6442e
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-04-25 13:15:22 +0000

    JEKT-1341 Update Pipeline settings (set JOB sampling = IO) 🔁 Modified     - Pipeline        - SAP_01_MD_PLNT        - JDE_01_MD_PLNT_DEU_DJD_SJD_JEM_JSW_JET_JES_BW2_GMD        - JDE_02_MD_PLNT_MTR

--- src/pipelines/JDE_01_MD_PLNT_DEU_DJD_SJD_JEM_JSW_JET_JES_BW2_GMD/code/jde_01_md_plnt_deu_djd_sjd_jem_jsw_jet_jes_bw2_gmd/pipeline.py
+++ src/pipelines/JDE_01_MD_PLNT_DEU_DJD_SJD_JEM_JSW_JET_JES_BW2_GMD/code/jde_01_md_plnt_deu_djd_sjd_jem_jsw_jet_jes_bw2_gmd/pipeline.py
@@ -8,15 +8,45 @@ from jde_01_md_plnt_deu_djd_sjd_jem_jsw_jet_jes_bw2_gmd.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_JDE_F0116 = JDE_F0116(spark)
+    df_JDE_F0116 = collectMetrics(
+        spark, 
+        df_JDE_F0116, 
+        "graph", 
+        "NbDhgrZZOVotu_LgAcRUq$$qUg9PowtJa7-G65aFEy1b", 
+        "O_DQKOyEBnZfHrkQX_FOc$$0eQyIngIn3fmq1gm2QvL9"
+    )
     df_JDE_F0116_FILTER = JDE_F0116_FILTER(spark, df_JDE_F0116)
     df_JDE_F0006 = JDE_F0006(spark)
+    df_JDE_F0006 = collectMetrics(
+        spark, 
+        df_JDE_F0006, 
+        "graph", 
+        "UFITQd1H2EiaZZOJIuYQm$$wj2_8cIdgj0ToOFDRfS1Q", 
+        "q1yiwG83r7EbrIkavD8qL$$wRaCGs3nmvo8JeWl_xj_T"
+    )
     df_JDE_F0006_FILTER = JDE_F0006_FILTER(spark, df_JDE_F0006)
     df_Join_JDE = Join_JDE(spark, df_JDE_F0006_FILTER, df_JDE_F0116_FILTER)
     df_NEW_FIELDS_RENAME_FORMAT = NEW_FIELDS_RENAME_FORMAT(spark, df_Join_JDE)
     df_SET_FIELD_ORDER_REFORMAT = SET_FIELD_ORDER_REFORMAT(spark, df_NEW_FIELDS_RENAME_FORMAT)
+    df_SET_FIELD_ORDER_REFORMAT = collectMetrics(
+        spark, 
+        df_SET_FIELD_ORDER_REFORMAT, 
+        "graph", 
+        "YtyZow5aU6_oUIUrbCOny$$tXl75YElEb9WBf2yki1OF", 
+        "LJGMJ0y9HdVHL28DWn-QX$$5pcQIvezlxpfh-NlUo-DV"
+    )
     MD_PLNT(spark, df_SET_FIELD_ORDER_REFORMAT)
     df_DUPLICATE_CHECK = DUPLICATE_CHECK(spark, df_SET_FIELD_ORDER_REFORMAT)
     df_DUPLICATE_FILTER = DUPLICATE_FILTER(spark, df_DUPLICATE_CHECK)
+    df_DUPLICATE_FILTER = collectMetrics(
+        spark, 
+        df_DUPLICATE_FILTER, 
+        "graph", 
+        "5RV7zOz6Cz5SjaIvx94wS$$Qi5C7O_AMnfMcFlNRVmp_", 
+        "gT3k40mR7jB6-brvV9n6p$$ihFZPEcusDshPwqOnYT4z"
+    )
+    df_DUPLICATE_FILTER.cache().count()
+    df_DUPLICATE_FILTER.unpersist()
 
 def main():
     spark = SparkSession.builder\
@@ -27,6 +57,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/JDE_01_MD_PLNT_DEU_DJD_SJD_JEM_JSW_JET_JES_BW2_GMD")
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/JDE_01_MD_PLNT_DEU_DJD_SJD_JEM_JSW_JET_JES_BW2_GMD")
--- src/pipelines/SAP_01_MD_PLNT/code/sap_01_md_plnt/pipeline.py
+++ src/pipelines/SAP_01_MD_PLNT/code/sap_01_md_plnt/pipeline.py
@@ -8,17 +8,54 @@ from sap_01_md_plnt.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_SAP_T005T = SAP_T005T(spark)
+    df_SAP_T005T = collectMetrics(
+        spark, 
+        df_SAP_T005T, 
+        "graph", 
+        "Io2BnH_yCLHYgk_uMOGbq$$aYTDKU40XlGH4k6c3GQL4", 
+        "crYdTEGWB4IfUKd0mrZIy$$c-YM4B1CQj_GTPpqfBB79"
+    )
     df_MANDT_FILTER_T005T = MANDT_FILTER_T005T(spark, df_SAP_T005T)
     LU_SAP_T005T(spark, df_MANDT_FILTER_T005T)
     df_SAP_T001K = SAP_T001K(spark)
+    df_SAP_T001K = collectMetrics(
+        spark, 
+        df_SAP_T001K, 
+        "graph", 
+        "KFL4qV8chcd0_bL50n1W8$$swmBT0sSCrX11zrLcjuns", 
+        "lKTMJHsll_4OTkF04smty$$ts_vK1IUxW6D2ezDpYcq5"
+    )
     df_SAP_T001W = SAP_T001W(spark)
+    df_SAP_T001W = collectMetrics(
+        spark, 
+        df_SAP_T001W, 
+        "graph", 
+        "t5FhbUfGhh8JCGf16zK2J$$Cq5BB-Ysw8-zslL2mYPxO", 
+        "j1huw-d1AD66QXkMFaJ7p$$WSr7SvGSRbuPD3Ct6N18j"
+    )
     df_MANDT_FILTER_T001W = MANDT_FILTER_T001W(spark, df_SAP_T001W)
     df_MANDT_FILTER_T001K = MANDT_FILTER_T001K(spark, df_SAP_T001K)
     df_Join_1 = Join_1(spark, df_MANDT_FILTER_T001W, df_MANDT_FILTER_T001K)
     df_NEW_FIELDS_RENAME_FORMAT = NEW_FIELDS_RENAME_FORMAT(spark, df_Join_1)
     df_SET_FIELD_ORDER_REFORMAT = SET_FIELD_ORDER_REFORMAT(spark, df_NEW_FIELDS_RENAME_FORMAT)
+    df_SET_FIELD_ORDER_REFORMAT = collectMetrics(
+        spark, 
+        df_SET_FIELD_ORDER_REFORMAT, 
+        "graph", 
+        "JunzaeSEOTP42SedOc_xY$$wXUAm2qJF-rfLVdZScLFy", 
+        "oFAl0saCzvXJe0rgzYJje$$r1qD6ApQDZy2eqbkXrVci"
+    )
     df_DUPLICATE_CHECK = DUPLICATE_CHECK(spark, df_SET_FIELD_ORDER_REFORMAT)
     df_DUPLICATE_FILTER = DUPLICATE_FILTER(spark, df_DUPLICATE_CHECK)
+    df_DUPLICATE_FILTER = collectMetrics(
+        spark, 
+        df_DUPLICATE_FILTER, 
+        "graph", 
+        "l5AbQy3wTrDOQsBUkmmY-$$ptKHSYjt6lK5_M4AJcM8m", 
+        "VjOiP6WoDVjrKd5r2AFyQ$$aq1KoxD31YmEc7rA6C_Em"
+    )
+    df_DUPLICATE_FILTER.cache().count()
+    df_DUPLICATE_FILTER.unpersist()
     MD_PLNT(spark, df_SET_FIELD_ORDER_REFORMAT)
 
 def main():
@@ -30,6 +67,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/SAP_01_MD_PLNT")
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/SAP_01_MD_PLNT")
