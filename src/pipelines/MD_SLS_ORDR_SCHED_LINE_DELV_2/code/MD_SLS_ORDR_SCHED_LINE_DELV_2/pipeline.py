f55a0eeefcd76dd85e159667c85e0bf6426af6ae
fsano1@its.jnj.com <fsano1@its.jnj.com>
2023-04-27 22:28:46 +0000

    JEKT-1594 🔁 Modified     - Pipeline        - MD_SLS_ORDR_SCHED_LINE_DELV_2_p01     - Dataset        - MD_SLS_ORDR_SCHED_LINE_DELV

--- src/pipelines/MD_SLS_ORDR_SCHED_LINE_DELV_2/code/MD_SLS_ORDR_SCHED_LINE_DELV_2/pipeline.py
+++ src/pipelines/MD_SLS_ORDR_SCHED_LINE_DELV_2/code/MD_SLS_ORDR_SCHED_LINE_DELV_2/pipeline.py
@@ -9,6 +9,13 @@ from MD_SLS_ORDR_SCHED_LINE_DELV_2.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_sql_MD_SLS_ORDR_SCHED_LINE_DELV = sql_MD_SLS_ORDR_SCHED_LINE_DELV(spark)
     df_addL1fields = addL1fields(spark, df_sql_MD_SLS_ORDR_SCHED_LINE_DELV)
+    df_addL1fields = collectMetrics(
+        spark, 
+        df_addL1fields, 
+        "graph", 
+        "b493da30-5c0a-4320-805f-fa211586ca57", 
+        "a4983029-137c-41c0-9d12-7c5c72290ca0"
+    )
     MD_SLS_ORDR_SCHED_LINE_DELV(spark, df_addL1fields)
 
 def main():
@@ -20,6 +27,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/MD_SLS_ORDR_SCHED_LINE_DELV_2")
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/MD_SLS_ORDR_SCHED_LINE_DELV_2")
