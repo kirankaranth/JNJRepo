49233dfb3e3179387e77ebaac6e407b6aeace1c4
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-04-27 15:33:14 +0000

    JEKT-1395 ❇️ New     - Pipeline        - JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

---
+++ src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/pipeline.py
@@ -0,0 +1,34 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.config.ConfigStore import *
+from jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.udfs.UDFs import *
+from prophecy.utils import *
+from jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    pass
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set(
+        "prophecy.metadata.pipeline.uri",
+        "pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES"
+    )
+    
+    MetricsCollector.start(
+        spark = spark,
+        pipelineId = "pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES"
+    )
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
