0bbd53c803816c006838d4849a1e83aa415a7c21
smille29@its.jnj.com <smille29@its.jnj.com>
2023-04-27 17:16:52 +0000

    JEKT-186 🔁 Modified - Fixed Package Names     - Pipeline        - JDE_MD_MATL_LOC_MTR        - JDE_MD_MATL_LOC_JSW        - JDE_MD_MATL_LOC_DJD        - JDE_MD_MATL_LOC_DEU        - JDE_MD_MATL_LOC_JEM        - JDE_MD_MATL_LOC_BW2        - SAP_MD_MATL_LOC_BBA_BBL_BBN        - JDE_MD_MATL_LOC_JES        - JDE_MD_MATL_LOC_GMD        - JDE_MD_MATL_LOC_SJD        - SAP_MD_MATL_LOC_TAI_MRS_P01_HCS_BWI_GEU_SVS_MBP_FSN        - JDE_MD_MATL_LOC_JET        - SAP_MD_MATL_LOC_HMD

---
+++ src/pipelines/JDE_MD_MATL_LOC_DJD/code/jde_md_matl_loc_djd/graph/__init__.py
@@ -0,0 +1,5 @@
+from .Deduplicate_1 import Deduplicate_1
+from .MD_MATL_LOC import MD_MATL_LOC
+from .JDE_F4102 import JDE_F4102
+from .SET_FIELD_ORDER_REFORMAT import SET_FIELD_ORDER_REFORMAT
+from .NEW_FIELDS_PK import NEW_FIELDS_PK
---
+++ src/pipelines/JDE_MD_MATL_LOC_JEM/code/jde_md_matl_loc_jem/graph/MD_MATL_LOC.py
@@ -0,0 +1,13 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from jde_md_matl_loc_jem.config.ConfigStore import *
+from jde_md_matl_loc_jem.udfs.UDFs import *
+
+def MD_MATL_LOC(spark: SparkSession, in0: DataFrame):
+    in0.write\
+        .format("delta")\
+        .option("replaceWhere", f"SRC_SYS_CD = '{Config.sourceSystem}'")\
+        .mode("overwrite")\
+        .saveAsTable(f"{Config.targetSchema}.MD_MATL_LOC")
