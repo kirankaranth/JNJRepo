c99fe1c8e4f5dcb2806a72bbfb32e19bf110f2ce
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-04-27 12:48:21 +0000

    JEKT-402 Unit test re-added 🔁 Modified     - Pipeline        - JDE_MD_MATL_INV_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ        - MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ        - MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI

--- src/pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI/code/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/pipeline.py
+++ src/pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI/code/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai/pipeline.py
@@ -8,12 +8,33 @@ from md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai.graph i
 
 def pipeline(spark: SparkSession) -> None:
     df_DS_SAP_MARA_01 = DS_SAP_MARA_01(spark)
+    df_DS_SAP_MARA_01 = collectMetrics(
+        spark, 
+        df_DS_SAP_MARA_01, 
+        "graph", 
+        "61L0HnhccZW2gq1xdCSDI$$5LB3_g6JdscOYsQolGbBQ", 
+        "DTIlKMzDvq6FPJHIrBbud$$Vps9ShjmHDHlIzJf5Nk_h"
+    )
     df_MANDT1 = MANDT1(spark, df_DS_SAP_MARA_01)
     MEINS_LU(spark, df_MANDT1)
     df_DS_SAP_01_MARM = DS_SAP_01_MARM(spark)
+    df_DS_SAP_01_MARM = collectMetrics(
+        spark, 
+        df_DS_SAP_01_MARM, 
+        "graph", 
+        "taWajNbWifZpohozbS4St$$CxbiIVPI2kjSyw5BmEUjF", 
+        "y21rOpvx1-CugFPWmLB2T$$y4kQ_YJ_qRlhC4PV76-Wc"
+    )
     df_MANDT = MANDT(spark, df_DS_SAP_01_MARM)
     df_SchemaTransform_1 = SchemaTransform_1(spark, df_MANDT)
     df_SET_FIELDS = SET_FIELDS(spark, df_SchemaTransform_1)
+    df_SET_FIELDS = collectMetrics(
+        spark, 
+        df_SET_FIELDS, 
+        "graph", 
+        "LnMkjaXjwHhLl2OUtniJk$$kQU8TmqGjEn4rFOvwzxp1", 
+        "CXsK6Edgczs48PrXz0o_V$$qaKNGtqOJLjAxJSg1eRYA"
+    )
     MD_MATL_ALT_UOM(spark, df_SET_FIELDS)
 
 def main():
@@ -25,6 +46,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set(
         "prophecy.metadata.pipeline.uri",
         "pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI"
