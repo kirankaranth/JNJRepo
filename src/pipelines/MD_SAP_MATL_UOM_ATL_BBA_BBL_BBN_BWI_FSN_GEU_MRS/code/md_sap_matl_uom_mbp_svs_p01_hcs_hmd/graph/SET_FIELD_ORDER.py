d6dc5ac8c41d64651f7e8565b539a40989e3aaf9
YPatel8@its.jnj.com <YPatel8@its.jnj.com>
2023-05-01 18:05:19 +0000

    JEKT-1495 (New SAP Pipeline) ❇️ New     - Pipeline        - MD_SAP_MATL_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_MRS
    🔁 Modified
        - Dataset
           - MD_MATL_UOM

---
+++ src/pipelines/MD_SAP_MATL_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_MRS/code/md_sap_matl_uom_mbp_svs_p01_hcs_hmd/graph/SET_FIELD_ORDER.py
@@ -0,0 +1,46 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from md_sap_matl_uom_mbp_svs_p01_hcs_hmd.config.ConfigStore import *
+from md_sap_matl_uom_mbp_svs_p01_hcs_hmd.udfs.UDFs import *
+
+def SET_FIELD_ORDER(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.select(
+        col("SRC_SYS_CD"), 
+        col("UOM_CD"), 
+        col("EXTRNL_UOM_IN"), 
+        col("EXTRNL_UOM_ID"), 
+        col("DEC_PLACE_FOR_RD"), 
+        col("COMML_MSR_UNIT_ID"), 
+        col("VAL_BAS_CMMT_IN"), 
+        col("UNIT_IN_1"), 
+        col("UNIT_IN_2"), 
+        col("DIM_KEY"), 
+        col("NUMRTR_FOR_CONV_UNIT"), 
+        col("DENOM_FOR_CONV_UNIT"), 
+        col("BASE_TEN_EXP_FOR_CONV"), 
+        col("ADDTV_CNST_FOR_CONV"), 
+        col("EXP_OF_TEN_FOR_FLT_PT_FMT"), 
+        col("NUM_OF_DEC_PLACE_FOR_NUM_DSPLY"), 
+        col("ISO_CD_FOR_UOM"), 
+        col("SLCT_FLD_FOR_CONV_FROM_ISO_CD_TO_INT_CD"), 
+        col("TEMP"), 
+        col("TEMP_UNIT"), 
+        col("UOM_FMLY"), 
+        col("PRESR_VAL"), 
+        col("UNIT_OF_PRESR"), 
+        col("EXTRNL_UOM_COMML_FMT"), 
+        col("EXTRNL_UOM_TECH_FMT"), 
+        col("UOM_SHRT_TEXT"), 
+        col("UOM_LONG_TEXT"), 
+        col("DIM_TEXT"), 
+        col("DAI_ETL_ID"), 
+        col("DAI_CRT_DTTM"), 
+        col("DAI_UPDT_DTTM"), 
+        col("_l0_upt_"), 
+        col("_pk_"), 
+        col("_pk_md5_"), 
+        col("_l1_upt_"), 
+        col("_deleted_")
+    )
