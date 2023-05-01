d15b328f7e908e73364fc9ccf3c23b54901003b1
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-01 17:54:43 +0000

    JEKT-1351 🔁 Added duplicate checks     - Pipeline        - JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD

--- src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd/graph/DUPLICATE_CHECK.py
+++ src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd/graph/DUPLICATE_CHECK.py
@@ -5,5 +5,7 @@ from prophecy.libs import typed_lit
 from jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd.config.ConfigStore import *
 from jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd.udfs.UDFs import *
 
-def Aggregate_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.agg()
+def DUPLICATE_CHECK(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    df1 = in0.groupBy(col("SRC_SYS_CD"), col("CRNCY_CD"))
+
+    return df1.agg(count(lit(1)).alias("PK_COUNT"))
