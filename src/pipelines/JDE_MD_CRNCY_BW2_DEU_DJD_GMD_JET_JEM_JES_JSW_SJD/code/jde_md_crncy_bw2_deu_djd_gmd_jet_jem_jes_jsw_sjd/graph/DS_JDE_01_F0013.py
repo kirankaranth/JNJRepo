3bbd7429fd2f42b540f7e107b8ce9a671ffb3e45
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-01 18:01:19 +0000

    JEKT-1351 🔁 Ran all jobs adhoc     - Pipeline        - JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD     - Dataset        - DS_JDE_01_F0013

--- src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd/graph/DS_JDE_01_F0013.py
+++ src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd/graph/DS_JDE_01_F0013.py
@@ -6,4 +6,4 @@ from jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd.config.ConfigStore import 
 from jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd.udfs.UDFs import *
 
 def DS_JDE_01_F0013(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"bw2.f0013")
+    return spark.read.table(f"{Config.sourceDatabase}.f0013")
