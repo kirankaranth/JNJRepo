6cc6786d2c7826a0e23b9066d0f143d3257f61fb
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-01 14:02:08 +0000

    JEKT-1395 🔁 Modified     - Pipeline        - JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
+++ src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
@@ -11,10 +11,11 @@ class Config(ConfigBase):
             MANDT: str=None,
             DAI_ETL_ID: int=None,
             sourceTable: str=None,
+            ConfigDatabase: str=None,
             **kwargs
     ):
         self.spark = None
-        self.update(sourceSystem, sourceDatabase, targetSchema, MANDT, DAI_ETL_ID, sourceTable)
+        self.update(sourceSystem, sourceDatabase, targetSchema, MANDT, DAI_ETL_ID, sourceTable, ConfigDatabase)
 
     def update(
             self,
@@ -24,6 +25,7 @@ class Config(ConfigBase):
             MANDT: str="100",
             DAI_ETL_ID: int=0,
             sourceTable: str="F4201",
+            ConfigDatabase: str=" ",
             **kwargs
     ):
         prophecy_spark = self.spark
@@ -33,4 +35,5 @@ class Config(ConfigBase):
         self.MANDT = MANDT
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.sourceTable = sourceTable
+        self.ConfigDatabase = ConfigDatabase
         pass
