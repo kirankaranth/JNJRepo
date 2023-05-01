57882580ba87be192646a005fb2cbed9c743c9c7
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-01 12:54:09 +0000

    JEKT-1351 🔁 Created Join     - Pipeline        - SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2_HMD

--- src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd/pipeline.py
+++ src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd/pipeline.py
@@ -9,8 +9,9 @@ from sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_DS_SAP_01_TCURC = DS_SAP_01_TCURC(spark)
     df_DS_SAP_01_TCURX = DS_SAP_01_TCURX(spark)
-    df_MANDT_FILTER_TCURC = MANDT_FILTER_TCURC(spark, df_DS_SAP_01_TCURC)
     df_MANDT_FILTER_TCURX = MANDT_FILTER_TCURX(spark, df_DS_SAP_01_TCURX)
+    df_MANDT_FILTER_TCURC = MANDT_FILTER_TCURC(spark, df_DS_SAP_01_TCURC)
+    df_Join_1 = Join_1(spark, df_MANDT_FILTER_TCURC, df_MANDT_FILTER_TCURX)
 
 def main():
     spark = SparkSession.builder\
