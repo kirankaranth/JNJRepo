b9659c6d0a1c9e4a92529fa3afc3a5818e2fb976
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-02 12:30:29 +0000

    JEKT-1209 Updated filters and passed in variables for database name and table 🔁 Modified     - Pipeline        - JDE_MD_MATL     - Dataset        - DS_JDE_01_F0005_41

--- src/pipelines/JDE_MD_MATL/code/jde_md_matl/graph/F0005_41.py
+++ src/pipelines/JDE_MD_MATL/code/jde_md_matl/graph/F0005_41.py
@@ -6,4 +6,4 @@ from jde_md_matl.config.ConfigStore import *
 from jde_md_matl.udfs.UDFs import *
 
 def F0005_41(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM gmd.f0005 WHERE Trim(DRSY) = '41'")
+    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.{Config.DBTABLE1} WHERE Trim(DRSY) = '41'")
--- src/pipelines/JDE_MD_MATL/code/jde_md_matl/graph/MATL_GRP.py
+++ src/pipelines/JDE_MD_MATL/code/jde_md_matl/graph/MATL_GRP.py
@@ -6,4 +6,4 @@ from jde_md_matl.config.ConfigStore import *
 from jde_md_matl.udfs.UDFs import *
 
 def MATL_GRP(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(lit(True))
+    return in0.filter(((col("_deleted_") == lit("F")) & (trim(col("DRRT")) == lit(Config.MATL_GRP_FILTER))))
