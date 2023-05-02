3f74f976d1b6151dc3b777771734660b7d7dd918
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-02 15:42:15 +0000

    JEKT-1351 🔁 Completed Unit test     - Pipeline        - SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2_HMD

--- src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/test/TestSuite.py
+++ src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
