b1de5747acc6ec7607b062bfbb6df74d68188099
smille29@its.jnj.com <smille29@its.jnj.com>
2023-05-02 20:10:10 +0000

    JEKT-14 - ❌ Deleted - Delete Old Pipeline that was merged into on SAP Pipeline     - Pipeline        - SAP_MD_BOM_HDR_BBA_BBN_HCS_MRS

--- src/pipelines/SAP_MD_BOM_HDR_BBA_BBN_HCS_MRS/code/sap_md_bom_hdr_bba_bbn_hcs_mrs/graph/Filter_1.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_md_bom_hdr_bba_bbn_hcs_mrs.config.ConfigStore import *
-from sap_md_bom_hdr_bba_bbn_hcs_mrs.udfs.UDFs import *
-
-def Filter_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter((col("PK_COUNT") > lit(1)))
