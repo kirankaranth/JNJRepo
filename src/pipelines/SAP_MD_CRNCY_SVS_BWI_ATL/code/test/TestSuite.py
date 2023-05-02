bfb561bd7bdfb5a3df6aca543596e0bf6dd7ab5d
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-02 15:49:39 +0000

    JEKT-1351 🔁 Completed unit test     - Pipeline        - SAP_MD_CRNCY_SVS_BWI_ATL

--- src/pipelines/SAP_MD_CRNCY_SVS_BWI_ATL/code/test/TestSuite.py
+++ src/pipelines/SAP_MD_CRNCY_SVS_BWI_ATL/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.sap_md_crncy_svs_bwi_atl.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/SAP_MD_CRNCY_SVS_BWI_ATL/code/test/__init__.py
+++ src/pipelines/SAP_MD_CRNCY_SVS_BWI_ATL/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .sap_md_crncy_svs_bwi_atl import *
 from .conftest import *
 from .TestSuite import *
