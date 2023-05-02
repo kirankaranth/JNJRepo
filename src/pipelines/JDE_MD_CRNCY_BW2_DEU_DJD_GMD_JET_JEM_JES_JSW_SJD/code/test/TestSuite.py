b7ae9be18b27266a6b35a3092870bfa3af683729
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-02 15:57:12 +0000

    JEKT-1351 🔁 Completed unit testing     - Pipeline        - JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD

--- src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/test/TestSuite.py
+++ src/pipelines/JDE_MD_CRNCY_BW2_DEU_DJD_GMD_JET_JEM_JES_JSW_SJD/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_md_crncy_bw2_deu_djd_gmd_jet_jem_jes_jsw_sjd.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
