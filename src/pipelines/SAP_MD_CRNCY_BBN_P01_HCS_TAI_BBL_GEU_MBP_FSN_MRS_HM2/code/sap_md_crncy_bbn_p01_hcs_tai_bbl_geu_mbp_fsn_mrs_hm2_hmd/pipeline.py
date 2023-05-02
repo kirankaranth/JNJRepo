a8d2c933c6fd5c5d4b6af6b53e1711cf386cc47b
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-02 15:03:23 +0000

    JEKT-1351 🔁 Added new reformat gem before join     - Pipeline        - SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2_HMD        - SAP_MD_CRNCY_SVS_BWI_ATL     - Dataset        - MD_CRNCY

--- src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd/pipeline.py
+++ src/pipelines/SAP_MD_CRNCY_BBN_P01_HCS_TAI_BBL_GEU_MBP_FSN_MRS_HM2/code/sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd/pipeline.py
@@ -9,9 +9,11 @@ from sap_md_crncy_bbn_p01_hcs_tai_bbl_geu_mbp_fsn_mrs_hm2_hmd.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_DS_SAP_01_TCURC = DS_SAP_01_TCURC(spark)
     df_MANDT_FILTER_TCURC = MANDT_FILTER_TCURC(spark, df_DS_SAP_01_TCURC)
+    df_SET_FIELD_TCURC = SET_FIELD_TCURC(spark, df_MANDT_FILTER_TCURC)
     df_DS_SAP_01_TCURX = DS_SAP_01_TCURX(spark)
     df_MANDT_FILTER_TCURX = MANDT_FILTER_TCURX(spark, df_DS_SAP_01_TCURX)
-    df_Join_1 = Join_1(spark, df_MANDT_FILTER_TCURC, df_MANDT_FILTER_TCURX)
+    df_SET_FIELD_TCURX = SET_FIELD_TCURX(spark, df_MANDT_FILTER_TCURX)
+    df_Join_1 = Join_1(spark, df_SET_FIELD_TCURC, df_SET_FIELD_TCURX)
     df_NEW_FIELDS_RENAME_FORMAT = NEW_FIELDS_RENAME_FORMAT(spark, df_Join_1)
     df_SET_FIELD_ORDER_REFORMAT = SET_FIELD_ORDER_REFORMAT(spark, df_NEW_FIELDS_RENAME_FORMAT)
     df_DUPLICATE_CHECK = DUPLICATE_CHECK(spark, df_SET_FIELD_ORDER_REFORMAT)
