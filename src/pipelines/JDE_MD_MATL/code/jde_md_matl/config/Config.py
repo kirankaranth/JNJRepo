e1f0bdb03af0b31ee6f7a3d474dec3dbd4937146
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-03 10:01:58 +0000

    JEKT-1209 Added in Lookup fields to SELECT fields gem via expr(config) 🔁 Modified     - Pipeline        - JDE_MD_MATL

--- src/pipelines/JDE_MD_MATL/code/jde_md_matl/config/Config.py
+++ src/pipelines/JDE_MD_MATL/code/jde_md_matl/config/Config.py
@@ -13,12 +13,12 @@ class Config(ConfigBase):
             DBTABLE1: str=None,
             DAI_ETL_ID: int=None,
             BRAVO_MINOR_DESC_FILTER: str=None,
-            FRAN_CD_FILTER: str=None,
-            MATL_GRP_FILTER: str=None,
-            MATL_TYPE_DESC_FILTER: str=None,
-            F_C_LU_Field: str=None,
             B_M_LU_Field: str=None,
+            FRAN_CD_FILTER: str=None,
+            F_C_LU_Field: str=None,
+            MATL_GRP_FILTER: str=None,
             M_G_LU_Field: str=None,
+            MATL_TYPE_DESC_FILTER: str=None,
             M_T_D_LU_Field: str=None,
             **kwargs
     ):
@@ -32,12 +32,12 @@ class Config(ConfigBase):
             DBTABLE1, 
             DAI_ETL_ID, 
             BRAVO_MINOR_DESC_FILTER, 
-            FRAN_CD_FILTER, 
-            MATL_GRP_FILTER, 
-            MATL_TYPE_DESC_FILTER, 
-            F_C_LU_Field, 
             B_M_LU_Field, 
+            FRAN_CD_FILTER, 
+            F_C_LU_Field, 
+            MATL_GRP_FILTER, 
             M_G_LU_Field, 
+            MATL_TYPE_DESC_FILTER, 
             M_T_D_LU_Field
         )
 
@@ -51,12 +51,12 @@ class Config(ConfigBase):
             DBTABLE1: str="f0005",
             DAI_ETL_ID: int=0,
             BRAVO_MINOR_DESC_FILTER: str="Filter N/A",
-            FRAN_CD_FILTER: str="Filter N/A",
-            MATL_GRP_FILTER: str="Filter N/A",
-            MATL_TYPE_DESC_FILTER: str="Filter N/A",
-            F_C_LU_Field: str="Field N/A",
             B_M_LU_Field: str="Field N/A",
+            FRAN_CD_FILTER: str="Filter N/A",
+            F_C_LU_Field: str="Field N/A",
+            MATL_GRP_FILTER: str="Filter N/A",
             M_G_LU_Field: str="Field N/A",
+            MATL_TYPE_DESC_FILTER: str="Filter N/A",
             M_T_D_LU_Field: str="Field N/A",
             **kwargs
     ):
@@ -69,11 +69,11 @@ class Config(ConfigBase):
         self.DBTABLE1 = DBTABLE1
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.BRAVO_MINOR_DESC_FILTER = BRAVO_MINOR_DESC_FILTER
-        self.FRAN_CD_FILTER = FRAN_CD_FILTER
-        self.MATL_GRP_FILTER = MATL_GRP_FILTER
-        self.MATL_TYPE_DESC_FILTER = MATL_TYPE_DESC_FILTER
-        self.F_C_LU_Field = F_C_LU_Field
         self.B_M_LU_Field = B_M_LU_Field
+        self.FRAN_CD_FILTER = FRAN_CD_FILTER
+        self.F_C_LU_Field = F_C_LU_Field
+        self.MATL_GRP_FILTER = MATL_GRP_FILTER
         self.M_G_LU_Field = M_G_LU_Field
+        self.MATL_TYPE_DESC_FILTER = MATL_TYPE_DESC_FILTER
         self.M_T_D_LU_Field = M_T_D_LU_Field
         pass
