29d08426018ec62b4646ca020853bcad3f3593af
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-05-04 02:54:45 +0000

    JEKT-1367: JDE Pipeline development 🔁 Modified     - Pipeline        - JDE_01_MD_CUST

---
+++ src/pipelines/JDE_01_MD_CUST/code/jde_01_md_cust/graph/DEDUPLICATE.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from jde_01_md_cust.config.ConfigStore import *
+from jde_01_md_cust.udfs.UDFs import *
+
+def DEDUPLICATE(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.dropDuplicates(in0.columns)
