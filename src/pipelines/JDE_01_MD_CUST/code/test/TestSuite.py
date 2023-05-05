66c201af4bfce97c899c7220ab0c1aca2aa66b6a
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-05-05 01:27:17 +0000

    JEKT-1367 🔁 Modified     - Pipeline        - JDE_01_MD_CUST

--- src/pipelines/JDE_01_MD_CUST/code/test/TestSuite.py
+++ src/pipelines/JDE_01_MD_CUST/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_01_md_cust.graph.test_NEW_FIELDS_RENAME_FORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/JDE_01_MD_CUST/code/test/__init__.py
+++ src/pipelines/JDE_01_MD_CUST/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .jde_01_md_cust import *
 from .conftest import *
 from .TestSuite import *
