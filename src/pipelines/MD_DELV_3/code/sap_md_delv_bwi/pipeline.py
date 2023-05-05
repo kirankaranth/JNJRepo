0b23dad2d00b15bc469a4e0a552e067f7b3cdf42
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-05 14:14:59 +0000

    🔁 Modified Pipeline name and package, also config setting     - Pipeline        - SAP_MD_DELV_BWI

--- src/pipelines/MD_DELV_3/code/sap_md_delv_bwi/pipeline.py
+++ src/pipelines/MD_DELV_3/code/sap_md_delv_bwi/pipeline.py
@@ -1,14 +1,28 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from MD_DELV_3.config.ConfigStore import *
-from MD_DELV_3.udfs.UDFs import *
+from sap_md_delv_bwi.config.ConfigStore import *
+from sap_md_delv_bwi.udfs.UDFs import *
 from prophecy.utils import *
-from MD_DELV_3.graph import *
+from sap_md_delv_bwi.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_sql_MD_DELV = sql_MD_DELV(spark)
+    df_sql_MD_DELV = collectMetrics(
+        spark, 
+        df_sql_MD_DELV, 
+        "graph", 
+        "fc07c147-25e4-4855-b3fd-ab019eb84adf", 
+        "e47bfef6-68d5-445d-8f8b-fc04d670e080"
+    )
     df_addL1fields = addL1fields(spark, df_sql_MD_DELV)
+    df_addL1fields = collectMetrics(
+        spark, 
+        df_addL1fields, 
+        "graph", 
+        "9a12d2ef-b3ac-4f2b-be4e-e122071cf8de", 
+        "744bc30d-6065-4bee-b19c-72c2a4a87948"
+    )
     MD_DELV(spark, df_addL1fields)
 
 def main():
@@ -20,6 +34,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/MD_DELV_3")
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/MD_DELV_3")
