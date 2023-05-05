3caa1b9952b0bc0a65821cbbe20edba77723f948
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-05 17:12:08 +0000

    JEKT-1209 Created new pipeline due to missing t023t on HMD side. 🔁 Modified     - Pipeline        - SAP_MD_MATL_HMD        - JDE_BW2_MD_MATL     - Dataset        - MD_MATL     - Job        - MD_MATL_JOB
    ❇️ New
        - Pipeline
           - SAP_MD_MATL_HMD1

---
+++ src/pipelines/SAP_MD_MATL_HMD1/code/__init__.py
@@ -0,0 +1,1 @@
+from .sap_md_matl_hmd import *
---
+++ src/pipelines/SAP_MD_MATL_HMD1/code/sap_md_matl_hmd/graph/NDL_ALLOY.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from sap_md_matl_hmd.config.ConfigStore import *
+from sap_md_matl_hmd.udfs.UDFs import *
+
+def NDL_ALLOY(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    return in0.filter((col("cabn_filter") == lit("NDL_ALLOY")))
