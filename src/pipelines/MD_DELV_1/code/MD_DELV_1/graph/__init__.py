404483a54ac481abe7b5e579ca252523482d3784
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-05 13:56:20 +0000

    JEKT-1604 🔁 Modified Pipeline name and package name and config setting     - Pipeline        - SAP_MD_DELV_BBA_BBL_BBN_HCS_MRS        - MD_DELV_9_bw2_gmd_jsw_jet_mtr

--- src/pipelines/MD_DELV_1/code/MD_DELV_1/graph/__init__.py
+++
@@ -1,3 +0,0 @@
-from .addL1fields import addL1fields
-from .sql_MD_DELV import sql_MD_DELV
-from .MD_DELV import MD_DELV
--- src/pipelines/MD_DELV_1/code/sap_md_delv_bba_bbl_bbn_hcs_mrs/graph/sql_MD_DELV.py
+++ src/pipelines/MD_DELV_1/code/sap_md_delv_bba_bbl_bbn_hcs_mrs/graph/sql_MD_DELV.py
@@ -2,8 +2,8 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
-from MD_DELV_1.config.ConfigStore import *
-from MD_DELV_1.udfs.UDFs import *
+from sap_md_delv_bba_bbl_bbn_hcs_mrs.config.ConfigStore import *
+from sap_md_delv_bba_bbl_bbn_hcs_mrs.udfs.UDFs import *
 
 def sql_MD_DELV(spark: SparkSession) -> DataFrame:
     out0 = spark.sql(
