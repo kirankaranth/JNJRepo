1a902e38136ccbc414271431c170f170a865f066
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-05-05 14:26:30 +0000

    JEKT-1367 🔁 Modified     - Pipeline        - SAP_01_MD_CUST_HMD_HM2     - Dataset        - MD_CUST

--- src/pipelines/SAP_01_MD_SHIP_SVS/code/sap_03_md_cust_hmd_hm2/graph/DEDUPLICATE.py
+++ src/pipelines/SAP_01_MD_SHIP_SVS/code/sap_03_md_cust_hmd_hm2/graph/DEDUPLICATE.py
@@ -6,4 +6,14 @@ from sap_03_md_cust_hmd_hm2.config.ConfigStore import *
 from sap_03_md_cust_hmd_hm2.udfs.UDFs import *
 
 def DEDUPLICATE(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.dropDuplicates(in0.columns)
+    return in0\
+        .withColumn(
+          "row_number",
+          row_number()\
+            .over(Window\
+            .partitionBy("SRC_SYS_CD", "CUST_NUM")\
+            .orderBy(lit(1))\
+            .rowsBetween(Window.unboundedPreceding, Window.currentRow))
+        )\
+        .filter(col("row_number") == lit(1))\
+        .drop("row_number")
