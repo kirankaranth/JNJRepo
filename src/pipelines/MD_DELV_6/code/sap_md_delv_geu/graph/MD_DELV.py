df4de1c15cfd9e5513a1819c5480d20e969ada26
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-05 14:28:34 +0000

    JEKT-1604🔁 Modified pipelien name an dpackage     - Pipeline        - SAP_MD_DELV_GEU

--- src/pipelines/MD_DELV_6/code/sap_md_delv_geu/graph/MD_DELV.py
+++ src/pipelines/MD_DELV_6/code/sap_md_delv_geu/graph/MD_DELV.py
@@ -2,8 +2,8 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
-from MD_DELV_6.config.ConfigStore import *
-from MD_DELV_6.udfs.UDFs import *
+from sap_md_delv_geu.config.ConfigStore import *
+from sap_md_delv_geu.udfs.UDFs import *
 
 def MD_DELV(spark: SparkSession, in0: DataFrame):
     in0.write\
--- src/pipelines/MD_DELV_6/code/sap_md_delv_geu/pipeline.py
+++ src/pipelines/MD_DELV_6/code/sap_md_delv_geu/pipeline.py
@@ -1,14 +1,28 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from MD_DELV_6.config.ConfigStore import *
-from MD_DELV_6.udfs.UDFs import *
+from sap_md_delv_geu.config.ConfigStore import *
+from sap_md_delv_geu.udfs.UDFs import *
 from prophecy.utils import *
-from MD_DELV_6.graph import *
+from sap_md_delv_geu.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_sql_MD_DELV = sql_MD_DELV(spark)
+    df_sql_MD_DELV = collectMetrics(
+        spark, 
+        df_sql_MD_DELV, 
+        "graph", 
+        "730b8f1a-e47a-4cc2-87dc-667a456ef437", 
+        "8b66f31c-4d7c-4b84-b5aa-243eb0f9ce09"
+    )
     df_addL1fields = addL1fields(spark, df_sql_MD_DELV)
+    df_addL1fields = collectMetrics(
+        spark, 
+        df_addL1fields, 
+        "graph", 
+        "e07ba6be-3dcd-43d6-928f-d491a92144ae", 
+        "f916f7b0-67aa-4d9b-b140-404a4de82eb6"
+    )
     MD_DELV(spark, df_addL1fields)
 
 def main():
@@ -20,6 +34,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/MD_DELV_6")
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/MD_DELV_6")
