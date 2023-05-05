5e07477c7dd5ac2e02916fdea4dd1014d4c7eee1
smille29@its.jnj.com <smille29@its.jnj.com>
2023-05-05 20:12:31 +0000

    JEKT-1211-❇️ New - Added new Job for HM2 and updated Pipeline Names and Package Name     - Job        - JOB_MD_MATL_BOM_HM2
    🔁 Modified
        - Pipeline
           - SAP_MD_MATL_BOM_ATL_BBA_BBL_BBN_HCS_MBP_MRS_P01_SVS_BWI_FSN_TAI_GEU_HMD
           - JDE_MD_MATL_BOM_BW2_DEU_GMD_JEM_JES_JET_JSW_MTR_SJD
        - Job
           - JOB_MD_MATL_BOM

--- src/pipelines/SAP_MD_MATL_BOM/code/sap_md_matl_bom/graph/SAP_MAST.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_md_matl_bom.config.ConfigStore import *
-from sap_md_matl_bom.udfs.UDFs import *
-
-def SAP_MAST(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.mast")
