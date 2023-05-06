b246eec6b46df8c060e3d3f429d914d384b4f2c2
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-06 22:47:09 +0000

    JEKT-1341 Minor updates (Remove space for CRNCY_KEY in SchemaTransform Gem) 🔁 Modified     - Pipeline        - SAP_01_MD_PLNT     - Dataset        - MD_PLNT

--- src/pipelines/SAP_01_MD_PLNT/code/sap_01_md_plnt/graph/SET_FIELD_ORDER_REFORMAT.py
+++ src/pipelines/SAP_01_MD_PLNT/code/sap_01_md_plnt/graph/SET_FIELD_ORDER_REFORMAT.py
@@ -28,5 +28,5 @@ def SET_FIELD_ORDER_REFORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
         col("_pk_md5_"), 
         col("_l1_upt_"), 
         col("_deleted_"), 
-        col("`CRNCY_KEY   `").alias("CRNCY_KEY   ")
+        col("CRNCY_KEY")
     )
