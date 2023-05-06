0a0944e49711e2cada726ecc369fa2206410de67
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-06 10:09:19 +0000

    JEKT-1726 🔁 Modified     - Pipeline        - SAP_MD_MATL_VALUT

--- src/pipelines/SAP_MD_MATL_VALUT/code/sap_md_matl_valut/config/Config.py
+++ src/pipelines/SAP_MD_MATL_VALUT/code/sap_md_matl_valut/config/Config.py
@@ -3,14 +3,31 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, sourceDatabase: str=None, DBTABLE: str=None, DBTABLE1: str=None, MANDT: str=None, **kwargs):
+    def __init__(
+            self,
+            sourceDatabase: str=None,
+            DBTABLE: str=None,
+            DBTABLE1: str=None,
+            MANDT: str=None,
+            SRC_SYS_CD: str=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update(sourceDatabase, DBTABLE, DBTABLE1, MANDT)
+        self.update(sourceDatabase, DBTABLE, DBTABLE1, MANDT, SRC_SYS_CD)
 
-    def update(self, sourceDatabase: str="bba", DBTABLE: str="MBEW", DBTABLE1: str="MARA", MANDT: str="100", **kwargs):
+    def update(
+            self,
+            sourceDatabase: str="bba",
+            DBTABLE: str="MBEW",
+            DBTABLE1: str="MARA",
+            MANDT: str="100",
+            SRC_SYS_CD: str="bba",
+            **kwargs
+    ):
         prophecy_spark = self.spark
         self.sourceDatabase = sourceDatabase
         self.DBTABLE = DBTABLE
         self.DBTABLE1 = DBTABLE1
         self.MANDT = MANDT
+        self.SRC_SYS_CD = SRC_SYS_CD
         pass
