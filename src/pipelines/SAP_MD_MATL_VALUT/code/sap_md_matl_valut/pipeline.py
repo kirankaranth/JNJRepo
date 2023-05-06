5ab54cacf22a3e18185cb50cd748f0a49cc4a7f3
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-06 08:39:18 +0000

    JEKT-1726 Creation of new pipeline ❇️ New     - Pipeline        - SAP_MD_MATL_VALUT

---
+++ src/pipelines/SAP_MD_MATL_VALUT/code/sap_md_matl_valut/pipeline.py
@@ -0,0 +1,28 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from sap_md_matl_valut.config.ConfigStore import *
+from sap_md_matl_valut.udfs.UDFs import *
+from prophecy.utils import *
+from sap_md_matl_valut.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    pass
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/SAP_MD_MATL_VALUT")
+    
+    MetricsCollector.start(spark = spark, pipelineId = "pipelines/SAP_MD_MATL_VALUT")
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
