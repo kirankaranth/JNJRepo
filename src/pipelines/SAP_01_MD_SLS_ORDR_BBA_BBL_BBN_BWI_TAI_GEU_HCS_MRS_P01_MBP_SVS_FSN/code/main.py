98ff0b2be64283610f6e258c2666d0f60a231065
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-08 18:02:53 +0000

    JEKT-1395 ❇️ New     - Dataset        - DS_SAP_08_BVAK
    🔁 Modified
        - Pipeline
           - SAP_01_MD_SLS_ORDR_BBN_GEU_P01_MBP_SVS

--- src/pipelines/SAP_01_MD_SLS_ORDR_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN/code/main.py
+++ src/pipelines/SAP_01_MD_SLS_ORDR_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN/code/main.py
@@ -1,3 +1,3 @@
-from jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.pipeline import main
+from sap_01_md_sls_ordr_bbn_geu_p01_mbp_svs.pipeline import main
 
 main()
---
+++ src/pipelines/SAP_01_MD_SLS_ORDR_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN/code/sap_01_md_sls_ordr_bbn_geu_p01_mbp_svs/graph/JOIN_VBAK_VBUK.py
@@ -0,0 +1,11 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from sap_01_md_sls_ordr_bbn_geu_p01_mbp_svs.config.ConfigStore import *
+from sap_01_md_sls_ordr_bbn_geu_p01_mbp_svs.udfs.UDFs import *
+
+def JOIN_VBAK_VBUK(spark: SparkSession, in0: DataFrame, in1: DataFrame, ) -> DataFrame:
+    return in0\
+        .alias("in0")\
+        .join(in1.alias("in1"), (col("in0.SLS_ORDR_DOC_ID") == col("in1.SLS_ORDR_DOC_ID_VBUK")), "left_outer")
