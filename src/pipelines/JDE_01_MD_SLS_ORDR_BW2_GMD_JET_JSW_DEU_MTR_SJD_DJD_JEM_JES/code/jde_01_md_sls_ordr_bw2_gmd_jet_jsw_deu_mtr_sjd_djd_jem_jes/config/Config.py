f5ae5f9f2ee51f579adaa97b210173c73f734eec
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-08 20:35:35 +0000

    JEKT-1395 🔁 Modified     - Pipeline        - JDE_01_MD_SLS_ORDR_LINE_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR        - JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
+++ src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/config/Config.py
@@ -8,21 +8,19 @@ class Config(ConfigBase):
             sourceSystem: str=None,
             sourceDatabase: str=None,
             targetSchema: str=None,
-            MANDT: str=None,
             DAI_ETL_ID: int=None,
             sourceTable: str=None,
             ConfigDatabase: str=None,
             **kwargs
     ):
         self.spark = None
-        self.update(sourceSystem, sourceDatabase, targetSchema, MANDT, DAI_ETL_ID, sourceTable, ConfigDatabase)
+        self.update(sourceSystem, sourceDatabase, targetSchema, DAI_ETL_ID, sourceTable, ConfigDatabase)
 
     def update(
             self,
             sourceSystem: str="bw2",
             sourceDatabase: str="bw2",
             targetSchema: str="dev_md_l1",
-            MANDT: str="100",
             DAI_ETL_ID: int=0,
             sourceTable: str="F4201",
             ConfigDatabase: str=" ",
@@ -32,7 +30,6 @@ class Config(ConfigBase):
         self.sourceSystem = sourceSystem
         self.sourceDatabase = sourceDatabase
         self.targetSchema = targetSchema
-        self.MANDT = MANDT
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.sourceTable = sourceTable
         self.ConfigDatabase = ConfigDatabase
--- src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/pipeline.py
+++ src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/pipeline.py
@@ -8,11 +8,34 @@ from jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_JDE_F4201 = JDE_F4201(spark)
+    df_JDE_F4201 = collectMetrics(
+        spark, 
+        df_JDE_F4201, 
+        "graph", 
+        "oai-k6rsM3DaXL8zlH0gP$$4ma6w2BOf6zMU4wjWX7TZ", 
+        "PXwL7I3W4R6S5dTxhzhwA$$GTHn8zlGc59X8m7Dg0wJi"
+    )
     df_JDE_F4201_FILTER = JDE_F4201_FILTER(spark, df_JDE_F4201)
     df_NEW_FIELDS_TRANSFORMATION = NEW_FIELDS_TRANSFORMATION(spark, df_JDE_F4201_FILTER)
     df_SET_FIELD_ORDER_REFORMAT = SET_FIELD_ORDER_REFORMAT(spark, df_NEW_FIELDS_TRANSFORMATION)
+    df_SET_FIELD_ORDER_REFORMAT = collectMetrics(
+        spark, 
+        df_SET_FIELD_ORDER_REFORMAT, 
+        "graph", 
+        "fnuWA3iEP8yj0Wpmr7x7w$$RlB3rFyhHr8kKTVsFk6Jj", 
+        "mtvb5N4jIUORTT5rEKyuI$$G_MoZoeS_MwazxHSJUA4l"
+    )
     df_GET_DUP = GET_DUP(spark, df_SET_FIELD_ORDER_REFORMAT)
     df_DUP_FILTER = DUP_FILTER(spark, df_GET_DUP)
+    df_DUP_FILTER = collectMetrics(
+        spark, 
+        df_DUP_FILTER, 
+        "graph", 
+        "6Ox2NQn66uCJXu0tsLuTT$$OIhLh__wvgzXE0AXUcTi1", 
+        "-ZvjtQMEZsEHoq2pY6-mY$$AC5SgPju82x-bO3RoNp85"
+    )
+    df_DUP_FILTER.cache().count()
+    df_DUP_FILTER.unpersist()
     MD_SLS_ORDR(spark, df_SET_FIELD_ORDER_REFORMAT)
 
 def main():
@@ -24,6 +47,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set(
         "prophecy.metadata.pipeline.uri",
         "pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES"
