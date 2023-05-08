bb084df803672d891b806757357fcefb6dece16b
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-05-08 18:18:09 +0000

    JEKT-1783 Correct optimized pipelines 🔁 Modified     - Pipeline        - SAP_01_MD_SLS_ORDR_LINE_bbn        - SAP_MD_BILL_DOC_HDR_HMD        - SAP_01_MD_SLS_ORDR_LINE_hmd        - SAP_MD_MATL_INV_ATL     - Dataset        - MD_BILL_DOC_HDR        - MD_SLS_ORDR_LINE        - DS_SAP_01_VBRK     - Job        - ATL_PIPELINE_JOB

--- src/pipelines/SAP_01_MD_SLS_ORDR_LINE_bbn/code/sap_01_md_sls_ordr_line_bbn/pipeline.py
+++ src/pipelines/SAP_01_MD_SLS_ORDR_LINE_bbn/code/sap_01_md_sls_ordr_line_bbn/pipeline.py
@@ -96,15 +96,6 @@ def pipeline(spark: SparkSession) -> None:
         "-5sicrluGo4KP6YxY4ZfA$$8KntWIgxbxcywF5SdcD2h"
     )
     df_MANDT_FILTER = MANDT_FILTER(spark, df_SAP_VBAP)
-    df_SAP_VBAK = SAP_VBAK(spark)
-    df_SAP_VBAK = collectMetrics(
-        spark, 
-        df_SAP_VBAK, 
-        "graph", 
-        "V5RlhzURZ3AovBH4EtGoR$$RzAs4L5PxhIDJaZn_M0iF", 
-        "sjH_LLGBfigv0q-MWiqp1$$G22L17gIO1yk7wrAFjV7H"
-    )
-    df_MANDT_FILTER_1 = MANDT_FILTER_1(spark, df_SAP_VBAK)
     df_SAP_TVST = SAP_TVST(spark)
     df_SAP_TVST = collectMetrics(
         spark, 
@@ -115,7 +106,27 @@ def pipeline(spark: SparkSession) -> None:
     )
     df_MANDT_FILTER_1_1_1_2_1_1_1_1_1_1 = MANDT_FILTER_1_1_1_2_1_1_1_1_1_1(spark, df_SAP_TVST)
     df_SELECT_TVST = SELECT_TVST(spark, df_MANDT_FILTER_1_1_1_2_1_1_1_1_1_1)
-    df_Join_1 = Join_1(spark, df_MANDT_FILTER, df_MANDT_FILTER_1, df_SELECT_TVST)
+    df_SAP_VBAK = SAP_VBAK(spark)
+    df_SAP_VBAK = collectMetrics(
+        spark, 
+        df_SAP_VBAK, 
+        "graph", 
+        "V5RlhzURZ3AovBH4EtGoR$$RzAs4L5PxhIDJaZn_M0iF", 
+        "sjH_LLGBfigv0q-MWiqp1$$G22L17gIO1yk7wrAFjV7H"
+    )
+    df_MANDT_FILTER_1 = MANDT_FILTER_1(spark, df_SAP_VBAK)
+    df_SELECT_VBAK = SELECT_VBAK(spark, df_MANDT_FILTER_1)
+    df_SAP_VBKD = SAP_VBKD(spark)
+    df_SAP_VBKD = collectMetrics(
+        spark, 
+        df_SAP_VBKD, 
+        "graph", 
+        "-IVLYiizxuJsVaNAApNn-$$FUWMauCCyRzCP3mv6bnws", 
+        "Vrb1zO3XQx8eG-gwIAYs3$$l__PwkEUPq7stwQbww9t3"
+    )
+    df_MANDT_FILTER_1_1_1 = MANDT_FILTER_1_1_1(spark, df_SAP_VBKD)
+    df_SELECT_VBKD = SELECT_VBKD(spark, df_MANDT_FILTER_1_1_1)
+    df_Join_1 = Join_1(spark, df_MANDT_FILTER, df_SELECT_VBAK, df_SELECT_VBKD, df_SELECT_TVST)
     df_NEW_FIEDS = NEW_FIEDS(spark, df_Join_1)
     df_NEW_FIEDS = collectMetrics(
         spark, 
@@ -136,35 +147,6 @@ def pipeline(spark: SparkSession) -> None:
     )
     df_DUPLICATE_CHECK_1.cache().count()
     df_DUPLICATE_CHECK_1.unpersist()
-    df_SAP_VBKD = SAP_VBKD(spark)
-    df_SAP_VBKD = collectMetrics(
-        spark, 
-        df_SAP_VBKD, 
-        "graph", 
-        "-IVLYiizxuJsVaNAApNn-$$FUWMauCCyRzCP3mv6bnws", 
-        "Vrb1zO3XQx8eG-gwIAYs3$$l__PwkEUPq7stwQbww9t3"
-    )
-    df_MANDT_FILTER_1_1_1 = MANDT_FILTER_1_1_1(spark, df_SAP_VBKD)
-    df_SELECT_VBKD = SELECT_VBKD(spark, df_MANDT_FILTER_1_1_1)
-    df_SELECT_VBKD = collectMetrics(
-        spark, 
-        df_SELECT_VBKD, 
-        "graph", 
-        "ZwkQnOgm-UlW3ESJ0VdQ1$$RmXjJgXF0FSljfNdtHaq_", 
-        "ViAkYsdhb7d0wv0sA8SEC$$QuOWJd2HuBUFzzXB-Jwz6"
-    )
-    df_SELECT_VBKD.cache().count()
-    df_SELECT_VBKD.unpersist()
-    df_SELECT_VBAK = SELECT_VBAK(spark)
-    df_SELECT_VBAK = collectMetrics(
-        spark, 
-        df_SELECT_VBAK, 
-        "graph", 
-        "BmeOuAZTeBhotqZ1jJndD$$saZ3wm7Kq87BXZNp2xlhQ", 
-        "Rhph_FxEqyJdGR2KauQLO$$l6JcnrbKptU-MhrhhoR_Q"
-    )
-    df_SELECT_VBAK.cache().count()
-    df_SELECT_VBAK.unpersist()
 
 def main():
     spark = SparkSession.builder\
--- src/pipelines/SAP_MD_MATL_INV_ATL/code/sap_md_matl_inv_atl/pipeline.py
+++ src/pipelines/SAP_MD_MATL_INV_ATL/code/sap_md_matl_inv_atl/pipeline.py
@@ -7,7 +7,17 @@ from prophecy.utils import *
 from sap_md_matl_inv_atl.graph import *
 
 def pipeline(spark: SparkSession) -> None:
-    Lookup_1_MARA(spark)
+    df_DS_SAP_0000006_MARA = DS_SAP_0000006_MARA(spark)
+    df_DS_SAP_0000006_MARA = collectMetrics(
+        spark, 
+        df_DS_SAP_0000006_MARA, 
+        "graph", 
+        "IJ5NuzifNlRHw1_S3acvn$$XZiimepA2pHCz3CG1qyUi", 
+        "MybdmUVzs15xSdR_1-D_J$$odQZABeQQyezVnDG0iPmv"
+    )
+    df_MANDT_6 = MANDT_6(spark, df_DS_SAP_0000006_MARA)
+    df_SELECT_MARA = SELECT_MARA(spark, df_MANDT_6)
+    Lookup_1_MARA(spark, df_SELECT_MARA)
     df_DS_SAP_0000002_MCHB = DS_SAP_0000002_MCHB(spark)
     df_DS_SAP_0000002_MCHB = collectMetrics(
         spark, 
@@ -26,15 +36,6 @@ def pipeline(spark: SparkSession) -> None:
     )
     df_MANDT_5 = MANDT_5(spark, df_DS_SAP_0000005_MSSL)
     df_SELECT_MSSL = SELECT_MSSL(spark, df_MANDT_5)
-    df_SELECT_MSSL = collectMetrics(
-        spark, 
-        df_SELECT_MSSL, 
-        "graph", 
-        "exKYGwFOvd_sVIUyQ5R9U$$pCKG18pVTFT4_h1yMPO5B", 
-        "qwRowAdJ26_jNGHi16BHo$$QspaObCKlpv5kTi5ByOBj"
-    )
-    df_SELECT_MSSL.cache().count()
-    df_SELECT_MSSL.unpersist()
     df_DS_SAP_0000004_MSLB = DS_SAP_0000004_MSLB(spark)
     df_DS_SAP_0000004_MSLB = collectMetrics(
         spark, 
@@ -44,25 +45,37 @@ def pipeline(spark: SparkSession) -> None:
         "8nL-3lUELkgn4haw_OD2h$$iFm3V3TPeEyeJWmb7fan4"
     )
     df_MANDT_4 = MANDT_4(spark, df_DS_SAP_0000004_MSLB)
-    df_SchemaTransform_2_MCHB = SchemaTransform_2_MCHB(spark)
-    df_SET_FIELD_ORDER_2 = SET_FIELD_ORDER_2(spark, df_SchemaTransform_2_MCHB)
     df_MANDT_2 = MANDT_2(spark, df_DS_SAP_0000002_MCHB)
     df_Select_MCHB = Select_MCHB(spark, df_MANDT_2)
-    df_Select_MCHB = collectMetrics(
+    df_SchemaTransform_2_MCHB = SchemaTransform_2_MCHB(spark, df_Select_MCHB)
+    df_SET_FIELD_ORDER_2 = SET_FIELD_ORDER_2(spark, df_SchemaTransform_2_MCHB)
+    df_DS_SAP_0000003_MKOL = DS_SAP_0000003_MKOL(spark)
+    df_DS_SAP_0000003_MKOL = collectMetrics(
         spark, 
-        df_Select_MCHB, 
+        df_DS_SAP_0000003_MKOL, 
         "graph", 
-        "5dvQ2TzbtdkaTHiIfSJcd$$3uqr72vWHuk-3XibRC5OG", 
-        "RGbf_zbObduQDfvFeK7wg$$TeVVxYz0pPmjBI_0okVQS"
+        "QtliVpMYiwU9jss57BUIB$$0pqUECXCWnZO69-LTkizA", 
+        "b_x4_YkQB3KV-r_kyQWZG$$dsxZOaYupJMRTut5U0pae"
     )
-    df_Select_MCHB.cache().count()
-    df_Select_MCHB.unpersist()
-    df_SchemaTransform_3_MKOL = SchemaTransform_3_MKOL(spark)
+    df_MANDT_3 = MANDT_3(spark, df_DS_SAP_0000003_MKOL)
+    df_Select_MKOL = Select_MKOL(spark, df_MANDT_3)
+    df_SchemaTransform_3_MKOL = SchemaTransform_3_MKOL(spark, df_Select_MKOL)
     df_SET_FIELD_ORDER_3 = SET_FIELD_ORDER_3(spark, df_SchemaTransform_3_MKOL)
-    df_SchemaTransform_5_MSSL = SchemaTransform_5_MSSL(spark)
+    df_SchemaTransform_5_MSSL = SchemaTransform_5_MSSL(spark, df_SELECT_MSSL)
     df_SET_FIELD_ORDER_5 = SET_FIELD_ORDER_5(spark, df_SchemaTransform_5_MSSL)
-    df_SchemaTransform_4_MSLB = SchemaTransform_4_MSLB(spark)
-    df_SchemaTransform_1_MARD = SchemaTransform_1_MARD(spark)
+    df_SELECT_MSLB = SELECT_MSLB(spark, df_MANDT_4)
+    df_SchemaTransform_4_MSLB = SchemaTransform_4_MSLB(spark, df_SELECT_MSLB)
+    df_DS_SAP_0000001_MARD = DS_SAP_0000001_MARD(spark)
+    df_DS_SAP_0000001_MARD = collectMetrics(
+        spark, 
+        df_DS_SAP_0000001_MARD, 
+        "graph", 
+        "N3kaCSsahMsUWknQ9M67x$$i8bk6KYGJ_gbIcvNKTL3w", 
+        "zbp2a-qKqL5YcabsPqTSS$$-_mucTqkssRXA3cW606RA"
+    )
+    df_MANDT_1 = MANDT_1(spark, df_DS_SAP_0000001_MARD)
+    df_Select_MARD_Columns = Select_MARD_Columns(spark, df_MANDT_1)
+    df_SchemaTransform_1_MARD = SchemaTransform_1_MARD(spark, df_Select_MARD_Columns)
     df_SET_FIELD_ORDER_1 = SET_FIELD_ORDER_1(spark, df_SchemaTransform_1_MARD)
     df_SET_FIELD_ORDER_4 = SET_FIELD_ORDER_4(spark, df_SchemaTransform_4_MSLB)
     df_SetOperation_1_Union = SetOperation_1_Union(
@@ -80,74 +93,7 @@ def pipeline(spark: SparkSession) -> None:
         "xKifw-NoXimEOSoY6rBDY$$2DHwRrQJVUoIg5RoTtBRa", 
         "LLCYFN1HuOWzuV3az61i9$$nU1QMY1wYgK_Kngg5TvUV"
     )
-    df_DS_SAP_0000003_MKOL = DS_SAP_0000003_MKOL(spark)
-    df_DS_SAP_0000003_MKOL = collectMetrics(
-        spark, 
-        df_DS_SAP_0000003_MKOL, 
-        "graph", 
-        "QtliVpMYiwU9jss57BUIB$$0pqUECXCWnZO69-LTkizA", 
-        "b_x4_YkQB3KV-r_kyQWZG$$dsxZOaYupJMRTut5U0pae"
-    )
-    df_MANDT_3 = MANDT_3(spark, df_DS_SAP_0000003_MKOL)
-    df_Select_MKOL = Select_MKOL(spark, df_MANDT_3)
-    df_Select_MKOL = collectMetrics(
-        spark, 
-        df_Select_MKOL, 
-        "graph", 
-        "jlOBPCB0sgdxKwJQd7lxC$$n43JJqT6nwTFshL8SUmi7", 
-        "QCoj9ze5FLDyRJPmCpAGA$$q0KBEJGCcNB34_-0VvZCM"
-    )
-    df_Select_MKOL.cache().count()
-    df_Select_MKOL.unpersist()
-    df_DS_SAP_0000006_MARA = DS_SAP_0000006_MARA(spark)
-    df_DS_SAP_0000006_MARA = collectMetrics(
-        spark, 
-        df_DS_SAP_0000006_MARA, 
-        "graph", 
-        "IJ5NuzifNlRHw1_S3acvn$$XZiimepA2pHCz3CG1qyUi", 
-        "MybdmUVzs15xSdR_1-D_J$$odQZABeQQyezVnDG0iPmv"
-    )
-    df_MANDT_6 = MANDT_6(spark, df_DS_SAP_0000006_MARA)
     MD_MATL_INV(spark, df_SetOperation_1_Union)
-    df_DS_SAP_0000001_MARD = DS_SAP_0000001_MARD(spark)
-    df_DS_SAP_0000001_MARD = collectMetrics(
-        spark, 
-        df_DS_SAP_0000001_MARD, 
-        "graph", 
-        "N3kaCSsahMsUWknQ9M67x$$i8bk6KYGJ_gbIcvNKTL3w", 
-        "zbp2a-qKqL5YcabsPqTSS$$-_mucTqkssRXA3cW606RA"
-    )
-    df_MANDT_1 = MANDT_1(spark, df_DS_SAP_0000001_MARD)
-    df_Select_MARD_Columns = Select_MARD_Columns(spark, df_MANDT_1)
-    df_Select_MARD_Columns = collectMetrics(
-        spark, 
-        df_Select_MARD_Columns, 
-        "graph", 
-        "FuZn1W-qoeHIkm1TG7SBa$$mATcsgtaPyEERcBA9GVp_", 
-        "18jFw_-0sHU2xRvLY3yCH$$dmVYJebdw6zsbYAqEXKqJ"
-    )
-    df_Select_MARD_Columns.cache().count()
-    df_Select_MARD_Columns.unpersist()
-    df_SELECT_MARA = SELECT_MARA(spark, df_MANDT_6)
-    df_SELECT_MARA = collectMetrics(
-        spark, 
-        df_SELECT_MARA, 
-        "graph", 
-        "w3ys13DyozKiR5ptDEo8t$$wt1wG8sSqdAJIzxja4FZy", 
-        "nqHm-Y1OlkMWzi8eQfmmo$$KJ3lbojCTAsTzpjxrD4BH"
-    )
-    df_SELECT_MARA.cache().count()
-    df_SELECT_MARA.unpersist()
-    df_SELECT_MSLB = SELECT_MSLB(spark, df_MANDT_4)
-    df_SELECT_MSLB = collectMetrics(
-        spark, 
-        df_SELECT_MSLB, 
-        "graph", 
-        "RCjM0fZ4xSWaDuqwy5Ps3$$QiRopmqNgLedN-B18l0No", 
-        "RdaHbGjUTrbFVmoOVoAd3$$H501nEhc6tv0HRizGe5AB"
-    )
-    df_SELECT_MSLB.cache().count()
-    df_SELECT_MSLB.unpersist()
 
 def main():
     spark = SparkSession.builder\
