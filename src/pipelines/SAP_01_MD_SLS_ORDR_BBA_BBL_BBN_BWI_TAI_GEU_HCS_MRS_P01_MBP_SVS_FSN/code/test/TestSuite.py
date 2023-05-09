7b9d5fc5626d9fb4ab76bbac8a67f4eb53168e29
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-09 12:53:05 +0000

    JEKT-1395 🔁 Modified     - Pipeline        - SAP_01_MD_SLS_ORDR_HMD        - JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES        - SAP_01_MD_SLS_ORDR_BBN_GEU_P01_MBP_SVS     - Job        - JOB_MD_SLS_ORDR        - JOB_MD_SLS_ORDR_HM2

--- src/pipelines/SAP_01_MD_SLS_ORDR_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN/code/test/TestSuite.py
+++ src/pipelines/SAP_01_MD_SLS_ORDR_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.sap_01_md_sls_ordr_bbn_geu_p01_mbp_svs.graph.test_NEW_FIELDS_TRANSFORMATION import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
