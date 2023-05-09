18e790f32a5bf621038297d921a63094da3b43b7
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-09 18:25:43 +0000

    JEKT-1395 🔁 Modified     - Pipeline        - JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/test/TestSuite.py
+++ src/pipelines/JDE_01_MD_SLS_ORDR_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_01_md_sls_ordr_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.graph.test_NEW_FIELDS_TRANSFORMATION import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
