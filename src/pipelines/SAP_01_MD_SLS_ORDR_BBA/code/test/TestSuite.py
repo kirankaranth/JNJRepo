8da5de05f7d59d2da4a69ff50db4819d06125a17
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-09 17:10:22 +0000

    JEKT-1395 🔁 Modified     - Pipeline        - SAP_01_MD_SLS_ORDR_BBA_MRS_FSN

--- src/pipelines/SAP_01_MD_SLS_ORDR_BBA/code/test/TestSuite.py
+++ src/pipelines/SAP_01_MD_SLS_ORDR_BBA/code/test/TestSuite.py
@@ -1,6 +1,7 @@
 import unittest
 
-
+from test.sap_01_md_sls_ordr_bba_mrs_fsn.graph.test_NEW_FIELDS_TRANSFORMATION_1 import *
+from test.sap_01_md_sls_ordr_bba_mrs_fsn.graph.test_NEW_FIELDS_TRANSFORMATION import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
