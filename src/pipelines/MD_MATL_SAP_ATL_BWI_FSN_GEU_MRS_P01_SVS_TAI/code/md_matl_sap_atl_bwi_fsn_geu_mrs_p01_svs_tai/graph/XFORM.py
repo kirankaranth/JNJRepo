fac621ed38cbb44fec0df4d2b122b6974be9acc5
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-09 16:55:34 +0000

    JEKT-1209 Corrected SHRT_MATL_NUM issue picked up in testing 🔁 Modified     - Pipeline        - SAP_MD_MATL_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI     - Dataset        - MD_MATL

--- src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/XFORM.py
+++ src/pipelines/MD_MATL_SAP_ATL_BWI_FSN_GEU_MRS_P01_SVS_TAI/code/md_matl_sap_atl_bwi_fsn_geu_mrs_p01_svs_tai/graph/XFORM.py
@@ -100,4 +100,10 @@ def XFORM(spark: SparkSession, in0: DataFrame) -> DataFrame:
         .withColumn("_deleted_L1", lit("F"))\
         .withColumn("MATL_TYPE_DESC", lookup("MTBEZ_LU", col("MTART")).getField("MTBEZ"))\
         .withColumn("MATL_GRP_DESC", lookup("WGBEZx_LU", col("MATKL")).getField("WGBEZ"))\
-        .withColumn("MATL_GRP_DESC_2", lookup("WGBEZx_LU", col("MATKL")).getField("WGBEZ60"))
+        .withColumn("MATL_GRP_DESC_2", lookup("WGBEZx_LU", col("MATKL")).getField("WGBEZ60"))\
+        .withColumn(
+        "SHRT_MATL_NUM",
+        lit(
+          "#"
+        )
+    )
