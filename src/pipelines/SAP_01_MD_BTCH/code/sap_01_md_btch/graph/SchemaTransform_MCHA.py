2542a4a78f38ad7c5a6c0b72620280dce272866c
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-09 13:19:17 +0000

    JEKT-1735 SchemaTransform #1 in progress 🔁 Modified     - Pipeline        - SAP_01_MD_BTCH

--- src/pipelines/SAP_01_MD_BTCH/code/sap_01_md_btch/graph/SchemaTransform_MCHA.py
+++ src/pipelines/SAP_01_MD_BTCH/code/sap_01_md_btch/graph/SchemaTransform_MCHA.py
@@ -6,4 +6,77 @@ from sap_01_md_btch.config.ConfigStore import *
 from sap_01_md_btch.udfs.UDFs import *
 
 def SchemaTransform_MCHA(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.withColumn("SRC_SYS_CD", lit("sourceSystem")).withColumn("SRC_TBL_NM", lit("DBTable1"))
+    return in0\
+        .withColumn("SRC_SYS_CD", lit(Config.sourceSystem))\
+        .withColumn("SRC_TBL_NM", lit(Config.DBTable1))\
+        .withColumn("MATL_NUM", col("MATNR"))\
+        .withColumn("BTCH_NUM", col("CHARG"))\
+        .withColumn(
+          "PLNT_CD",
+          coalesce(
+            col("WERKS"), 
+            lit(
+              "#"
+            )
+          )
+        )\
+        .withColumn("DEL_IND", col("LVORM"))\
+        .withColumn(
+          "CRT_DTTM",
+          when((col("ersda") == lit("00000000")), lit(None))\
+            .when((length(col("ersda")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("ersda"), "yyyyMMdd"))
+        )\
+        .withColumn(
+          "CHG_DTTM",
+          when((col("laeda") == lit("00000000")), lit(None))\
+            .when((length(col("laeda")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("laeda"), "yyyyMMdd"))
+        )\
+        .withColumn(
+          "AVAIL_DTTM",
+          when((col("verab") == lit("00000000")), lit(None))\
+            .when((length(col("verab")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("verab"), "yyyyMMdd"))
+        )\
+        .withColumn(
+          "BTCH_EXP_DTTM",
+          when((col("vfdat") == lit("00000000")), lit(None))\
+            .when((length(col("vfdat")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("vfdat"), "yyyyMMdd"))
+        )\
+        .withColumn("BTCH_STS_CD", col("ZUSTD"))\
+        .withColumn(
+          "BTCH_LAST_STS_DTTM",
+          when((col("zaedt") == lit("00000000")), lit(None))\
+            .when((length(col("zaedt")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("zaedt"), "yyyyMMdd"))
+        )\
+        .withColumn("SUP_NUM", col("LIFNR"))\
+        .withColumn("SUP_BTCH_NUM", col("LICHA"))\
+        .withColumn(
+          "BTCH_LAST_GR_DTTM",
+          when((col("lwedt") == lit("00000000")), lit(None))\
+            .when((length(col("lwedt")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("lwedt"), "yyyyMMdd"))
+        )\
+        .withColumn(
+          "BTCH_MFG_DTTM",
+          when((col("hsdat") == lit("00000000")), lit(None))\
+            .when((length(col("hsdat")) < lit(8)), lit(None))\
+            .otherwise(to_timestamp(col("hsdat"), "yyyyMMdd"))
+        )\
+        .withColumn("BTCH_TYPE", lit(None).cast(StringType()))\
+        .withColumn("SUI_IND", lit(None).cast(StringType()))\
+        .withColumn("LOT_GRADE", lit(None).cast(StringType()))\
+        .withColumn("PARENT_CODE", lit(None).cast(StringType()))\
+        .withColumn(
+          "SHRT_MATL_NUM",
+          lit(
+              "#"
+            )\
+            .cast(StringType())
+        )\
+        .withColumn("DAI_ETL_ID", lit(Config.DAI_ETL_ID))\
+        .withColumn("DAI_CRT_DTTM", current_timestamp())\
+        .withColumn("DAI_UPDT_DTTM", current_timestamp())
