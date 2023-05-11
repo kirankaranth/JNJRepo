dc3a233c2e190d02976c35fc38983fa8a3d56ea8
fsano1@its.jnj.com <fsano1@its.jnj.com>
2023-05-11 12:30:25 +0000

    JEKT-1604 🔁 Modified     - Pipeline        - SAP_MD_DELV_HMD

--- src/pipelines/MD_DELV_10/code/setup.py
+++ src/pipelines/MD_DELV_10/code/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'SAP_MD_DELV_HMD',
+    name = 'MD_DELV_10_hm2',
     version = '1.0',
     packages = find_packages(include = ('sap_md_delv_hmd*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
