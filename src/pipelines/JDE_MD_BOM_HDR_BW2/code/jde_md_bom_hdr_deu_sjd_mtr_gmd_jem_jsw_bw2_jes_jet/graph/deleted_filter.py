83aa71afd471c495a234abc2c112197cd4a6f278
smille29@its.jnj.com <smille29@its.jnj.com>
2023-05-11 20:20:21 +0000

    JEKT-14 🔁 Modified     - Pipeline        - SAP_MD_BOM_HDR_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI_HMD        - JDE_MD_BOM_HDR_DEU_SJD_MTR_GMD_JEM_JSW_BW2_JES_JET

--- src/pipelines/JDE_MD_BOM_HDR_BW2/code/jde_md_bom_hdr_deu_sjd_mtr_gmd_jem_jsw_bw2_jes_jet/graph/deleted_filter.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from jde_md_bom_hdr_deu_sjd_mtr_gmd_jem_jsw_bw2_jes_jet.config.ConfigStore import *
-from jde_md_bom_hdr_deu_sjd_mtr_gmd_jem_jsw_bw2_jes_jet.udfs.UDFs import *
-
-def deleted_filter(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter((col("_deleted_") == lit("F")))
