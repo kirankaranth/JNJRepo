dfc99d276d7bbba759eb25e1ad25439ba9da3655
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-12 15:05:56 +0000

    JEKT-1735 Unit Test for JDE pipeline created 🔁 Modified     - Pipeline        - JDE_01_MD_BTCH_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_01_MD_BTCH/code/test/TestSuite.py
+++ src/pipelines/JDE_01_MD_BTCH/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.graph.test_NEW_FIELDS import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
