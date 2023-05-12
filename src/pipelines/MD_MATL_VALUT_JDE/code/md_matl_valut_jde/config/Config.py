72d7a3a6783ab37c6004d52472bc461894be1574
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-12 09:21:45 +0000

    JEKT-1726 Loaded all config for all 10 systems 🔁 Modified     - Pipeline        - MD_MATL_VALUT_JDE

--- src/pipelines/MD_MATL_VALUT_JDE/code/md_matl_valut_jde/config/Config.py
+++ src/pipelines/MD_MATL_VALUT_JDE/code/md_matl_valut_jde/config/Config.py
@@ -3,10 +3,40 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            sourceSystem: str=None,
+            F4101: str=None,
+            F4105: str=None,
+            F41021: str=None,
+            divisor: str=None,
+            DAI_ETL_ID: str=None,
+            configDatabase: str=None,
+            targetSchema: str=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update()
+        self.update(sourceSystem, F4101, F4105, F41021, divisor, DAI_ETL_ID, configDatabase, targetSchema)
 
-    def update(self, **kwargs):
+    def update(
+            self,
+            sourceSystem: str="gmd",
+            F4101: str="f4101",
+            F4105: str="f4105",
+            F41021: str="f41021",
+            divisor: str="1",
+            DAI_ETL_ID: str="0",
+            configDatabase: str="  ",
+            targetSchema: str="dev_md_l1",
+            **kwargs
+    ):
         prophecy_spark = self.spark
+        self.sourceSystem = sourceSystem
+        self.F4101 = F4101
+        self.F4105 = F4105
+        self.F41021 = F41021
+        self.divisor = divisor
+        self.DAI_ETL_ID = DAI_ETL_ID
+        self.configDatabase = configDatabase
+        self.targetSchema = targetSchema
         pass
