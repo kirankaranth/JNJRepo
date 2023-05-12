f68bd2a03e218f655b41c804869d4a58aa32f0f7
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-12 14:48:34 +0000

    JEKT-1735 Unit Test for SAP Pipeline created 🔁 Modified     - Pipeline        - SAP_01_MD_BTCH_BBA_BBL_BBN_BWI_TAI_GEU_HCS_MRS_P01_MBP_SVS_FSN_ATL_HM2_HMD

--- src/pipelines/SAP_01_MD_BTCH/code/test/TestSuite.py
+++ src/pipelines/SAP_01_MD_BTCH/code/test/TestSuite.py
@@ -1,5 +1,6 @@
 import unittest
 
+from test.sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd.graph.test_SchemaTransform_MCH1 import *
 from test.sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd.graph.test_SchemaTransform_MCHA import *
 
 if __name__ == "__main__":
--- src/pipelines/SAP_01_MD_BTCH/code/test/sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd/graph/__init__.py
+++ src/pipelines/SAP_01_MD_BTCH/code/test/sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd/graph/__init__.py
@@ -1,1 +1,2 @@
+from .test_SchemaTransform_MCH1 import *
 from .test_SchemaTransform_MCHA import *
