b4b115ac698fbf8917048d8023694e6e528e90f3
Kostadinov, Sava [US] <skostadi@its.jnj.com>
2023-05-15 16:18:49 -0400

    Hotfix for config overrides - ensuring that configsets are properly applied to jobs irrespective of sequencing

--- updateDeploy.py
+++ updateDeploy.py
@@ -5,6 +5,18 @@ import json
 #Used to handle the environment substitution
 dbLoc = "code/databricks-job.json"
 env = sys.argv[1]
+
+
+def getPipelinePath(pipelineName, jsonObject):
+
+    for item in jsonObject["components"]:
+        #print(item["PipelineComponent"]["nodeName"] + "==" + pipelineName)
+        try:
+            if item["PipelineComponent"]["nodeName"] == pipelineName:
+                return item["PipelineComponent"]["id"]
+        except:
+            pass
+
 if env == "prod":
   envFolder = "prd"
 else:
@@ -21,11 +33,28 @@ for item in os.scandir("./src/jobs/"):
             jsonObject = json.load(f)
             #print(jsonObject)
             jsonObject["request"]["name"] = env+"_"+app+"_"+ jsonObject["request"]["name"]
+            ##Auto disable jobs that don't have PIPELINE in their name 
+            if "PIPELINE" not in jsonObject["request"]["name"].upper():
+                jsonObject["request"]["schedule"]["pause_status"] = "PAUSED"
             for task in jsonObject["request"]["tasks"]:
 
                 if 'python_wheel_task' in task:
-               
-                    a = json.loads(task["python_wheel_task"]["parameters"][3])
+
+                    ##Address bug with configs dropping out 
+                    orConfig = open("./src/"+getPipelinePath(task["task_key"], jsonObject)+"/code/configs/"
+                        + "resources/config/" + task["python_wheel_task"]["parameters"][1] + ".json")
+                    a = json.load(orConfig)
+                    #components -> nodename ->  get id
+                    #loop through  jsonObject["components"]
+                    #    task["key"]
+                    #
+                    #Read pipeline folder + id + /code/configs/task["python_wheel_task"]["parameters"][1] .json
+                    # Load that as a 
+                    
+
+                    # Then load the overwrites 
+                    c = json.loads(task["python_wheel_task"]["parameters"][3])
+                    a.update(c)
                     #print(a)
                     if env == "prod" and task["python_wheel_task"]["parameters"][1].upper() == "HM2":
                         #print(jsonObject)
