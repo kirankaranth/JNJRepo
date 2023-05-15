e2d267d8d10247bfe3064f84aacf532433947085
hgermi1@its.jnj.com <hgermi1@its.jnj.com>
2023-05-15 13:09:15 +0000

    🔁 JEKT-1914     - Pipeline        - SAP_MD_CUST_MSTR_UNLD_DATA

--- src/pipelines/SAP_MD_CUST_MSTR_UNLD_DATA/code/sap_md_cust_mstr_unld_data/graph/NEW_FIELDS_RENAME_FORMAT.py
+++ src/pipelines/SAP_MD_CUST_MSTR_UNLD_DATA/code/sap_md_cust_mstr_unld_data/graph/NEW_FIELDS_RENAME_FORMAT.py
@@ -22,4 +22,43 @@ def NEW_FIELDS_RENAME_FORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
         .withColumn("RECV_HRS_MON_PM_FROM", trim(col("MOAB2")))\
         .withColumn("RECV_HRS_MON_PM_UNTL", trim(col("MOBI2")))\
         .withColumn("RECV_HRS_TUE_MORNING_FROM", trim(col("DIAB1")))\
-        .withColumn("RCPT_TIMES_TUE_MORNING_UNTL", trim(col("DIBI1")))
+        .withColumn("RCPT_TIMES_TUE_MORNING_UNTL", trim(col("DIBI1")))\
+        .withColumn("RECV_HRS_TUE_PM_FROM", trim(col("DIAB2")))\
+        .withColumn("RECV_HRS_TUE_PM_UNTL", trim(col("DIBI2")))\
+        .withColumn("RECV_HRS_WED_MORNING_FROM", trim(col("MIAB1")))\
+        .withColumn("RECV_HRS_WED_MORNING_UNTL", trim(col("MIBI1")))\
+        .withColumn("RECV_HRS_WED_PM_FROM", trim(col("MIAB2")))\
+        .withColumn("RECV_HRS_WED_PM_UNTL", trim(col("MIBI2")))\
+        .withColumn("RECV_HRS_THU_MORNING_FROM", trim(col("DOAB1")))\
+        .withColumn("RECV_HRS_THU_MORNING_UNTL", trim(col("DOBI1")))\
+        .withColumn("RECV_HRS_THU_PM_FROM", trim(col("DOAB2")))\
+        .withColumn("RECV_HRS_THU_PM_UNTL", trim(col("DOBI2")))\
+        .withColumn("RECV_HRS_FRI_MORNING_FROM", trim(col("FRAB1")))\
+        .withColumn("RECV_HRS_FRI_MORNING_UNTL", trim(col("FRBI1")))\
+        .withColumn("RECV_HRS_FRI_PM_FROM", trim(col("FRAB2")))\
+        .withColumn("RECV_HRS_FRI_PM_UNTL", trim(col("FRBI2")))\
+        .withColumn("RECV_HRS_SAT_MORNING_FROM", trim(col("SAAB1")))\
+        .withColumn("RECV_HRS_SAT_MORNING_UNTL", trim(col("SABI1")))\
+        .withColumn("RECV_HRS_SAT_PM_FROM", trim(col("SAAB2")))\
+        .withColumn("RECV_HRS_SAT_PM_UNTL", trim(col("SABI2")))\
+        .withColumn("RECV_HRS_SUN_MORNING_FROM", trim(col("SOAB1")))\
+        .withColumn("RECV_HRS_SUN_MORNING_UNTL", trim(col("SOBI1")))\
+        .withColumn("RECV_HRS_SUN_PM_FROM", trim(col("SOAB2")))\
+        .withColumn("RECV_HRS_SUN_PM_UNTL", trim(col("SOBI2")))\
+        .withColumn("DFLT_UNLOADING_PT", trim(col("DEFAB")))\
+        .withColumn("DAI_ETL_ID", lit(Config.DAI_ETL_ID))\
+        .withColumn("DAI_CRT_DTTM", current_timestamp())\
+        .withColumn("DAI_UPDT_DTTM", current_timestamp())\
+        .withColumn("_l0_upt_", col("_upt_"))\
+        .withColumn(
+          "_pk_",
+          to_json(expr("named_struct('SRC_SYS_CD', SRC_SYS_CD, 'CUST_NUM', CUST_NUM, 'UNLOADING_PT', UNLOADING_PT)"))
+        )\
+        .withColumn(
+          "_pk_md5_",
+          md5(
+            to_json(expr("named_struct('SRC_SYS_CD', SRC_SYS_CD, 'CUST_NUM', CUST_NUM, 'UNLOADING_PT', UNLOADING_PT)"))
+          )
+        )\
+        .withColumn("_l1_upt_", current_timestamp())\
+        .withColumn("_deleted_", lit("F"))
