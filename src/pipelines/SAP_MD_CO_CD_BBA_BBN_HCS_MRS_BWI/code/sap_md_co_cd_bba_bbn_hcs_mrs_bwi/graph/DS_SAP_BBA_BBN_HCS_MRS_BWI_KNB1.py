5be6a211b805e59df6736773a6507dec1f29c678
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-15 15:55:44 +0000

    JEKT-1753 🔁 Ran jobs     - Pipeline        - SAP_MD_CO_CD_BBA_BBN_HCS_MRS_BWI     - Dataset        - DS_SAP_HMD_KNB1

--- src/pipelines/SAP_MD_CO_CD_BBA_BBN_HCS_MRS_BWI/code/sap_md_co_cd_bba_bbn_hcs_mrs_bwi/graph/DS_SAP_BBA_BBN_HCS_MRS_BWI_KNB1.py
+++ src/pipelines/SAP_MD_CO_CD_BBA_BBN_HCS_MRS_BWI/code/sap_md_co_cd_bba_bbn_hcs_mrs_bwi/graph/DS_SAP_BBA_BBN_HCS_MRS_BWI_KNB1.py
@@ -6,4 +6,4 @@ from sap_md_co_cd_bba_bbn_hcs_mrs_bwi.config.ConfigStore import *
 from sap_md_co_cd_bba_bbn_hcs_mrs_bwi.udfs.UDFs import *
 
 def DS_SAP_BBA_BBN_HCS_MRS_BWI_KNB1(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"bba.knb1")
+    return spark.read.table(f"{Config.sourceDatabase}.knb1")
