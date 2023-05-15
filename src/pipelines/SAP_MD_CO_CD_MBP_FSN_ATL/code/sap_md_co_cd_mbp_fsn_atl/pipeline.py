00f40c8adc1934bd5b414144251f54a41843e438
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-15 18:17:49 +0000

    JEKT-1753 ❇️ New     - Pipeline        - SAP_MD_CO_CD_MBP_FSN_ATL

---
+++ src/pipelines/SAP_MD_CO_CD_MBP_FSN_ATL/code/sap_md_co_cd_mbp_fsn_atl/pipeline.py
@@ -0,0 +1,32 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from sap_md_co_cd_mbp_fsn_atl.config.ConfigStore import *
+from sap_md_co_cd_mbp_fsn_atl.udfs.UDFs import *
+from prophecy.utils import *
+from sap_md_co_cd_mbp_fsn_atl.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    pass
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
+    Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/SAP_MD_CO_CD_MBP_FSN_ATL")
+    
+    MetricsCollector.start(spark = spark, pipelineId = "pipelines/SAP_MD_CO_CD_MBP_FSN_ATL")
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
---
+++ src/pipelines/SAP_MD_CO_CD_MBP_FSN_ATL/code/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'SAP_MD_CO_CD_MBP_FSN_ATL',
+    version = '1.0',
+    packages = find_packages(include = ('sap_md_co_cd_mbp_fsn_atl*', )) + ["prophecy_config_instances"],
+    package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
+    package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.5.0'],
+    entry_points = {
+'console_scripts' : [
+'main = sap_md_co_cd_mbp_fsn_atl.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
