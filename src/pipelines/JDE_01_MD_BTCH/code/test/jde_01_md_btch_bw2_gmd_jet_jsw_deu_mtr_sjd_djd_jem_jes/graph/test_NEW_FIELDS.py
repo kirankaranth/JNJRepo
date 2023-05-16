a50a2f30bea40e83e1a04de033ed8a438a1bc998
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-16 16:36:37 +0000

    JEKT-1735 Updated Filter Gem and added Unit Test for JDE Pipeline 🔁 Modified     - Pipeline        - JDE_01_MD_BTCH_BW2_GMD_JET_JSW_DEU_MTR_SJD_DJD_JEM_JES

--- src/pipelines/JDE_01_MD_BTCH/code/test/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/test_NEW_FIELDS.py
+++ src/pipelines/JDE_01_MD_BTCH/code/test/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/test_NEW_FIELDS.py
@@ -46,6 +46,26 @@ class NEW_FIELDSTest(BaseTestCase):
         dfOutComputed = NEW_FIELDS(self.spark, dfIn0)
         assertDFEquals(dfOut.select("CHG_DTTM"), dfOutComputed.select("CHG_DTTM"), self.maxUnequalRowsToShow)
 
+    def test_trim(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/NEW_FIELDS/in0/schema.json',
+            'test/resources/data/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/NEW_FIELDS/in0/data/test_trim.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/NEW_FIELDS/out/schema.json',
+            'test/resources/data/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/NEW_FIELDS/out/data/test_trim.json',
+            'out'
+        )
+        dfOutComputed = NEW_FIELDS(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("BTCH_STS_CD", "SUP_NUM", "SUP_BTCH_NUM", "BTCH_TYPE", "LOT_GRADE"),
+            dfOutComputed.select("BTCH_STS_CD", "SUP_NUM", "SUP_BTCH_NUM", "BTCH_TYPE", "LOT_GRADE"),
+            self.maxUnequalRowsToShow
+        )
+
     def setUp(self):
         BaseTestCase.setUp(self)
         import os
