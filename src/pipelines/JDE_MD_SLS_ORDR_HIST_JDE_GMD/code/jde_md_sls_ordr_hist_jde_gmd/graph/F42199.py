68732a5f75081788f92d60691da35196cef0b7f4
ggrilo1@its.jnj.com <ggrilo1@its.jnj.com>
2023-05-17 17:57:10 +0000

    JEKT-1871 - gmd pipeline ❇️ New     - Pipeline        - JDE_MD_SLS_ORDR_HIST_JDE_GMD
    🔁 Modified
        - Dataset
           - DS_JDE_MD_SLS_ORDR_HIST_JDE

---
+++ src/pipelines/JDE_MD_SLS_ORDR_HIST_JDE_GMD/code/jde_md_sls_ordr_hist_jde_gmd/graph/F42199.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from jde_md_sls_ordr_hist_jde_gmd.config.ConfigStore import *
+from jde_md_sls_ordr_hist_jde_gmd.udfs.UDFs import *
+
+def F42199(spark: SparkSession) -> DataFrame:
+    return spark.read.table(f"{Config.sourceDatabase}.f42199")
---
+++ src/pipelines/JDE_MD_SLS_ORDR_HIST_JDE_GMD/code/test/TestSuite.py
@@ -0,0 +1,7 @@
+import unittest
+
+from test.jde_md_sls_ordr_hist_jde_gmd.graph.test_NEW_FIELDS import *
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(unittest.TestSuite())
