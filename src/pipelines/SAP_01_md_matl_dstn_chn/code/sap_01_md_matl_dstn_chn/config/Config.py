50419161cbfb68e3e12ee68f1770597efa1091f3
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-05-17 15:52:56 +0000

    JEKT-1905: main SAP mapping ended 🔁 Modified     - Pipeline        - SAP_01_md_matl_dstn_chn     - Dataset        - MD_MATL_DSTN_CHN

--- src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn/config/Config.py
+++ src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn/config/Config.py
@@ -11,10 +11,33 @@ class Config(ConfigBase):
             sourceTable: str=None,
             targetSchema: str=None,
             sourceDatabase: str=None,
+            ENTRP_DSTN_CHN_STS_CD: str=None,
+            MATL_BASE_CD: str=None,
+            MATL_SLS_CAT_GRP_DESC: str=None,
+            PROD_HIER_LVL_NUM: str=None,
+            DSTN_CHN_STS_CD_DESC: str=None,
+            BLOK_FOR_SLS_ORDR: str=None,
+            PRC_BND_CAT: str=None,
+            PROD_HIER_DESC: str=None,
             **kwargs
     ):
         self.spark = None
-        self.update(SRC_SYS_CD, MANDT, DAI_ETL_ID, sourceTable, targetSchema, sourceDatabase)
+        self.update(
+            SRC_SYS_CD, 
+            MANDT, 
+            DAI_ETL_ID, 
+            sourceTable, 
+            targetSchema, 
+            sourceDatabase, 
+            ENTRP_DSTN_CHN_STS_CD, 
+            MATL_BASE_CD, 
+            MATL_SLS_CAT_GRP_DESC, 
+            PROD_HIER_LVL_NUM, 
+            DSTN_CHN_STS_CD_DESC, 
+            BLOK_FOR_SLS_ORDR, 
+            PRC_BND_CAT, 
+            PROD_HIER_DESC
+        )
 
     def update(
             self,
@@ -24,6 +47,14 @@ class Config(ConfigBase):
             sourceTable: str="mvke",
             targetSchema: str="dev_md_l1",
             sourceDatabase: str="bbl",
+            ENTRP_DSTN_CHN_STS_CD: str="null",
+            MATL_BASE_CD: str="null",
+            MATL_SLS_CAT_GRP_DESC: str="null",
+            PROD_HIER_LVL_NUM: str="TRIM(T179.STUFE)",
+            DSTN_CHN_STS_CD_DESC: str="TRIM(TVMST.VMSTB)",
+            BLOK_FOR_SLS_ORDR: str="TRIM(TVMS.SPVBC)",
+            PRC_BND_CAT: str="trim(PLGTP)",
+            PROD_HIER_DESC: str="TRIM(T179T.VTEXT)",
             **kwargs
     ):
         prophecy_spark = self.spark
@@ -33,4 +64,12 @@ class Config(ConfigBase):
         self.sourceTable = sourceTable
         self.targetSchema = targetSchema
         self.sourceDatabase = sourceDatabase
+        self.ENTRP_DSTN_CHN_STS_CD = ENTRP_DSTN_CHN_STS_CD
+        self.MATL_BASE_CD = MATL_BASE_CD
+        self.MATL_SLS_CAT_GRP_DESC = MATL_SLS_CAT_GRP_DESC
+        self.PROD_HIER_LVL_NUM = PROD_HIER_LVL_NUM
+        self.DSTN_CHN_STS_CD_DESC = DSTN_CHN_STS_CD_DESC
+        self.BLOK_FOR_SLS_ORDR = BLOK_FOR_SLS_ORDR
+        self.PRC_BND_CAT = PRC_BND_CAT
+        self.PROD_HIER_DESC = PROD_HIER_DESC
         pass
--- src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn/graph/DEDUPLICATE.py
+++ src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn/graph/DEDUPLICATE.py
@@ -6,4 +6,14 @@ from sap_01_md_matl_dstn_chn.config.ConfigStore import *
 from sap_01_md_matl_dstn_chn.udfs.UDFs import *
 
 def DEDUPLICATE(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.dropDuplicates(in0.columns)
+    return in0\
+        .withColumn(
+          "row_number",
+          row_number()\
+            .over(Window\
+            .partitionBy("SRC_SYS_CD", "MATL_NUM", "SL_ORG_NUM", "DSTR_CHNL_CD")\
+            .orderBy(lit(1))\
+            .rowsBetween(Window.unboundedPreceding, Window.currentRow))
+        )\
+        .filter(col("row_number") == lit(1))\
+        .drop("row_number")
