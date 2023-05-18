5aa713d6d2e078590dedef6f478427455eb2ec5f
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-18 20:30:26 +0000

    JEKT-1753🔁 Completed Unit Test     - Pipeline        - JDE_MD_CO_CD_BW2_JET_DJD_JES_GMD_MTR_JSW_DEU_JEM_SJD

--- src/pipelines/JDE_MD_CO_CD_BW2_JET_DJD_JES/code/test/TestSuite.py
+++ src/pipelines/JDE_MD_CO_CD_BW2_JET_DJD_JES/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_md_co_cd_bw2_jet_djd_jes_gmd_mtr_jsw_deu_jem_sjd.graph.test_NEW_FIELDS_TRANSFORMATION import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/JDE_MD_CO_CD_BW2_JET_DJD_JES/code/test/__init__.py
+++ src/pipelines/JDE_MD_CO_CD_BW2_JET_DJD_JES/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .jde_md_co_cd_bw2_jet_djd_jes_gmd_mtr_jsw_deu_jem_sjd import *
 from .conftest import *
 from .TestSuite import *
