e6e55d652cb3b1400647d240b2778bd9bce3acba
smille29@its.jnj.com <smille29@its.jnj.com>
2023-05-18 22:26:35 +0000

    JEKT-402- 🔁 Modified - Removed Variable for SAP Tables in Source GEMs and set all Config to values vs pulling from Default Config     - Pipeline        - MD_MATL_ALT_UOM_BW2_DEU_DJD_GMD_JEM_JES_JET_JSW_MTR_SDJ        - MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI_HMD     - Dataset        - DS_SAP_01_MARM        - DS_SAP_MARA_01

--- src/pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI/code/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai_hmd/graph/DS_SAP_01_MARM.py
+++ src/pipelines/MD_MATL_ALT_UOM_ATL_BBA_BBL_BBN_BWI_FSN_GEU_HCS_MBP_MRS_P01_SVS_TAI/code/md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai_hmd/graph/DS_SAP_01_MARM.py
@@ -6,4 +6,4 @@ from md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai_hmd.con
 from md_matl_alt_uom_atl_bba_bbl_bbn_bwi_fsn_geu_hcs_mbp_mrs_p01_svs_tai_hmd.udfs.UDFs import *
 
 def DS_SAP_01_MARM(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.{Config.DBTABLE}")
+    return spark.read.table(f"{Config.sourceDatabase}.marm")
