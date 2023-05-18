967698016e7725ba7edcbcda43c232a0378cae42
ggrilo1@its.jnj.com <ggrilo1@its.jnj.com>
2023-05-18 17:35:04 +0000

    JEKT-1871 - adjustments on all the pipelines 🔁 Modified     - Pipeline        - JDE_MD_SLS_ORDR_HIST_LDGR_BW2        - JDE_MD_SLS_ORDR_HIST_LDGR_JES        - JDE_MD_SLS_ORDR_HIST_LDGR_GMD        - JDE_MD_SLS_ORDR_HIST_LDGR_DEU_DJD_JEM_JET_SJD        - JDE_MD_SLS_ORDR_HIST_LDGR_JSW     - Dataset        - MD_SLS_ORDR_HIST_LDGR

--- src/pipelines/JDE_MD_SLS_ORDR_HIST_JDE_DEU_DJD_JEM_JET_SJD/code/jde_md_sls_ordr_hist_ldgr_deu_djd_jem_jet_sjd/graph/MD_SLS_ORDR_HIST_LDGR.py
+++ src/pipelines/JDE_MD_SLS_ORDR_HIST_JDE_DEU_DJD_JEM_JET_SJD/code/jde_md_sls_ordr_hist_ldgr_deu_djd_jem_jet_sjd/graph/MD_SLS_ORDR_HIST_LDGR.py
@@ -5,5 +5,9 @@ from prophecy.libs import typed_lit
 from jde_md_sls_ordr_hist_ldgr_deu_djd_jem_jet_sjd.config.ConfigStore import *
 from jde_md_sls_ordr_hist_ldgr_deu_djd_jem_jet_sjd.udfs.UDFs import *
 
-def MD_SLS_ORDR_HIST_JDE(spark: SparkSession, in0: DataFrame):
-    in0.write.format("delta").mode("overwrite").saveAsTable(f"dev_md_l1.MD_SLS_ORDR_HIST_LDGR")
+def MD_SLS_ORDR_HIST_LDGR(spark: SparkSession, in0: DataFrame):
+    in0.write\
+        .format("delta")\
+        .option("replaceWhere", f"SRC_SYS_CD = '{Config.sourceSystem}'")\
+        .mode("overwrite")\
+        .saveAsTable(f"dev_md_l1.MD_SLS_ORDR_HIST_LDGR")
