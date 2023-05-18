00f928d700fb61d884fcd5ec18fd88815e267679
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-18 13:52:36 +0000

    JEKT-1887 🔁 Modified     - Pipeline        - SAP_01_MD_MATL_LOC_HMD

--- src/pipelines/SAP_01_MD_MATL_LOC_HMD/code/sap_01_md_matl_loc_hmd/config/Config.py
+++ src/pipelines/SAP_01_MD_MATL_LOC_HMD/code/sap_01_md_matl_loc_hmd/config/Config.py
@@ -12,26 +12,10 @@ class Config(ConfigBase):
             sourceTable: str=None,
             DAI_ETL_ID: int=None,
             ConfigDatabase: str=None,
-            ACT_PUSH: str=None,
-            MTS_MTO_FL: str=None,
-            DUAL_SRCNG_WIP: str=None,
-            CNSMPTN_MODE: str=None,
             **kwargs
     ):
         self.spark = None
-        self.update(
-            sourceSystem, 
-            targetSchema, 
-            MANDT, 
-            sourceDatabase, 
-            sourceTable, 
-            DAI_ETL_ID, 
-            ConfigDatabase, 
-            ACT_PUSH, 
-            MTS_MTO_FL, 
-            DUAL_SRCNG_WIP, 
-            CNSMPTN_MODE
-        )
+        self.update(sourceSystem, targetSchema, MANDT, sourceDatabase, sourceTable, DAI_ETL_ID, ConfigDatabase)
 
     def update(
             self,
@@ -42,10 +26,6 @@ class Config(ConfigBase):
             sourceTable: str="NSDM_V_MARC",
             DAI_ETL_ID: int=0,
             ConfigDatabase: str=" ",
-            ACT_PUSH: str="cast(null as string)",
-            MTS_MTO_FL: str="cast(null as string)",
-            DUAL_SRCNG_WIP: str="cast(null as string)",
-            CNSMPTN_MODE: str="cast(null as string)",
             **kwargs
     ):
         prophecy_spark = self.spark
@@ -56,8 +36,4 @@ class Config(ConfigBase):
         self.sourceTable = sourceTable
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.ConfigDatabase = ConfigDatabase
-        self.ACT_PUSH = ACT_PUSH
-        self.MTS_MTO_FL = MTS_MTO_FL
-        self.DUAL_SRCNG_WIP = DUAL_SRCNG_WIP
-        self.CNSMPTN_MODE = CNSMPTN_MODE
         pass
