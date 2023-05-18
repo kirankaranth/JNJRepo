3dd1086e20109325c8832b8c1861e6f072a3ef9d
JCacere7@its.jnj.com <JCacere7@its.jnj.com>
2023-05-18 10:18:53 +0000

    JEKT-1905: 🔁 Modified     - Pipeline        - SAP_01_md_matl_dstn_chn_bba_bbl_bbn_bwi_geu_hcs_mbp_mrs_p01_tai_svs_hmd_hm2_atl_fsn     - Dataset        - MD_MATL_DSTN_CHN

--- src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn_bba_bbl_bbn_bwi_geu_hcs_mbp_mrs_p01_tai_svs_hmd_hm2_atl_fsn/config/Config.py
+++ src/pipelines/SAP_01_md_matl_dstn_chn/code/sap_01_md_matl_dstn_chn_bba_bbl_bbn_bwi_geu_hcs_mbp_mrs_p01_tai_svs_hmd_hm2_atl_fsn/config/Config.py
@@ -6,12 +6,12 @@ class Config(ConfigBase):
     def __init__(
             self,
             SRC_SYS_CD: str=None,
+            sourceDatabase: str=None,
+            sourceSystem: str=None,
             MANDT: str=None,
             DAI_ETL_ID: int=None,
             sourceTable: str=None,
             targetSchema: str=None,
-            sourceDatabase: str=None,
-            sourceSystem: str=None,
             ENTRP_DSTN_CHN_STS_CD: str=None,
             MATL_BASE_CD: str=None,
             MATL_SLS_CAT_GRP_DESC: str=None,
@@ -21,12 +21,12 @@ class Config(ConfigBase):
         self.spark = None
         self.update(
             SRC_SYS_CD, 
+            sourceDatabase, 
+            sourceSystem, 
             MANDT, 
             DAI_ETL_ID, 
             sourceTable, 
             targetSchema, 
-            sourceDatabase, 
-            sourceSystem, 
             ENTRP_DSTN_CHN_STS_CD, 
             MATL_BASE_CD, 
             MATL_SLS_CAT_GRP_DESC, 
@@ -36,12 +36,12 @@ class Config(ConfigBase):
     def update(
             self,
             SRC_SYS_CD: str="bbl",
+            sourceDatabase: str="bbl",
+            sourceSystem: str="bbl",
             MANDT: str="100",
             DAI_ETL_ID: int=0,
             sourceTable: str="mvke",
             targetSchema: str="dev_md_l1",
-            sourceDatabase: str="bbl",
-            sourceSystem: str="bbl",
             ENTRP_DSTN_CHN_STS_CD: str="null",
             MATL_BASE_CD: str="null",
             MATL_SLS_CAT_GRP_DESC: str="null",
@@ -50,12 +50,12 @@ class Config(ConfigBase):
     ):
         prophecy_spark = self.spark
         self.SRC_SYS_CD = SRC_SYS_CD
+        self.sourceDatabase = sourceDatabase
+        self.sourceSystem = sourceSystem
         self.MANDT = MANDT
         self.DAI_ETL_ID = self.get_int_value(DAI_ETL_ID)
         self.sourceTable = sourceTable
         self.targetSchema = targetSchema
-        self.sourceDatabase = sourceDatabase
-        self.sourceSystem = sourceSystem
         self.ENTRP_DSTN_CHN_STS_CD = ENTRP_DSTN_CHN_STS_CD
         self.MATL_BASE_CD = MATL_BASE_CD
         self.MATL_SLS_CAT_GRP_DESC = MATL_SLS_CAT_GRP_DESC
