6e3ce1f221859592e03dc3c691486b75f9423fc9
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-19 23:19:04 +0000

    JEKT-1887 🔁 Modified     - Pipeline        - JDE_MD_MATL_LOC_JET_JSW_MTR_BW2_GMD_DEU_JEM_JES_SJD_DJD     - Dataset        - MD_MATL_LOC

--- src/pipelines/JDE_MD_MATL_LOC_JET_JSW_MTR_BW2_GMD_DEU_JEM_JES_SJD_DJD/code/jde_md_matl_loc_jet_jsw_mtr_bw2_gmd_deu_jem_jes_sjd_djd/graph/SET_FIELD_ORDER_REFORMAT.py
+++ src/pipelines/JDE_MD_MATL_LOC_JET_JSW_MTR_BW2_GMD_DEU_JEM_JES_SJD_DJD/code/jde_md_matl_loc_jet_jsw_mtr_bw2_gmd_deu_jem_jes_sjd_djd/graph/SET_FIELD_ORDER_REFORMAT.py
@@ -52,5 +52,8 @@ def SET_FIELD_ORDER_REFORMAT(spark: SparkSession, in0: DataFrame) -> DataFrame:
         col("_pk_"), 
         col("_pk_md5_"), 
         col("_l1_upt_"), 
-        col("_deleted_")
+        col("_deleted_"), 
+        col("BUY_NUM"), 
+        col("LINE_TYPE"), 
+        col("SHIPPING_CMMDTY_CLS")
     )
