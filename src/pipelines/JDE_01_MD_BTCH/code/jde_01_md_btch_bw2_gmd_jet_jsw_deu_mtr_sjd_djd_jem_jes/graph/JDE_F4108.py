f9850bc8f78c49c9128288d333900f24f302f6cf
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-05-19 14:01:19 +0000

    JEKT-1735 Updated Pipeline and Package Name 🔁 Modified     - Pipeline        - SAP_MD_BTCH        - JDE_MD_BTCH     - Job        - JOB_MD_BTCH

--- src/pipelines/JDE_01_MD_BTCH/code/jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes/graph/JDE_F4108.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.config.ConfigStore import *
-from jde_01_md_btch_bw2_gmd_jet_jsw_deu_mtr_sjd_djd_jem_jes.udfs.UDFs import *
-
-def JDE_F4108(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.{Config.sourceTable}")
--- src/pipelines/SAP_01_MD_BTCH/code/test/TestSuite.py
+++ src/pipelines/SAP_01_MD_BTCH/code/test/TestSuite.py
@@ -1,7 +1,7 @@
 import unittest
 
-from test.sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd.graph.test_SchemaTransform_MCH1 import *
-from test.sap_01_md_btch_bba_bbl_bbn_bwi_tai_geu_hcs_mrs_p01_mbp_svs_fsn_atl_hm2_hmd.graph.test_SchemaTransform_MCHA import *
+from test.sap_md_btch.graph.test_SchemaTransform_MCH1 import *
+from test.sap_md_btch.graph.test_SchemaTransform_MCHA import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
