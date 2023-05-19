b0475f852c4bbc6138caaaaa412359fdec7c3832
Kostadinov, Sava [US] <skostadi@its.jnj.com>
2023-05-18 21:31:42 -0400

    reverting src/pipelines/SAP_01_MD_PO_SCHED_LINE_DELV src/pipelines/SAP_MD_MATL_BBA_BBN src/pipelines/SAP_MD_MATL_BBL src/pipelines/SAP_MD_MATL_HCS src/pipelines/SAP_MD_MATL_HM2 src/pipelines/SAP_MD_MATL_HMD1 src/pipelines/SAP_MD_MATL_MBP to beta

--- src/pipelines/SAP_01_MD_PO_SCHED_LINE_DELV/code/sap_md_po_sched_line_delv_01/graph/DUPLICATE_CHECK_FILTER.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_md_po_sched_line_delv_01.config.ConfigStore import *
-from sap_md_po_sched_line_delv_01.udfs.UDFs import *
-
-def DUPLICATE_CHECK_FILTER(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter((col("PK_COUNT") > lit(1)))
