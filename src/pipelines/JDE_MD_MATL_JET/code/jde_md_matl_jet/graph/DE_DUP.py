be764f28175bfb1c4a027200b924487d087a63e7
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-22 09:29:18 +0000

    🔁 Modified     - Pipeline        - JDE_MD_MATL_JET

--- src/pipelines/JDE_MD_MATL_JET/code/jde_md_matl_jet/graph/DE_DUP.py
+++ src/pipelines/JDE_MD_MATL_JET/code/jde_md_matl_jet/graph/DE_DUP.py
@@ -7,10 +7,6 @@ from jde_md_matl_jet.udfs.UDFs import *
 
 def DE_DUP(spark: SparkSession, in0: DataFrame) -> DataFrame:
     return in0\
-        .withColumn(
-          "row_number",
-          row_number()\
-            .over(Window.partitionBy("T162_XBITM").orderBy(lit(1)).rowsBetween(Window.unboundedPreceding, Window.currentRow))
-        )\
+        .withColumn("row_number", row_number().over(Window.partitionBy("T162_XBITM").orderBy(lit(1))))\
         .filter(col("row_number") == lit(1))\
         .drop("row_number")
