4ea068a8a287f2f41ff3a92690c08f5e24cfd830
luis.sorarrain@its.jnj.com <luis.sorarrain@its.jnj.com>
2023-05-23 20:58:16 +0000

    JEKT-1887 🔁 Modified     - Pipeline        - SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL     - Dataset        - MD_MATL_LOC

--- src/pipelines/SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL/code/sap_md_matl_loc_fsn_geu_bba_atl/config/Config.py
+++ src/pipelines/SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL/code/sap_md_matl_loc_fsn_geu_bba_atl/config/Config.py
@@ -15,10 +15,7 @@ class Config(ConfigBase):
             MMS_FIN_CLSN_CD: str=None,
             VMI_IND: str=None,
             MSTR_PLNG_FMLY_CD: str=None,
-            ACT_PUSH: str=None,
             MTS_MTO_FL: str=None,
-            DUAL_SRCNG_WIP: str=None,
-            CNSMPTN_MODE: str=None,
             **kwargs
     ):
         self.spark = None
@@ -33,10 +30,7 @@ class Config(ConfigBase):
             MMS_FIN_CLSN_CD, 
             VMI_IND, 
             MSTR_PLNG_FMLY_CD, 
-            ACT_PUSH, 
-            MTS_MTO_FL, 
-            DUAL_SRCNG_WIP, 
-            CNSMPTN_MODE
+            MTS_MTO_FL
         )
 
     def update(
@@ -51,10 +45,7 @@ class Config(ConfigBase):
             MMS_FIN_CLSN_CD: str="cast(null as string)",
             VMI_IND: str="cast(null as string)",
             MSTR_PLNG_FMLY_CD: str="cast(null as string)",
-            ACT_PUSH: str="cast(null as string)",
             MTS_MTO_FL: str="cast(null as string)",
-            DUAL_SRCNG_WIP: str="cast(null as string)",
-            CNSMPTN_MODE: str="cast(null as string)",
             **kwargs
     ):
         prophecy_spark = self.spark
@@ -68,8 +59,5 @@ class Config(ConfigBase):
         self.MMS_FIN_CLSN_CD = MMS_FIN_CLSN_CD
         self.VMI_IND = VMI_IND
         self.MSTR_PLNG_FMLY_CD = MSTR_PLNG_FMLY_CD
-        self.ACT_PUSH = ACT_PUSH
         self.MTS_MTO_FL = MTS_MTO_FL
-        self.DUAL_SRCNG_WIP = DUAL_SRCNG_WIP
-        self.CNSMPTN_MODE = CNSMPTN_MODE
         pass
