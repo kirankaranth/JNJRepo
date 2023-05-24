4e6a2fc6445f7f921a723f9b7e327910d0bdf2d1
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-24 13:15:07 +0000

    JEKT-2509 Created all config 🔁 Modified     - Pipeline        - SAP_MD_OWN_EXPLN_FOR_TERM_OF_PMT

--- src/pipelines/SAP_MD_OWN_EXPLN_FOR_TERM_OF_PMT/code/sap_md_own_expln_for_term_of_pmt/config/Config.py
+++ src/pipelines/SAP_MD_OWN_EXPLN_FOR_TERM_OF_PMT/code/sap_md_own_expln_for_term_of_pmt/config/Config.py
@@ -3,10 +3,34 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            sourceDatabase: str=None,
+            sourceSystem: str=None,
+            MANDT: str=None,
+            targetSchema: str=None,
+            configDatabase: str=None,
+            DAI_ETL_ID: str=None,
+            **kwargs
+    ):
         self.spark = None
-        self.update()
+        self.update(sourceDatabase, sourceSystem, MANDT, targetSchema, configDatabase, DAI_ETL_ID)
 
-    def update(self, **kwargs):
+    def update(
+            self,
+            sourceDatabase: str="atl",
+            sourceSystem: str="atl",
+            MANDT: str="100",
+            targetSchema: str="dev_md_l1",
+            configDatabase: str=" ",
+            DAI_ETL_ID: str="0",
+            **kwargs
+    ):
         prophecy_spark = self.spark
+        self.sourceDatabase = sourceDatabase
+        self.sourceSystem = sourceSystem
+        self.MANDT = MANDT
+        self.targetSchema = targetSchema
+        self.configDatabase = configDatabase
+        self.DAI_ETL_ID = DAI_ETL_ID
         pass
