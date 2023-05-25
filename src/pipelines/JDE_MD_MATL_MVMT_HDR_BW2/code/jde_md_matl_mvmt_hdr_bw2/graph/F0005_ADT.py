c817096954dc64e0eee8fea19e7d9f1dcc3b4056
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-05-25 14:06:00 +0000

    JEKT-1210 Update sourceSystem -> sourceDatabase in DataSets source 🔁 Modified     - Dataset        - DS_JDE_F4111        - DS_F0005_ADT        - DS_JDE_F0005     - Pipeline        - JDE_MD_MATL_MVMT_HDR_DEU_DJD_MTR_JEM_JES_JET_JSW        - JDE_MD_MATL_MVMT_HDR_BW2        - JDE_MD_MATL_MVMT_HDR_GMD

--- src/pipelines/JDE_MD_MATL_MVMT_HDR_BW2/code/jde_md_matl_mvmt_hdr_bw2/graph/F0005_ADT.py
+++ src/pipelines/JDE_MD_MATL_MVMT_HDR_BW2/code/jde_md_matl_mvmt_hdr_bw2/graph/F0005_ADT.py
@@ -6,4 +6,4 @@ from jde_md_matl_mvmt_hdr_bw2.config.ConfigStore import *
 from jde_md_matl_mvmt_hdr_bw2.udfs.UDFs import *
 
 def F0005_ADT(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceSystem}.f0005_adt")
+    return spark.read.table(f"{Config.sourceDatabase}.f0005_adt")
