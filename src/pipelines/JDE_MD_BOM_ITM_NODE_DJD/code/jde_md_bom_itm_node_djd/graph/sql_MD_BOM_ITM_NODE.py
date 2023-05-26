60112e88fd78b8beb85e8842d308ca1bfdbfb494
jcovezd1@its.jnj.com <jcovezd1@its.jnj.com>
2023-05-26 11:39:22 +0000

    JEKT-2457🔁 Added upt field     - Pipeline        - JDE_MD_BOM_ITM_NODE_DJD

--- src/pipelines/JDE_MD_BOM_ITM_NODE_DJD/code/jde_md_bom_itm_node_djd/graph/sql_MD_BOM_ITM_NODE.py
+++ src/pipelines/JDE_MD_BOM_ITM_NODE_DJD/code/jde_md_bom_itm_node_djd/graph/sql_MD_BOM_ITM_NODE.py
@@ -11,7 +11,7 @@ def sql_MD_BOM_ITM_NODE(spark: SparkSession) -> DataFrame:
     SELECT
 '{Config.sourceSystem}' AS SRC_SYS_CD
 , f3002.ixtbm as BOM_CAT_CD
-,concat(f3002.ixkit,';',f3002.ixmmcu,';' ,f3002.ixtbm,';',f3002.ixbqty,';',f3002.ixcoby,';',f3002.ixsbnt) AS BOM_NUM
+,concat(f3002.ixkit,';',f3002.ixmmcu,';' ,f3002.ixtbm,';',f3002.ixbqty,';',f3002.ixcoby,';',f3002.ixcpnt,';',f3002.ixsbnt) AS BOM_NUM
 , \"01\" AS ALT_BOM_NUM
 , f3002.ixitm AS BOM_ITM_NODE_NUM
 ,concat(f3002.ixcpnt,\".\",f3002.ixsbnt) AS BOM_ITM_NODE_CNTR_NBR
@@ -21,7 +21,8 @@ def sql_MD_BOM_ITM_NODE(spark: SparkSession) -> DataFrame:
 , CASE WHEN LOWER(TRIM(f3002.ixupmj)) = 'CAST(NULL AS timestamp)' OR TRIM(f3002.ixupmj) = '' OR TRIM(f3002.ixupmj) = '0' THEN CAST(NULL AS TIMESTAMP) ELSE to_timestamp(concat(substr(CAST(date_add(concat(substr(CAST(CAST(TRIM(f3002.ixupmj) AS INT) + 1900000 as string),1,4),'-01-01'), CAST(substr(CAST(CAST(TRIM(f3002.ixupmj) AS INT) + 1900000 AS string),5) AS INT )-1) AS string), 1, 10),' ', lpad(TRIM(f3002.ixtday), 6, '0')), \"yyyy-MM-dd HHmmss\") END AS CHG_DTTM
 , NULL as DEL_IND,
 f3002._upt_ as _l0_upt_,
-f3002._deleted_
+f3002._deleted_,
+f3002._upt_
 FROM  {Config.sourceDatabase}.f3002 as f3002  WHERE f3002._deleted_ = 'F'
   
  
