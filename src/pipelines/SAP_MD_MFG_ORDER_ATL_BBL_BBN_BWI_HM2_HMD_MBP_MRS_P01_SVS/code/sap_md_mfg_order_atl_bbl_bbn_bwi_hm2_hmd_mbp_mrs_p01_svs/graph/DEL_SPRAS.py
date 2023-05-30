87a051e3f457567974330f61d6dfd583296d5900
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-05-30 12:06:29 +0000

    JEKT-2083 Updated Select fields gem to include all the AFKO fields 🔁 Modified     - Pipeline        - SAP_MD_MFG_ORDER_ATL_BBL_BBN_BWI_HM2_HMD_MBP_MRS_P01_SVS     - Dataset        - MD_MFG_ORDER

--- src/pipelines/SAP_MD_MFG_ORDER_ATL_BBL_BBN_BWI_HM2_HMD_MBP_MRS_P01_SVS/code/sap_md_mfg_order_atl_bbl_bbn_bwi_hm2_hmd_mbp_mrs_p01_svs/graph/DEL_SPRAS.py
+++ src/pipelines/SAP_MD_MFG_ORDER_ATL_BBL_BBN_BWI_HM2_HMD_MBP_MRS_P01_SVS/code/sap_md_mfg_order_atl_bbl_bbn_bwi_hm2_hmd_mbp_mrs_p01_svs/graph/DEL_SPRAS.py
@@ -6,4 +6,4 @@ from sap_md_mfg_order_atl_bbl_bbn_bwi_hm2_hmd_mbp_mrs_p01_svs.config.ConfigStore
 from sap_md_mfg_order_atl_bbl_bbn_bwi_hm2_hmd_mbp_mrs_p01_svs.udfs.UDFs import *
 
 def DEL_SPRAS(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(lit(True))
+    return in0.filter(((col("_deleted_") == lit("F")) & (col("SPRAS") == lit("E"))))
