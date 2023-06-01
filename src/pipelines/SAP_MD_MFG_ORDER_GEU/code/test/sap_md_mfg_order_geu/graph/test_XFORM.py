c0f1da6c2a8e931d387509245e0d1f66e5656b35
Natkins7@its.jnj.com <Natkins7@its.jnj.com>
2023-06-01 16:15:18 +0000

    JEKT-2083 More unit tests 🔁 Modified     - Pipeline        - SAP_MD_MFG_ORDER_GEU

--- src/pipelines/SAP_MD_MFG_ORDER_GEU/code/test/sap_md_mfg_order_geu/graph/test_XFORM.py
+++ src/pipelines/SAP_MD_MFG_ORDER_GEU/code/test/sap_md_mfg_order_geu/graph/test_XFORM.py
@@ -26,6 +26,46 @@ class XFORMTest(BaseTestCase):
         dfOutComputed = XFORM(self.spark, dfIn0)
         assertDFEquals(dfOut.select("CHG_DTTM"), dfOutComputed.select("CHG_DTTM"), self.maxUnequalRowsToShow)
 
+    def test_decimal_test_(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/in0/schema.json',
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/in0/data/test_decimal_test_.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/out/schema.json',
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/out/data/test_decimal_test_.json',
+            'out'
+        )
+        dfOutComputed = XFORM(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("EST_TOT_COSTS_OF_ORDR"),
+            dfOutComputed.select("EST_TOT_COSTS_OF_ORDR"),
+            self.maxUnequalRowsToShow
+        )
+
+    def test_trim_test_(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/in0/schema.json',
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/in0/data/test_trim_test_.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/out/schema.json',
+            'test/resources/data/sap_md_mfg_order_geu/graph/XFORM/out/data/test_trim_test_.json',
+            'out'
+        )
+        dfOutComputed = XFORM(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select("MFG_ORDR_TYP_CD", "MFG_ORDR_NUM", "DEL_IND", "OBJECT_NUMBER", "PLNT_CD"),
+            dfOutComputed.select("MFG_ORDR_TYP_CD", "MFG_ORDR_NUM", "DEL_IND", "OBJECT_NUMBER", "PLNT_CD"),
+            self.maxUnequalRowsToShow
+        )
+
     def setUp(self):
         BaseTestCase.setUp(self)
         import os
