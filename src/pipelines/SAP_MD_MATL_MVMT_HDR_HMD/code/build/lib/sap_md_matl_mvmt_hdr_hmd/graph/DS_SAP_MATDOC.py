af42d7367a95caf41830eb0bca561bfa4ca8d0fc
Kostadinov, Sava [US] <skostadi@its.jnj.com>
2023-06-07 16:21:07 -0400

    restoring src/pipelines/SAP_MD_MATL_HM2 src/pipelines/SAP_MD_MATL_MVMT_HDR_HMD

--- src/pipelines/SAP_MD_MATL_MVMT_HDR_HMD/code/build/lib/sap_md_matl_mvmt_hdr_hmd/graph/DS_SAP_MATDOC.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_md_matl_mvmt_hdr_hmd.config.ConfigStore import *
-from sap_md_matl_mvmt_hdr_hmd.udfs.UDFs import *
-
-def DS_SAP_MATDOC(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.matdoc")
