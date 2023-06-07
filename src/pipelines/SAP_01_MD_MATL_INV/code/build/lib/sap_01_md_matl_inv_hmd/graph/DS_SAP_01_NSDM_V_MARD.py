581a13043c1ff0726846312444fda4f8f9e7349b
Kostadinov, Sava [US] <skostadi@its.jnj.com>
2023-06-07 16:15:55 -0400

    restoring src/pipelines/SAP_01_MD_MATL_INV src/pipelines/SAP_01_MD_PO_SCHED_LINE_DELV src/pipelines/SAP_MD_BOM_HDR_ATL_BBL_BWI_FSN_GEU_MBP_P01_SVS_TAI

--- src/pipelines/SAP_01_MD_MATL_INV/code/build/lib/sap_01_md_matl_inv_hmd/graph/DS_SAP_01_NSDM_V_MARD.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_01_md_matl_inv_hmd.config.ConfigStore import *
-from sap_01_md_matl_inv_hmd.udfs.UDFs import *
-
-def DS_SAP_01_NSDM_V_MARD(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.NSDM_V_MARD")
--- src/pipelines/SAP_01_MD_MATL_INV/code/build/lib/sap_01_md_matl_inv_hmd/graph/MANDT_1.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_01_md_matl_inv_hmd.config.ConfigStore import *
-from sap_01_md_matl_inv_hmd.udfs.UDFs import *
-
-def MANDT_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(((col("_deleted_") == lit("F")) & (col("MANDT") == lit(Config.MANDT))))
