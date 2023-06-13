7f4ab8dd7e12d0939bcea99381467fca59a4168a
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-13 21:29:42 +0000

    JEKT-2901 - 🔁 Modified - Updated Pipeline to use Config Spark Expression for BOM_NUM field as JES is different Rule     - Pipeline        - JDE_MD_MATL_BOM_BW2_DEU_GMD_JEM_JES_JET_JSW_MTR_SJD     - Dataset        - MD_MATL_BOM        - JDE_01_f3002_f3002_adt

--- src/pipelines/JDE_01_MD_MATL_BOM/code/jde_md_matl_bom_bw2_deu_gmd_jem_jes_jet_jsw_mtr_sjd/graph/JDE_f3002_f3002_adt.py
+++ src/pipelines/JDE_01_MD_MATL_BOM/code/jde_md_matl_bom_bw2_deu_gmd_jem_jes_jet_jsw_mtr_sjd/graph/JDE_f3002_f3002_adt.py
@@ -2,8 +2,10 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from jde_md_matl_bom_bw2_deu_gmd_jem_jes_jet_jsw_mtr_sjd.config.ConfigStore import *
 from jde_md_matl_bom_bw2_deu_gmd_jem_jes_jet_jsw_mtr_sjd.udfs.UDFs import *
 
 def JDE_f3002_f3002_adt(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceSystem}.{Config.sourceTable}")
+    return spark.read.table(f"{Config.sourceDatabase}.{Config.sourceTable}")
