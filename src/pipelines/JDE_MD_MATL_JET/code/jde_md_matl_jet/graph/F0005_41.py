d6fe61b61982fc49d11589b09e7afd0618920dc4
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-15 22:09:02 +0000

    JEKT-1896 - 🔁 Modified - Fixed the F0005_41 GEM and added a Filter to remove the Predicate Filter.     - Dataset        - DS_JDE_01_F4101        - DS_JDE_F0005_NC        - DS_JDE_01_F0005_41        - MD_MATL     - Pipeline        - JDE_MD_MATL_JET

--- src/pipelines/JDE_MD_MATL_JET/code/jde_md_matl_jet/graph/F0005_41.py
+++ src/pipelines/JDE_MD_MATL_JET/code/jde_md_matl_jet/graph/F0005_41.py
@@ -2,8 +2,10 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from jde_md_matl_jet.config.ConfigStore import *
 from jde_md_matl_jet.udfs.UDFs import *
 
 def F0005_41(spark: SparkSession) -> DataFrame:
-    return spark.sql(f"SELECT * FROM {Config.sourceDatabase}.{Config.DBTABLE1} WHERE Trim(DRSY) = '41'")
+    return spark.read.table(f"{Config.sourceDatabase}.{Config.DBTABLE1}")
--- src/pipelines/JDE_MD_MATL_JET/code/setup.py
+++ src/pipelines/JDE_MD_MATL_JET/code/setup.py
@@ -4,10 +4,10 @@ setup(
     version = '1.0',
     packages = find_packages(include = ('jde_md_matl_jet*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
-    package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
+    package_data = {'prophecy_config_instances' : ['*.json', '*.py', '*.conf']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==1.5.0'],
+'prophecy-libs==1.5.4'],
     entry_points = {
 'console_scripts' : [
 'main = jde_md_matl_jet.pipeline:main'], },
