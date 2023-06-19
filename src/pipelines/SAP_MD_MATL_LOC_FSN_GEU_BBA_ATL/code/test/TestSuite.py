f12b90880f852d5adf22dc2eb9a54371153acf59
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-19 21:53:25 +0000

    JEKT-1887 - 🔁 Modified - Fixed failing Unit Tests     - Pipeline        - SAP_01_MD_MATL_LOC_HMD        - SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL        - SAP_MD_MATL_LOC_MBP_SVS_BBN_P01_MRS        - SAP_MD_MATL_LOC_BWI        - SAP_MD_MATL_LOC_BBL_HCS_TAI

--- src/pipelines/SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL/code/test/TestSuite.py
+++ src/pipelines/SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL/code/test/TestSuite.py
@@ -1,7 +1,6 @@
 import unittest
 
 from test.sap_md_matl_loc_fsn_geu_bba_atl.graph.test_NEW_FIELDS import *
-from test.sap_md_matl_loc_fsn_geu_bba_atl.graph.test_SET_FIELD_ORDER_REFORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/test/TestSuite.py
+++ src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/test/TestSuite.py
@@ -1,7 +1,6 @@
 import unittest
 
 from test.sap_md_matl_loc_mbp_svs_bbn_p01_mrs.graph.test_NEW_FIELDS import *
-from test.sap_md_matl_loc_mbp_svs_bbn_p01_mrs.graph.test_SET_FIELD_ORDER_REFORMAT import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
