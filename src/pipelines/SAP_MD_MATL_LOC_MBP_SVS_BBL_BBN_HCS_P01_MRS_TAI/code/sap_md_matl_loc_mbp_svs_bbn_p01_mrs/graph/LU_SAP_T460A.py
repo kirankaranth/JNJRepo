c2a1e194956c45ba520ff52e6e9e6af9b43dbaad
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-19 21:24:10 +0000

    JEKT-1887 - 🔁 Modified - Updated the MD_MATL_JOB to include new pipelines and added ATL - Verified all other MD_MATL Pipelines     - Pipeline        - JDE_MD_MATL_LOC_GMD_DEU_JEM_JES_DJD        - SAP_MD_MATL_LOC_MBP_SVS_BBN_P01_MRS        - SAP_MD_MATL_LOC_BWI        - JDE_MD_MATL_LOC_JET_JSW_MTR_BW2_SJD        - SAP_MD_MATL_LOC_BBL_HCS_TAI        - SAP_01_MD_MATL_LOC_HMD        - SAP_MD_MATL_LOC_FSN_GEU_BBA_ATL        - SAP_MD_MATL_LOC_HMD     - Job        - JOB_MD_MATL_LOC

--- src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/sap_md_matl_loc_mbp_svs_bbn_p01_mrs/graph/LU_SAP_T460A.py
+++ src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/sap_md_matl_loc_mbp_svs_bbn_p01_mrs/graph/LU_SAP_T460A.py
@@ -2,6 +2,8 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from sap_md_matl_loc_mbp_svs_bbn_p01_mrs.config.ConfigStore import *
 from sap_md_matl_loc_mbp_svs_bbn_p01_mrs.udfs.UDFs import *
 
