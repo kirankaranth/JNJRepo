0e4fd0ab155d11aa783b31eaf61f78432b7b0b70
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-20 15:00:36 +0000

    JEKT-1210 - 🔁 Modified Fixed the Unit Test     - Pipeline        - JDE_MD_MATL_MVMT_HDR_BW2

--- src/pipelines/JDE_MD_MATL_MVMT_HDR_BW2/code/jde_md_matl_mvmt_hdr_bw2/pipeline.py
+++ src/pipelines/JDE_MD_MATL_MVMT_HDR_BW2/code/jde_md_matl_mvmt_hdr_bw2/pipeline.py
@@ -4,6 +4,8 @@ from pyspark.sql.types import *
 from jde_md_matl_mvmt_hdr_bw2.config.ConfigStore import *
 from jde_md_matl_mvmt_hdr_bw2.udfs.UDFs import *
 from prophecy.utils import *
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from jde_md_matl_mvmt_hdr_bw2.graph import *
 
 def pipeline(spark: SparkSession) -> None:
@@ -26,6 +28,7 @@ def main():
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/JDE_MD_MATL_MVMT_HDR_BW2")
+    registerUDFs(spark)
     
     MetricsCollector.start(spark = spark, pipelineId = "pipelines/JDE_MD_MATL_MVMT_HDR_BW2")
     pipeline(spark)
