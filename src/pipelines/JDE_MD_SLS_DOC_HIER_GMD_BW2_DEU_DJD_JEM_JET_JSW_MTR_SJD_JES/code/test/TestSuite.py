480dc927a31ab35efe6b705fbfc276dce8b83979
mbolado1@its.jnj.com <mbolado1@its.jnj.com>
2023-06-22 20:20:10 +0000

    JEKT-2182 Unit Testing done for JDE Pipeline 🔁 Modified     - Pipeline        - JDE_MD_SLS_DOC_HIER_GMD_BW2_DEU_DJD_JEM_JET_JSW_MTR_SJD_JES

--- src/pipelines/JDE_MD_SLS_DOC_HIER_GMD_BW2_DEU_DJD_JEM_JET_JSW_MTR_SJD_JES/code/test/TestSuite.py
+++ src/pipelines/JDE_MD_SLS_DOC_HIER_GMD_BW2_DEU_DJD_JEM_JET_JSW_MTR_SJD_JES/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.jde_md_sls_doc_hier_gmd_bw2_deu_djd_jem_jet_jsw_mtr_sjd_jes.graph.test_NEW_FIELDS import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- src/pipelines/JDE_MD_SLS_DOC_HIER_GMD_BW2_DEU_DJD_JEM_JET_JSW_MTR_SJD_JES/code/test/__init__.py
+++ src/pipelines/JDE_MD_SLS_DOC_HIER_GMD_BW2_DEU_DJD_JEM_JET_JSW_MTR_SJD_JES/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .jde_md_sls_doc_hier_gmd_bw2_deu_djd_jem_jet_jsw_mtr_sjd_jes import *
 from .conftest import *
 from .TestSuite import *
