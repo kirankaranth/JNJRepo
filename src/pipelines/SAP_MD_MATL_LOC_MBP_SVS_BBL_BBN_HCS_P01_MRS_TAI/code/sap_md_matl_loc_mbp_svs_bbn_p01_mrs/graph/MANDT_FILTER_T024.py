ad4ca77fed7702d769a437e0ca73d582133bffb5
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-22 22:40:06 +0000

    JEKT-2742 - 🔁 Modified - Retouch     - Pipeline        - SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI

--- src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/sap_md_matl_loc_mbp_svs_bbn_p01_mrs/graph/MANDT_FILTER_T024.py
+++ src/pipelines/SAP_MD_MATL_LOC_MBP_SVS_BBL_BBN_HCS_P01_MRS_TAI/code/sap_md_matl_loc_mbp_svs_bbn_p01_mrs/graph/MANDT_FILTER_T024.py
@@ -8,4 +8,4 @@ from sap_md_matl_loc_mbp_svs_bbn_p01_mrs.config.ConfigStore import *
 from sap_md_matl_loc_mbp_svs_bbn_p01_mrs.udfs.UDFs import *
 
 def MANDT_FILTER_T024(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0.filter(((col("MANDT") == lit(Config.MANDT)) & (col("_deleted_") == lit("F"))))
+    return in0.filter(((col("_deleted_") == lit("F")) & (col("MANDT") == lit(Config.MANDT))))
