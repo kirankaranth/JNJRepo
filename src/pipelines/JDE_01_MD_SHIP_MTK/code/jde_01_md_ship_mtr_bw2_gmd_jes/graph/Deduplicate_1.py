99d280a7db06d79c25702c6799905825551a2090
smille29@its.jnj.com <smille29@its.jnj.com>
2023-06-23 22:28:48 +0000

    JEKT-2742- 🔁 Modified Touched Pipeline to force commit     - Pipeline        - JDE_01_MD_SHIP_MTK

--- src/pipelines/JDE_01_MD_SHIP_MTK/code/jde_01_md_ship_mtr_bw2_gmd_jes/graph/Deduplicate_1.py
+++ src/pipelines/JDE_01_MD_SHIP_MTK/code/jde_01_md_ship_mtr_bw2_gmd_jes/graph/Deduplicate_1.py
@@ -2,6 +2,8 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from jde_01_md_ship_mtr_bw2_gmd_jes.config.ConfigStore import *
 from jde_01_md_ship_mtr_bw2_gmd_jes.udfs.UDFs import *
 
@@ -11,9 +13,8 @@ def Deduplicate_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
           "row_number",
           row_number()\
             .over(Window\
-            .partitionBy("SRC_SYS_CD", "SHIP_NUM", "DELV_TYP_CD", "DOC_REF_NUM", "CO_CD", "DELV_LINE_NBR")\
-            .orderBy(lit(1))\
-            .rowsBetween(Window.unboundedPreceding, Window.currentRow))
+            .partitionBy("SRC_SYS_CD", "DELV_TYP_CD", "SHIP_NUM", "DOC_REF_NUM", "CO_CD", "DELV_LINE_NBR")\
+            .orderBy(lit(1)))
         )\
         .filter(col("row_number") == lit(1))\
         .drop("row_number")
