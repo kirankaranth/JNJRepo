b06cf87d9abd6723396f2297b6d8f4b8e5103df2
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-06-27 21:11:18 +0000

    Update capitalization of source tables in Config for MATL_INV Update jobs for DJD, BBN, and HM2 DJD_PIPELINE_JOB -> MD_MATL_LOC is pointing to source system DEU -> will change to DJD DJD_PIPELINE_JOB -> MD_MFG_ORDER_ITM is missing -> will add MD_MFG_ORDER_ITM BBN_PIPELINE_JOB -> MD_MATL_VALU should be MD_MATL_VALUT -> renaming the step HM2_PIPELINE_JOB is missing: MD_DOC_HDR_INVC_RCPT MD_PRCH_INFO MD_PRCH_INFO_ORG 🔁 Modified     - Job        - DEV_BBN_PIPELINE_JOB        - DEV_DJD_PIPELINE_JOB        - HM2_PIPELINE_JOB     - Pipeline        - SAP_MD_MATL_INV_P01        - SAP_01_MD_MATL_INV

--- src/pipelines/SAP_01_MD_MATL_INV/code/sap_01_md_matl_inv/graph/DS_SAP_03_NSDM_V_MSKU.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from sap_01_md_matl_inv.config.ConfigStore import *
-from sap_01_md_matl_inv.udfs.UDFs import *
-
-def DS_SAP_03_NSDM_V_MSKU(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceSystem}.nsdm_v_msku")
--- src/pipelines/SAP_MD_MATL_INV_P01/code/test/sap_md_matl_inv_p01/graph/__init__.py
+++
@@ -1,2 +0,0 @@
-from .test_SchemaTransform_5_MSLB import *
-from .test_SchemaTransform_2_MCHB import *
