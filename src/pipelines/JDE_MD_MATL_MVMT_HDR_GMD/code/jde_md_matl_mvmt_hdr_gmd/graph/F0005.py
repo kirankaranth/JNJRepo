f621039545c57193d3946fff2a36266d018b8892
skostadi@its.jnj.com <skostadi@its.jnj.com>
2023-06-27 21:15:27 +0000

    Correct MD_MATL_MVMT_HDR - Remove reference to DBTABLE1 for JDE 🔁 Modified     - Pipeline        - SAP_MD_MATL_MVMT_HDR        - JDE_MD_MATL_MVMT_HDR_GMD        - SAP_MD_MATL_MVMT_HDR_HMD        - JDE_MD_MATL_MVMT_HDR_DEU_DJD_MTR_JEM_JES_JET__JSW        - JDE_MD_MATL_MVMT_HDR_GMD_v2

--- src/pipelines/JDE_MD_MATL_MVMT_HDR_GMD/code/jde_md_matl_mvmt_hdr_gmd/graph/F0005.py
+++ src/pipelines/JDE_MD_MATL_MVMT_HDR_GMD/code/jde_md_matl_mvmt_hdr_gmd/graph/F0005.py
@@ -2,8 +2,10 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.libs import typed_lit
+from prophecy.transpiler import call_spark_fcn
+from prophecy.transpiler.fixed_file_schema import *
 from jde_md_matl_mvmt_hdr_gmd.config.ConfigStore import *
 from jde_md_matl_mvmt_hdr_gmd.udfs.UDFs import *
 
 def F0005(spark: SparkSession) -> DataFrame:
-    return spark.read.table(f"{Config.sourceDatabase}.{Config.DBTABLE1}")
+    return spark.read.table(f"{Config.sourceDatabase}.f0005")
